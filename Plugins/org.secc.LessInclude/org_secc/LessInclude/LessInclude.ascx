2020bdffbb14641f190297ebeb69af673d9ac009
mleesecc <mlee@secc.org>
2016-03-10 12:00:58 -0500

    ROCK-29 Init LessInclude to modify custom themes with LESS

---
+++ Plugins/org.secc.LessInclude/org_secc/LessInclude/LessInclude.ascx
@@ -0,0 +1,101 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="LessInclude.ascx.cs" Inherits="RockWeb.Plugins.org_secc.LessInclude.LessInclude" %>
+
+<script type="text/javascript">
+    function clearDialog() {
+        $('#rock-config-cancel-trigger').click();
+    }
+</script>
+
+<asp:UpdatePanel ID="upnlContent" runat="server" UpdateMode="Conditional">
+    <ContentTemplate>
+
+ <%-- Edit Panel --%>
+        <asp:Panel ID="pnlEditModal" runat="server" Visible="false">
+            <Rock:ModalDialog ID="mdEdit" runat="server" OnSaveClick="mdEdit_SaveClick" Title="Dynamic LESS Configuration" OnCancelScript="clearDialog();">
+                <Content>
+
+                    <asp:UpdatePanel ID="upnlEdit" runat="server">
+                        <ContentTemplate>
+                            <div class="panel-group" id="accordion" role="tablist">
+
+
+                                <div class="panel panel-default">
+                                    <div class="panel-heading" role="tab" id="overrideHeading">
+                                        <h4 class="panel-title">
+                                            <a role="button" data-toggle="collapse" data-parent="#override"
+                                                href="#override" aria-expanded="true" >
+                                                Variable Orverrides <i class="fa fa-chevron-down"></i>
+                                            </a>
+                                        </h4>
+                                    </div>
+                                    <div id="override" class="panel-collapse collapse" role="tabpanel">
+                                        <div class="panel-body">
+                                            <Rock:CodeEditor ID="ceOverrides" EditorMode="Less" Label="LESS Variable Overrides"
+                                                EditorHeight="400" runat="server"></Rock:CodeEditor>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="panel panel-default">
+                                    <div class="panel-heading" role="tab" id="variablesHeading">
+                                        <h4 class="panel-title">
+                                            <a role="button" data-toggle="collapse" data-parent="#variables"
+                                                href="#variables" aria-expanded="true" >
+                                                All Variables <i class="fa fa-chevron-down"></i>
+                                            </a>
+                                        </h4>
+                                    </div>
+                                    <div id="variables" class="panel-collapse collapse" role="tabpanel">
+                                        <div class="panel-body">
+                                            <Rock:CodeEditor ID="ceVariables" EditorMode="Less" Label="LESS Variables"
+                                                EditorHeight="400" runat="server"></Rock:CodeEditor>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="panel panel-default">
+                                    <div class="panel-heading" role="tab" id="themeHeading">
+                                        <h4 class="panel-title">
+                                            <a role="button" data-toggle="collapse" data-parent="#theme"
+                                                href="#theme" aria-expanded="true" >
+                                                Theme <i class="fa fa-chevron-down"></i>
+                                            </a>
+                                        </h4>
+                                    </div>
+                                    <div id="theme" class="panel-collapse collapse" role="tabpanel">
+                                        <div class="panel-body">
+                                            <Rock:CodeEditor ID="ceTheme" EditorMode="Less" Label="Template LESS"
+                                                EditorHeight="400" runat="server"></Rock:CodeEditor>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="panel panel-default">
+                                    <div class="panel-heading" role="tab" id="printHeading">
+                                        <h4 class="panel-title">
+                                            <a role="button" data-toggle="collapse" data-parent="#print"
+                                                href="#print" aria-expanded="true" >
+                                                Print <i class="fa fa-chevron-down"></i>
+                                            </a>
+                                        </h4>
+                                    </div>
+                                    <div id="print" class="panel-collapse collapse" role="tabpanel">
+                                        <div class="panel-body">
+                                            <Rock:CodeEditor ID="cePrint" EditorMode="Less" Label="Print LESS"
+                                                EditorHeight="400" runat="server"></Rock:CodeEditor>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="alert alert-warning">Note: A reload of the page may be required to refresh browser cached CSS.</div>
+                        </ContentTemplate>
+                    </asp:UpdatePanel>
+                </Content>
+            </Rock:ModalDialog>
+<%-- End Edit Panel --%>
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Plugins/org.secc.LessInclude/org_secc/LessInclude/LessInclude.ascx.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI.HtmlControls;
+using dotless.Core;
+using dotless.Core.configuration;
+using System.Text;
+
+namespace RockWeb.Plugins.org_secc.LessInclude
+{
+    [DisplayName("Less Include")]
+    [Category("CMS")]
+    [Description("Block to include custom CSS via LESS to current page.")]
+
+    // Custom Settings
+    [CodeEditorField("Variables", "LESS Variables", CodeEditorMode.Less,CodeEditorTheme.Rock,400,false,"", "CustomSetting")]
+    [CodeEditorField("Overrides", "LESS Overrides", CodeEditorMode.Less, CodeEditorTheme.Rock, 400, false, "", "CustomSetting")]
+    [CodeEditorField("Theme", "LESS Theme", CodeEditorMode.Less, CodeEditorTheme.Rock, 400, false, "", "CustomSetting")]
+    [CodeEditorField("Print", "LESS Print Theme", CodeEditorMode.Less, CodeEditorTheme.Rock, 400, false, "", "CustomSetting")]
+
+
+    public partial class LessInclude : RockBlockCustomSettings
+    {
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            //Remove fallback css
+            var ctrl = (ContentPlaceHolder)Page.Header.FindControl("CSSDefault");
+            ctrl.Visible = false;
+
+            //Load our new css
+            string bootstrapFileLocation = GetFileLocation("bootstrap.css");
+
+            var bootstrap = new HtmlLink();
+            bootstrap.Href = ResolveRockUrl(bootstrapFileLocation);
+            bootstrap.Attributes.Add("rel", "stylesheet");
+            bootstrap.Attributes.Add("type", "text/css");
+            Page.Header.Controls.Add(bootstrap);
+
+            string themeFileLocation = GetFileLocation("theme.css");
+
+            var theme = new HtmlLink();
+            theme.Href = ResolveRockUrl(themeFileLocation);
+            theme.Attributes.Add("rel", "stylesheet");
+            theme.Attributes.Add("type", "text/css");
+            Page.Header.Controls.Add(theme);
+        }
+
+        private string GetFileLocation(string fileName)
+        {
+            string blockId = BlockId.ToString();
+
+            var cache = (string) GetCacheItem(blockId + fileName);
+
+            if (cache != null)
+            {
+                return cache;
+            }
+
+            //Set CWD
+            string styleLocation = "~/Themes/" + base.RockPage.Site.Theme + "/Styles/";
+            Environment.CurrentDirectory = Server.MapPath(styleLocation);
+
+            string path = blockId + "/";
+
+            string fileLocation;
+
+            if (File.Exists(path + fileName))
+            {
+                fileLocation = "~~/Styles/" + blockId + "/" + fileName;
+                
+            }
+            else
+            {
+                fileLocation = "~~/Styles/" + fileName;
+            }
+            AddCacheItem(blockId + fileName, fileLocation);
+            return fileLocation;
+        }
+
+        protected override void ShowSettings()
+        {
+            pnlEditModal.Visible = true;
+            upnlContent.Update();
+            mdEdit.Show();
+
+            ceVariables.Text = GetPageFile("_variables.less");
+            ceOverrides.Text = GetPageFile("_variable-overrides.less");
+            ceTheme.Text = GetPageFile("theme.less");
+            cePrint.Text = GetPageFile("_print.less");
+
+        }
+
+        private string GetPageFile(string fileName)
+        {
+            string lessLocation = "~/Themes/" + base.RockPage.Site.Theme + "/Styles/";
+            Environment.CurrentDirectory = Server.MapPath(lessLocation);
+
+            string blockId = BlockId.ToString();
+            string path = blockId + "/";
+            
+            if (File.Exists(path + fileName))
+            {
+                using (var fs = File.OpenText(path + fileName))
+                {
+                    return fs.ReadToEnd();
+                }
+            }
+            else
+            {
+                using (var fs = File.OpenText(fileName))
+                {
+                    return fs.ReadToEnd();
+                }
+            }
+        }
+
+        private void ProcessLess()
+        {
+            string blockId = BlockId.ToString();
+
+            string lessLocation = "~/Themes/" + base.RockPage.Site.Theme + "/Styles/"+ blockId + "/";
+            Environment.CurrentDirectory = Server.MapPath(lessLocation);
+
+            string bootstrapLocation = lessLocation + "bootstrap.less";
+            var bootstrapMapped =  Server.MapPath(bootstrapLocation);
+            var bootstrapFile = File.ReadAllText(bootstrapMapped);
+            var bootstrapCSS = Less.Parse(bootstrapFile);
+
+            SavePageFile(bootstrapCSS, "bootstrap.css");
+
+            string themeLocation = lessLocation + "theme.less";
+            var themeMapped = Server.MapPath(themeLocation);
+            var themeFile = File.ReadAllText(themeMapped);
+            var themeCSS = Less.Parse(themeFile);
+
+            SavePageFile(themeCSS, "theme.css");
+
+            FlushCacheItem(blockId + "bootstrap.css");
+            FlushCacheItem(blockId + "theme.css");
+        }
+        
+        private void SavePageFile(string content, string fileName)
+        {
+            string lessLocation = "~/Themes/" + base.RockPage.Site.Theme + "/Styles/";
+            Environment.CurrentDirectory = Server.MapPath(lessLocation);
+
+            string blockId = BlockId.ToString();
+            string path = blockId + "/";
+
+            //If a directory for this block does not exist
+            //Create the directory and copy the custom bootsrap
+            //and template less files to the folder
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                File.Copy("themeCustom.less", path + "theme.less");
+                File.Copy("bootstrapCustom.less", path + "bootstrap.less");
+            }
+
+                
+            // Truncate the file if it exists.
+            if (File.Exists(path + fileName))
+            {
+                using (FileStream fs = File.Open(path + fileName, FileMode.Truncate))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes(content);
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+            else
+            {
+                // Create the file.
+                using (FileStream fs = File.Create(path + fileName))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes(content);
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+        }
+
+        protected void mdEdit_SaveClick(object sender, EventArgs e)
+        {
+            SavePageFile (ceVariables.Text, "_variables.less");
+            SavePageFile(ceOverrides.Text, "_variable-overrides.less");
+            SavePageFile(ceTheme.Text, "theme.less");
+            SavePageFile(cePrint.Text, "_print.less");
+
+            ProcessLess();
+
+            mdEdit.Hide();
+        }
+    }
+}
