7e77d559bf44c31b0656c8e76e9258e2e483ef2b
mleesecc <mlee@secc.org>
2016-03-31 11:04:56 -0400

    Rock-43 Init workflow to merge form fields in pdf

---
+++ Plugins/org.secc.PDFMerge/Helpers/PDFFormMergeEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Rock.Model;
+
+namespace org.secc.PDFMerge
+{
+    public class PDFFormMergeEntity
+    {
+        public BinaryFile PDF { get; set; }
+
+        public Dictionary<string,string> MergeFields { get; set; }
+
+        public BinaryFile MergedPDF { get; set; }
+
+        public bool Flatten { get; set; }
+    }
+}
---
+++ Plugins/org.secc.PDFMerge/Workflows/PDFFormMerge.cs
@@ -0,0 +1,129 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Workflow;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace org.secc.PDFMerge
+{
+    /// <summary>
+    /// Delays successful execution of action until a specified number of minutes have passed
+    /// </summary>
+    [ActionCategory( "PDF" )]
+    [Description( "Merges" )]
+    [Export( typeof( Rock.Workflow.ActionComponent ) )]
+    [ExportMetadata( "ComponentName", "PDF Form Merge" )]
+
+    class PDFFormMerge : ActionComponent
+    {
+        /// <summary>
+        /// Executes the action.
+        /// </summary>
+        /// <param name="rockContext">The rock context.</param>
+        /// <param name="action">The action.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="errorMessages">The error messages.</param>
+        /// <returns></returns>
+        public override bool Execute( RockContext rockContext, WorkflowAction action, object entity, out List<string> errorMessages )
+        {
+            errorMessages = new List<string>();
+
+            PDFFormMergeEntity pdfFormMergeEntity = GetPDFFormMergeFromEntity( entity, out errorMessages );
+
+            var pdf = pdfFormMergeEntity.PDF;
+
+            using (MemoryStream ms = new MemoryStream() )
+            {
+                var pdfBytes = pdf.ContentStream.ReadBytesToEnd();
+                var pdfReader = new PdfReader( pdfBytes );
+
+                var stamper = new PdfStamper( pdfReader, ms );
+
+                var form = stamper.AcroFields;
+
+                form.GenerateAppearances = true;
+
+                var fieldKeys = form.Fields.Keys;
+
+                foreach ( string fieldKey in fieldKeys )
+                {
+                    if ( pdfFormMergeEntity.MergeFields.ContainsKey( fieldKey ) )
+                    {
+                        form.SetField( fieldKey, pdfFormMergeEntity.MergeFields[fieldKey]);
+                   }
+                }
+
+                stamper.FormFlattening = pdfFormMergeEntity.Flatten;
+
+                stamper.Close();
+                pdfReader.Close();
+
+                BinaryFile mergedPDF = new BinaryFile();
+                mergedPDF.CopyPropertiesFrom( pdf );
+                mergedPDF.Guid = Guid.NewGuid();
+
+                BinaryFileData pdfData = new BinaryFileData();
+                pdfData.Content = ms.ToArray();
+
+                mergedPDF.DatabaseData = pdfData;
+
+                pdfFormMergeEntity.MergedPDF = mergedPDF;
+
+                pdfReader.Close();
+            }
+
+            return true;
+        }
+
+        private PDFFormMergeEntity GetPDFFormMergeFromEntity( object entity, out List<string> errorMessages )
+        {
+            errorMessages = new List<string>();
+
+            if ( entity is PDFFormMergeEntity )
+            {
+                return ( PDFFormMergeEntity ) entity;
+            }
+
+            errorMessages.Add( "Could not get PDFFormMergeEntity object" );
+            return null;
+        }
+
+        public static byte[] ReadFully( Stream input )
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using ( MemoryStream ms = new MemoryStream() )
+            {
+                int read;
+                while ( ( read = input.Read( buffer, 0, buffer.Length ) ) > 0 )
+                {
+                    ms.Write( buffer, 0, read );
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+}
