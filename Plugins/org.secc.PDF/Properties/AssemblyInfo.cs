f0cf2ef08eadf0a286795f7dff184e5ecefd17e9
mleesecc <mlee@secc.org>
2016-04-12 13:58:30 -0400

    ROCK-50 Removed PDFMerge and changed PDF to library

--- Plugins/org.secc.PDF/Properties/AssemblyInfo.cs
+++ Plugins/org.secc.PDF/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "org.secc.LavaPDF" )]
+[assembly: AssemblyTitle( "org.secc.PDF" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "org.secc.LavaPDF" )]
+[assembly: AssemblyProduct( "org.secc.PDF" )]
 [assembly: AssemblyCopyright( "Copyright ©  2016" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
