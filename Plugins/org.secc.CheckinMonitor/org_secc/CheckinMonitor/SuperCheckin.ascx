93ff28d901e590e32bd2af9da0ed11dca321c123
mleesecc <mlee@secc.org>
2016-05-19 15:44:03 -0400

    ROCK-96 Super checkin block init

---
+++ Plugins/org.secc.CheckinMonitor/org_secc/CheckinMonitor/SuperCheckin.ascx
@@ -0,0 +1,12 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="SuperCheckin.ascx.cs" Inherits="RockWeb.Plugins.org_secc.CheckinMonitor.SuperCheckin" %>
+<asp:UpdatePanel ID="upContent" runat="server">
+    <ContentTemplate>
+        <Rock:ModalAlert ID="maWarning" runat="server" />
+        <asp:Panel ID="pnlNewFamily" Visible="false" runat="server">
+            New Family
+        </asp:Panel>
+        <asp:Panel ID="pnlManageFamily" Visible="false" runat="server">
+            Existing Family
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Plugins/org.secc.CheckinMonitor/org_secc/CheckinMonitor/SuperCheckin.ascx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+using Rock;
+using Rock.Attribute;
+using Rock.CheckIn;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.org_secc.CheckinMonitor
+{
+    [DisplayName( "Super Checkin" )]
+    [Category( "SECC > Check-in" )]
+    [Description( "Advanced tool for managing checkin." )]
+    public partial class SuperCheckin : CheckInBlock
+    {
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            if ( !KioskCurrentlyActive )
+            {
+                NavigateToHomePage();
+            }
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+            if ( !Page.IsPostBack )
+            {
+                if ( CurrentCheckInState.CheckIn.Families.Where( f => f.Selected ).Any() )
+                {
+                    pnlManageFamily.Visible = true;
+                    ActivateFamily();
+                }
+                else
+                {
+                    pnlNewFamily.Visible = true;
+                }
+            }
+        }
+
+        private void ActivateFamily()
+        {
+            List<string> errorMessages = new List<string>();
+            ProcessActivity( GetAttributeValue( "WorkflowActivity" ), out errorMessages );
+        }
+    }
+}
