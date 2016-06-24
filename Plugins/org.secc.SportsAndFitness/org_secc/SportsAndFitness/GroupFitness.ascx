a8ceaf9810dec18456aeac795bd5de0fda2dc2ae
mleesecc <mlee@secc.org>
2016-06-24 15:37:48 -0400

    ROCK-107 Further work on group fitness

--- Plugins/org.secc.SportsAndFitness/org_secc/SportsAndFitness/GroupFitness.ascx
+++ Plugins/org.secc.SportsAndFitness/org_secc/SportsAndFitness/GroupFitness.ascx
@@ -14,13 +14,19 @@
         })
     })
 </script>
+<style>
+    hr {
+        visibility:hidden;
+    }
+</style>
 <asp:UpdatePanel ID="upContent" runat="server">
     <ContentTemplate>
         <Rock:ModalAlert ID="maError" runat="server" />
         <asp:Panel runat="server" ID="pnlSearch">
-            Please scan your card!
-            <Rock:RockTextBox ID="tbPhone" CssClass="checkin-phone-entry" autocomplete="off" runat="server"/>
-            <asp:LinkButton runat="server" ID="lbSearch" OnClick="lbSearch_Click" Text="SEARCH"></asp:linkbutton>
+            <h1>Welcome!</h1>
+            <h2>Please scan your card to sign into a group fitness class.</h2>
+            <Rock:RockTextBox ID="tbPhone" CssClass="checkin-phone-entry" autocomplete="off" runat="server" style="opacity:0; height:0px;"/>
+            <asp:LinkButton runat="server" ID="lbSearch" OnClick="lbSearch_Click" Text="SEARCH" style="opacity:0"></asp:linkbutton>
         </asp:Panel>
         <asp:Panel runat="server" ID="pnlNotFound" Visible="false">
            <Rock:NotificationBox runat="server" ID="nbNotFound" NotificationBoxType="Warning">
@@ -28,12 +34,25 @@
                We're sorry, we could not find your information in our system. If you think this is in error please
                contact one of our volunteers or staff.
            </Rock:NotificationBox>
+            <Rock:BootstrapButton runat="server" ID="btnBack" CssClass="btn btn-danger" Text="Back" OnClick="btnCancel_Click"></Rock:BootstrapButton>
         </asp:Panel>
-        <asp:Panel runat="server" ID="pnlCheckin" Visible="false">
+        <asp:Panel runat="server" ID="pnlCheckin" Visible="false" CssClass="well">
             <h1>
                 Welcome <asp:Literal runat="server" ID="ltNickName" />
             </h1>
             <h2>You have <asp:Literal runat="server" ID="ltSessions"/> sessions remaining.</h2>
+            <br /><br />
+            <Rock:NotificationBox runat="server" ID="nbNotOpen" NotificationBoxType="Info" Visible="false">There are no current group fitness sessions available to check-in at this time</Rock:NotificationBox>
+            <asp:PlaceHolder runat="server" id="phClasses"/>
+            <Rock:BootstrapButton runat="server" ID="btnCheckin" Visible="false" CssClass="btn btn-success btn-lg" Text="Check-In To Class" OnClick="btnCheckin_Click"></Rock:BootstrapButton>
+            <Rock:BootstrapButton runat="server" ID="btnCancel" CssClass="btn btn-danger btn-lg" Text="Back" OnClick="btnCancel_Click"></Rock:BootstrapButton>
+        </asp:Panel>
+        <asp:Panel runat="server" ID="pnlDone" Visible="false">
+            <Rock:NotificationBox ID="nbText" NotificationBoxType="Success" runat="server">
+                <h1>Welcome!</h1>
+                We are preparing your name tag now. Thank you for joining us today.
+            </Rock:NotificationBox>
+            <Rock:BootstrapButton runat="server" ID="btnDone" OnClick="btnCancel_Click"></Rock:BootstrapButton>
         </asp:Panel>
     </ContentTemplate>
         <Triggers>
--- Plugins/org.secc.SportsAndFitness/org_secc/SportsAndFitness/GroupFitness.ascx.cs
+++ Plugins/org.secc.SportsAndFitness/org_secc/SportsAndFitness/GroupFitness.ascx.cs
@@ -46,6 +46,9 @@ namespace RockWeb.Plugins.org_secc.SportsAndFitness
                 NavigateToHomePage();
             }
 
+            RockPage.AddScriptLink( "~/Scripts/CheckinClient/cordova-2.4.0.js", false );
+            RockPage.AddScriptLink( "~/Scripts/CheckinClient/ZebraPrint.js" );
+
             _rockContext = new RockContext();
         }
 
@@ -53,33 +56,49 @@ namespace RockWeb.Plugins.org_secc.SportsAndFitness
         {
             base.OnLoad( e );
 
+            if ( CurrentCheckInState == null )
+            {
+                NavigateToPreviousPage();
+                return;
+            }
+
             var groupSessionsAttributeGuid = GetAttributeValue( "SessionsAttribute" ).AsGuid();
             if ( groupSessionsAttributeGuid != Guid.Empty )
             {
                 _groupSessionsKey = AttributeCache.Read( groupSessionsAttributeGuid, _rockContext ).Key;
             }
 
+            nbNotOpen.Visible = false;
+            btnCheckin.Visible = false;
+
             if ( !Page.IsPostBack )
             {
                 this.Page.Form.DefaultButton = lbSearch.UniqueID;
 
-                if ( CurrentCheckInState == null )
-                {
-                    NavigateToPreviousPage();
-                    return;
-                }
 
                 CurrentCheckInState.CheckIn = new CheckInStatus();
                 SaveState();
                 //people can be preselected coming out of the workflow, we want to unselect them
             }
-
+            else
+            {
+                if ( CurrentCheckInState.CheckIn.CurrentPerson != null )
+                {
+                    ShowPersonCheckin();
+                }
+            }
         }
 
 
 
         protected void lbSearch_Click( object sender, EventArgs e )
         {
+            if ( CurrentCheckInState == null )
+            {
+                NavigateToPreviousPage();
+                return;
+            }
+
             if ( string.IsNullOrWhiteSpace( tbPhone.Text ) )
             { return; }
             CurrentCheckInState.CheckIn.SearchValue = tbPhone.Text;
@@ -112,7 +131,97 @@ namespace RockWeb.Plugins.org_secc.SportsAndFitness
             pnlNotFound.Visible = false;
             pnlSearch.Visible = false;
             pnlCheckin.Visible = true;
-            ltNickName.Text = CurrentCheckInState.CheckIn.CurrentPerson.Person.NickName;
+            var person = CurrentCheckInState.CheckIn.CurrentPerson;
+            ltNickName.Text = person.Person.NickName;
+            var group = GetMembershipGroup();
+            var groupMember = new GroupMemberService( _rockContext ).GetByGroupIdAndPersonId( group.Id, person.Person.Id ).FirstOrDefault();
+            if ( groupMember == null )
+            {
+                maError.Show( "Could not find group member entity", ModalAlertType.Alert );
+                return;
+            }
+            groupMember.LoadAttributes();
+            int sessions = groupMember.GetAttributeValue( _groupSessionsKey ).AsInteger();
+            ltSessions.Text = sessions.ToString();
+
+            if (sessions == 0 )
+            {
+                return;
+            }
+
+            //This is a cheat to know if any groups are active and display a no active message if not.
+            var checkinCount = 0;
+
+            phClasses.Controls.Clear();
+
+            foreach ( var groupType in person.GroupTypes )
+            {
+                foreach ( var chGroup in groupType.Groups )
+                {
+                    foreach ( var location in chGroup.Locations )
+                    {
+                        foreach ( var schedule in location.Schedules )
+                        {
+                            Panel pnlClass = new Panel();
+                            pnlClass.Style.Add( "margin", "5px" );
+                            phClasses.Controls.Add( pnlClass );
+
+                            BootstrapButton btnSelect = new BootstrapButton();
+                            if ( schedule.Selected )
+                            {
+                                btnSelect.Text = "<i class='fa fa-check-square-o'></i>";
+                                btnSelect.CssClass = "btn btn-success btn-lg";
+                            }
+                            else
+                            {
+                                btnSelect.Text = "<i class='fa fa-square-o'></i>";
+                                btnSelect.CssClass = "btn btn-default btn-lg";
+                            }
+                            btnSelect.ID = "s" + chGroup.Group.Id.ToString() + location.Location.Id.ToString() + schedule.Schedule.Id.ToString();
+                            btnSelect.Click += ( s, e ) => { ToggleClass( chGroup, location, schedule, schedule.Selected ); };
+                            pnlClass.Controls.Add( btnSelect );
+
+                            Literal ltClassName = new Literal();
+                            ltClassName.Text = " " + chGroup.Group.Name + " @ " + schedule.Schedule.Name + " in " + location.Location.Name;
+                            pnlClass.Controls.Add( ltClassName );
+                            checkinCount++;
+                        }
+                    }
+                }
+            }
+            if ( checkinCount == 0 )
+            {
+                nbNotOpen.Visible = true;
+            }
+        }
+
+        private void ToggleClass( CheckInGroup chGroup, CheckInLocation chLocation, CheckInSchedule chSchedule, bool alreadySelected )
+        {
+            foreach ( var groupType in CurrentCheckInState.CheckIn.CurrentPerson.GroupTypes )
+            {
+                groupType.Selected = true;
+                foreach ( var group in groupType.Groups )
+                {
+                    group.Selected = false;
+                    foreach ( var location in group.Locations )
+                    {
+                        location.Selected = false;
+                        foreach ( var schedule in location.Schedules )
+                        {
+                            schedule.Selected = false;
+                        }
+                    }
+                }
+            }
+            if ( !alreadySelected )
+            {
+                chGroup.Selected = true;
+                chLocation.Selected = true;
+                chSchedule.Selected = true;
+                btnCheckin.Visible = true;
+            }
+            SaveState();
+            ShowPersonCheckin();
         }
 
         private void ShowPersonNotFound()
@@ -127,27 +236,45 @@ namespace RockWeb.Plugins.org_secc.SportsAndFitness
 
         }
 
-        private Rock.Model.Group GetMembershipGroup( CheckInPerson person, Rock.Model.Group group = null )
+        private Rock.Model.Group GetMembershipGroup()
         {
             string membershipGroupGuid = null;
-
-            if ( group != null )
-            {
-                group.LoadAttributes();
-                membershipGroupGuid = group.GetAttributeValue( "Group" );
-            }
-
-
+            membershipGroupGuid = GetAttributeValue( "GroupFitnessGroup" );
             if ( string.IsNullOrWhiteSpace( membershipGroupGuid ) )
             {
-                membershipGroupGuid = GetAttributeValue( "Group Fitness Group" );
-                if ( string.IsNullOrWhiteSpace( membershipGroupGuid ) )
-                {
-                    return null;
-                }
+                return null;
             }
             var membershipGroup = new GroupService( _rockContext ).Get( membershipGroupGuid.AsGuid() );
             return membershipGroup;
+        }
+
+        protected void btnCancel_Click( object sender, EventArgs e )
+        {
+            tbPhone.Text = "";
+            CurrentCheckInState.CheckIn = new CheckInStatus();
+            SaveState();
+            pnlDone.Visible = false;
+            pnlCheckin.Visible = false;
+            pnlNotFound.Visible = false;
+            pnlSearch.Visible = true;
+        }
+
+        protected void btnCheckin_Click( object sender, EventArgs e )
+        {
+            var checkinAction = GetAttributeValue( "CheckinActivity" );
+            List<string> Errors;
+            ProcessActivity( checkinAction, out Errors );
+            ProcessLabels();
+            pnlCheckin.Visible = false;
+            pnlDone.Visible = true;
+        }
+        private void ProcessLabels()
+        {
+            LabelPrinter labelPrinter = new LabelPrinter( CurrentCheckInState, Request );
+            labelPrinter.PrintNetworkLabels();
+            var script = labelPrinter.GetClientScript();
+            script += "setTimeout( function(){ __doPostBack( '" + btnDone.UniqueID + "', 'OnClick' ); },6000)";
+            ScriptManager.RegisterStartupScript( upContent, upContent.GetType(), "addLabelScript", script, true );
         }
     }
 }
