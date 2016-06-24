6a1f838d6c4b890a0791e7fcab08bd6f3079bad4
cbump <cbump@secc.org>
2016-06-24 10:55:00 -0400

    ROCK-98 - Adding new Payment Frequencies.  - Adding a new custom SECC PayFloPro gateway which adds the following payment frequencies:  * Every 4 weeks  * Quarterly  * Semi-Annually  * Yearly

---
+++ Plugins/org.secc.PayFlowPro/Migrations/001_Create4WeekFrequency.cs
@@ -0,0 +1,24 @@
+﻿using Rock.Plugin;
+
+namespace org.secc.PayFloPro.Migrations
+{
+    [MigrationNumber( 1, "1.0.0" )]
+    class CreateCurrencyType : Migration
+    {
+        /// <summary>
+        /// The commands to run to migrate plugin to the specific version
+        /// </summary>
+        public override void Up()
+        {
+            RockMigrationHelper.AddDefinedValue( Rock.SystemGuid.DefinedType.FINANCIAL_FREQUENCY, "Every 4 Weeks", "Every 4 Weeks", org.secc.PayFlowPro.Gateway.TRANSACTION_FREQUENCY_FOUR_WEEKS );
+        }
+
+        /// <summary>
+        /// The commands to undo a migration from a specific version
+        /// </summary>
+        public override void Down()
+        {
+            RockMigrationHelper.DeleteDefinedValue( org.secc.PayFlowPro.Gateway.TRANSACTION_FREQUENCY_FOUR_WEEKS );
+        }
+    }
+}
