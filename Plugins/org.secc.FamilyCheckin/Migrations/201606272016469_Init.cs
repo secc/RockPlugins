66be3f3217161e321e069609a4c0e3393af79453
mleesecc <mlee@secc.org>
2016-06-27 19:50:12 -0400

    ROCK-133 Added KioskType and Kiosk entities

---
+++ Plugins/org.secc.FamilyCheckin/Migrations/201606272016469_Init.cs
@@ -0,0 +1,157 @@
+namespace org.secc.FamilyCheckin.Migrations
+{
+    using Rock.Plugin;
+
+    [MigrationNumber( 1, "1.4.0" )]
+    public partial class Init : Migration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo._org_secc_FamilyCheckin_KioskType",
+                c => new
+                {
+                    Id = c.Int( nullable: false, identity: true ),
+                    Name = c.String( maxLength: 255 ),
+                    Description = c.String(),
+                    CheckinTemplateId = c.Int(),
+                    CreatedDateTime = c.DateTime(),
+                    ModifiedDateTime = c.DateTime(),
+                    CreatedByPersonAliasId = c.Int(),
+                    ModifiedByPersonAliasId = c.Int(),
+                    Guid = c.Guid( nullable: false ),
+                    ForeignId = c.Int(),
+                    ForeignGuid = c.Guid(),
+                    ForeignKey = c.String( maxLength: 100 ),
+                } )
+                .PrimaryKey( t => t.Id )
+                .ForeignKey( "dbo.GroupType", t => t.CheckinTemplateId )
+                .ForeignKey( "dbo.PersonAlias", t => t.CreatedByPersonAliasId )
+                .ForeignKey( "dbo.PersonAlias", t => t.ModifiedByPersonAliasId )
+                .Index( t => t.CheckinTemplateId )
+                .Index( t => t.CreatedByPersonAliasId )
+                .Index( t => t.ModifiedByPersonAliasId )
+                .Index( t => t.Guid, unique: true )
+                .Index( t => t.ForeignId )
+                .Index( t => t.ForeignGuid )
+                .Index( t => t.ForeignKey );
+
+            CreateTable(
+                "dbo._org_secc_FamilyCheckin_KioskTypeGroupType",
+                c => new
+                {
+                    KioskTypeId = c.Int( nullable: false ),
+                    GroupTypeId = c.Int( nullable: false ),
+                } )
+                .PrimaryKey( t => new { t.KioskTypeId, t.GroupTypeId } )
+                .ForeignKey( "dbo._org_secc_FamilyCheckin_KioskType", t => t.KioskTypeId, cascadeDelete: true )
+                .ForeignKey( "dbo.GroupType", t => t.GroupTypeId, cascadeDelete: true )
+                .Index( t => t.KioskTypeId )
+                .Index( t => t.GroupTypeId );
+
+            CreateTable(
+                "dbo._org_secc_FamilyCheckin_KioskTypeLocation",
+                c => new
+                {
+                    KioskTypeId = c.Int( nullable: false ),
+                    LocationId = c.Int( nullable: false ),
+                } )
+                .PrimaryKey( t => new { t.KioskTypeId, t.LocationId } )
+                .ForeignKey( "dbo._org_secc_FamilyCheckin_KioskType", t => t.KioskTypeId, cascadeDelete: true )
+                .ForeignKey( "dbo.Location", t => t.LocationId, cascadeDelete: true )
+                .Index( t => t.KioskTypeId )
+                .Index( t => t.LocationId );
+
+            CreateTable(
+                "dbo._org_secc_FamilyCheckin_KioskTypeSchedule",
+                c => new
+                {
+                    KioskTypeId = c.Int( nullable: false ),
+                    ScheduleId = c.Int( nullable: false ),
+                } )
+                .PrimaryKey( t => new { t.KioskTypeId, t.ScheduleId } )
+                .ForeignKey( "dbo._org_secc_FamilyCheckin_KioskType", t => t.KioskTypeId, cascadeDelete: true )
+                .ForeignKey( "dbo.Schedule", t => t.ScheduleId, cascadeDelete: true )
+                .Index( t => t.KioskTypeId )
+                .Index( t => t.ScheduleId );
+
+            CreateTable(
+                "dbo._org_secc_FamilyCheckin_Kiosk",
+                c => new
+                {
+                    Id = c.Int( nullable: false, identity: true ),
+                    Name = c.String( maxLength: 255 ),
+                    Description = c.String(),
+                    KioskTypeId = c.Int(),
+                    IPAddress = c.String( maxLength: 45 ),
+                    PrinterDeviceId = c.Int(),
+                    PrintFrom = c.Int( nullable: false ),
+                    PrintToOverride = c.Int( nullable: false ),
+                    CreatedDateTime = c.DateTime(),
+                    ModifiedDateTime = c.DateTime(),
+                    CreatedByPersonAliasId = c.Int(),
+                    ModifiedByPersonAliasId = c.Int(),
+                    Guid = c.Guid( nullable: false ),
+                    ForeignId = c.Int(),
+                    ForeignGuid = c.Guid(),
+                    ForeignKey = c.String( maxLength: 100 ),
+                } )
+                .PrimaryKey( t => t.Id )
+                .ForeignKey( "dbo.PersonAlias", t => t.CreatedByPersonAliasId )
+                .ForeignKey( "dbo._org_secc_FamilyCheckin_KioskType", t => t.KioskTypeId )
+                .ForeignKey( "dbo.PersonAlias", t => t.ModifiedByPersonAliasId )
+                .ForeignKey( "dbo.Device", t => t.PrinterDeviceId )
+                .Index( t => t.KioskTypeId )
+                .Index( t => t.PrinterDeviceId )
+                .Index( t => t.CreatedByPersonAliasId )
+                .Index( t => t.ModifiedByPersonAliasId )
+                .Index( t => t.Guid, unique: true )
+                .Index( t => t.ForeignId )
+                .Index( t => t.ForeignGuid )
+                .Index( t => t.ForeignKey );
+        }
+        public override void Down()
+        {
+
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_Kiosk", "PrinterDeviceId", "dbo.Device" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_Kiosk", "ModifiedByPersonAliasId", "dbo.PersonAlias" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_Kiosk", "KioskTypeId", "dbo._org_secc_FamilyCheckin_KioskType" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_KioskTypeSchedule", "ScheduleId", "dbo.Schedule" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_KioskTypeSchedule", "KioskTypeId", "dbo._org_secc_FamilyCheckin_KioskType" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_KioskType", "ModifiedByPersonAliasId", "dbo.PersonAlias" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_KioskTypeLocation", "LocationId", "dbo.Location" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_KioskTypeLocation", "KioskTypeId", "dbo._org_secc_FamilyCheckin_KioskType" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_KioskTypeGroupType", "GroupTypeId", "dbo.GroupType" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_KioskTypeGroupType", "KioskTypeId", "dbo._org_secc_FamilyCheckin_KioskType" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_KioskType", "CreatedByPersonAliasId", "dbo.PersonAlias" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_KioskType", "CheckinTemplateId", "dbo.GroupType" );
+            DropForeignKey( "dbo._org_secc_FamilyCheckin_Kiosk", "CreatedByPersonAliasId", "dbo.PersonAlias" );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskTypeSchedule", new[] { "ScheduleId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskTypeSchedule", new[] { "KioskTypeId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskTypeLocation", new[] { "LocationId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskTypeLocation", new[] { "KioskTypeId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskTypeGroupType", new[] { "GroupTypeId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskTypeGroupType", new[] { "KioskTypeId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskType", new[] { "ForeignKey" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskType", new[] { "ForeignGuid" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskType", new[] { "ForeignId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskType", new[] { "Guid" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskType", new[] { "ModifiedByPersonAliasId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskType", new[] { "CreatedByPersonAliasId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_KioskType", new[] { "CheckinTemplateId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_Kiosk", new[] { "ForeignKey" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_Kiosk", new[] { "ForeignGuid" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_Kiosk", new[] { "ForeignId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_Kiosk", new[] { "Guid" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_Kiosk", new[] { "ModifiedByPersonAliasId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_Kiosk", new[] { "CreatedByPersonAliasId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_Kiosk", new[] { "PrinterDeviceId" } );
+            DropIndex( "dbo._org_secc_FamilyCheckin_Kiosk", new[] { "KioskTypeId" } );
+            DropTable( "dbo._org_secc_FamilyCheckin_KioskTypeSchedule" );
+            DropTable( "dbo._org_secc_FamilyCheckin_KioskTypeLocation" );
+            DropTable( "dbo._org_secc_FamilyCheckin_KioskTypeGroupType" );
+            DropTable( "dbo._org_secc_FamilyCheckin_KioskType" );
+            DropTable( "dbo._org_secc_FamilyCheckin_Kiosk" );
+        }
+    }
+}
---
+++ Plugins/org.secc.FamilyCheckin/Model/Kiosk.cs
@@ -0,0 +1,64 @@
+namespace org.secc.FamilyCheckin.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.ModelConfiguration;
+    using System.Runtime.Serialization;
+    using Rock.Model;
+    [Table( "_org_secc_FamilyCheckin_Kiosk" )]
+    public partial class Kiosk : Rock.Data.Model<Kiosk>, Rock.Security.ISecured
+    {
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        [StringLength( 255 )]
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public int? KioskTypeId { get; set; }
+
+        public virtual KioskType KioskType { get; set; }
+
+        [MaxLength( 45 )]
+        [DataMember]
+        public string IPAddress { get; set; }
+
+        [DataMember]
+        public int? PrinterDeviceId { get; set; }
+
+        public virtual Device PrinterDevice { get; set; }
+
+        [DataMember]
+        public PrintFrom PrintFrom { get; set; }
+
+        [DataMember]
+        public PrintTo PrintToOverride { get; set; }
+
+
+    }
+
+    /// <summary>
+    /// File Configuration class.
+    /// </summary>
+    public partial class KioskConfiguration : EntityTypeConfiguration<Kiosk>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KioskTypeConfiguration"/> class.
+        /// </summary>
+        public KioskConfiguration()
+        {
+            this.HasOptional( k => k.KioskType ).WithMany().HasForeignKey( k => k.KioskTypeId ).WillCascadeOnDelete( false );
+            this.HasOptional( k => k.PrinterDevice ).WithMany().HasForeignKey( k => k.PrinterDeviceId ).WillCascadeOnDelete( false );
+
+        }
+    }
+}
