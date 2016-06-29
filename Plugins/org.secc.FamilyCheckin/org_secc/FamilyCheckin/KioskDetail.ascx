9643a344aef8c0e274199abbb611d2f84bf4083d
mleesecc <mlee@secc.org>
2016-06-29 10:40:56 -0400

    ROCK-133 Init kiosk configuration

---
+++ Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/KioskDetail.ascx
@@ -0,0 +1,64 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="KioskDetail.ascx.cs" Inherits="RockWeb.Plugins.org_secc.FamilyCheckin.KioskDetail" %>
+
+<asp:UpdatePanel ID="upnlDevice" runat="server">
+    <ContentTemplate>
+        <asp:Panel ID="pnlDetails" CssClass="panel panel-block" runat="server" Visible="false">
+
+            <asp:HiddenField ID="hfKioskId" runat="server" />
+
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-power-off"></i>
+                    <asp:Literal ID="lActionTitle" runat="server" /></h1>
+            </div>
+            <div class="panel-body">
+
+                <asp:ValidationSummary ID="ValidationSummary1" runat="server" HeaderText="Please Correct the Following" CssClass="alert alert-danger" />
+
+                <fieldset>
+
+                    <Rock:NotificationBox ID="nbEditModeMessage" runat="server" NotificationBoxType="Info" />
+                    <Rock:NotificationBox ID="nbDuplicateDevice" runat="server" NotificationBoxType="Warning" Title="Sorry" Visible="false" />
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <Rock:DataTextBox ID="tbName" runat="server" SourceTypeName="org.secc.FamilyCheckin.Model.Kiosk, org.secc.FamilyCheckin" PropertyName="Name" Required="true" />
+                        </div>
+                        <div class="col-md-6">
+                            <Rock:DataTextBox ID="tbIPAddress" runat="server" SourceTypeName="org.secc.FamilyCheckin.Model.Kiosk, org.secc.FamilyCheckin" PropertyName="IPAddress" Required="true" />
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:DataTextBox CausesValidation="false" ID="tbDescription" runat="server" SourceTypeName="org.secc.FamilyCheckin.Model.Kiosk, org.secc.FamilyCheckin" PropertyName="Description" TextMode="MultiLine" Rows="3" />
+                        </div>
+                    </div>
+
+                    <Rock:DataDropDownList CausesValidation="false" runat="server" ID="ddlKioskType" Label="Kiosk Type" DataTextField="Name" DataValueField="Id" SourceTypeName="org.secc.FamilyCheckin.Model.Kiosk, org.secc.FamilyCheckin" PropertyName="KioskTypeId"></Rock:DataDropDownList>
+
+                    <h4>Print Settings</h4>
+                    <Rock:RockDropDownList ID="ddlPrintTo" runat="server" Label="Print Using" AutoPostBack="true" OnSelectedIndexChanged="ddlPrintTo_SelectedIndexChanged"
+                        Help="When this device needs to print, should it use the printer configured in next setting (Device Printer), the printer configured for the location (Location Printer), or should the Group Type's 'Print Using' setting determine the printer to use (Group Type)?">
+                        <asp:ListItem Text="Device Printer" Value="1" />
+                        <asp:ListItem Text="Location Printer" Value="2" />
+                        <asp:ListItem Text="Group Type" Value="0" />
+                    </Rock:RockDropDownList>
+                    <Rock:RockDropDownList ID="ddlPrinter" runat="server" Label="Printer" DataTextField="Name" DataValueField="Id"
+                        Help="The printer that this device should use for printing" />
+                    <Rock:RockDropDownList ID="ddlPrintFrom" runat="server" Label="Print From" Required="false"
+                        Help="When this device needs to print, where should the printing be initiated from?  Either the server running Rock, or from the actual client device? " />
+
+
+                </fieldset>
+
+                <div class="actions">
+                    <asp:LinkButton ID="btnSave" runat="server" AccessKey="s" Text="Save" CssClass="btn btn-primary" OnClick="btnSave_Click" />
+                    <asp:LinkButton ID="btnCancel" runat="server" AccessKey="c" Text="Cancel" CssClass="btn btn-link" CausesValidation="false" OnClick="btnCancel_Click" />
+                </div>
+
+            </div>
+
+        </asp:Panel>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/KioskList.ascx.cs
@@ -0,0 +1,147 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using org.secc.FamilyCheckin.Data;
+using org.secc.FamilyCheckin.Model;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.org_secc.FamilyCheckin
+{
+    [DisplayName( "Kiosk List" )]
+    [Category( "SECC > Check-in" )]
+    [Description( "Lists all the kiosks." )]
+
+    [LinkedPage("Detail Page")]
+    public partial class KioskList : RockBlock
+    { 
+        #region Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            
+            gKiosks.DataKeyNames = new string[] { "Id" };
+            gKiosks.Actions.ShowAdd = true;
+            gKiosks.Actions.AddClick += gKiosk_Add;
+            gKiosks.GridRebind += gKiosk_GridRebind;
+
+            // Block Security and special attributes (RockPage takes care of View)
+            bool canAddEditDelete = IsUserAuthorized( Authorization.EDIT );
+            gKiosks.Actions.ShowAdd = canAddEditDelete;
+            gKiosks.IsDeleteEnabled = canAddEditDelete;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            if ( !Page.IsPostBack )
+            {
+                BindGrid();
+            }
+
+            base.OnLoad( e );
+        }
+
+        #endregion
+
+        #region Grid Events (main grid)
+
+        protected void gKiosk_Add( object sender, EventArgs e )
+        {
+            NavigateToLinkedPage( "DetailPage", "KioskId", 0 );
+        }
+
+
+        protected void gKiosk_Edit( object sender, RowEventArgs e )
+        {
+            NavigateToLinkedPage( "DetailPage", "KioskId", e.RowKeyId );
+        }
+
+       
+        protected void gKiosk_Delete( object sender, RowEventArgs e )
+        {
+            var checkinContext = new FamilyCheckinContext();
+            KioskService KioskService = new KioskService( checkinContext );
+            Kiosk Kiosk = KioskService.Get( e.RowKeyId );
+
+            if ( Kiosk != null )
+            {
+                int kioskId = Kiosk.Id;
+
+                string errorMessage;
+                if ( !KioskService.CanDelete( Kiosk, out errorMessage ) )
+                {
+                    mdGridWarning.Show( errorMessage, ModalAlertType.Information );
+                    return;
+                }
+
+                KioskService.Delete( Kiosk );
+                checkinContext.SaveChanges();
+
+                Rock.CheckIn.KioskDevice.Flush( kioskId );
+            }
+
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Handles the GridRebind event of the gDevice control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        private void gKiosk_GridRebind( object sender, EventArgs e )
+        {
+            BindGrid();
+        }
+
+        #endregion
+
+        #region Internal Methods
+        /// <summary>
+        /// Binds the grid.
+        /// </summary>
+        private void BindGrid()
+        {
+            var kioskService = new KioskService( new FamilyCheckinContext() );
+            var kiosks = kioskService.Queryable().Select( kt => kt ).ToList();
+
+            gKiosks.DataSource = kiosks;
+            gKiosks.DataBind();
+        }
+
+        #endregion
+    }
+}
