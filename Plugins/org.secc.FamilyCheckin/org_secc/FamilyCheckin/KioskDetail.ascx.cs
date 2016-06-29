5c7daeb5d78b557497177955231520b99a656b26
mleesecc <mlee@secc.org>
2016-06-29 10:40:56 -0400

    ROCK-133 Init kiosk configuration

---
+++ Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/KioskDetail.ascx.cs
@@ -0,0 +1,244 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rock;
+using Rock.Attribute;
+using Rock.Constants;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using org.secc.FamilyCheckin.Model;
+using org.secc.FamilyCheckin.Data;
+using System.Data.Entity;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.org_secc.FamilyCheckin
+{
+    [DisplayName( "Kiosk Detail" )]
+    [Category( "SECC > Check-in" )]
+    [Description( "Displays the details of the given device." )]
+    public partial class KioskDetail : RockBlock, IDetailBlock
+    {
+        #region Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.AddConfigurationUpdateTrigger( upnlDevice );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            nbDuplicateDevice.Visible = false;
+
+            if ( !Page.IsPostBack )
+            {
+                ShowDetail( PageParameter( "KioskId" ).AsInteger() );
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+
+        protected void btnSave_Click( object sender, EventArgs e )
+        {
+            Kiosk Kiosk = null;
+
+            var rockContext = new RockContext();
+            var checkinContext = new FamilyCheckinContext();
+            var kioskService = new KioskService( checkinContext );
+            var attributeService = new AttributeService( rockContext );
+
+            int KioskId = int.Parse( hfKioskId.Value );
+
+            if ( KioskId != 0 )
+            {
+                Kiosk = kioskService.Get( KioskId );
+            }
+
+            if ( Kiosk == null )
+            {
+                Kiosk = new Kiosk();
+                kioskService.Add( Kiosk );
+
+            }
+
+            if ( Kiosk != null )
+            {
+                Kiosk.Name = tbName.Text;
+                Kiosk.Description = tbDescription.Text;
+                Kiosk.IPAddress = tbIPAddress.Text;
+
+                if ( !Kiosk.IsValid || !Page.IsValid )
+                {
+                    // Controls will render the error messages
+                    return;
+                }
+
+                //Save kiosk's checkin type
+                Kiosk.KioskTypeId = ddlKioskType.SelectedValue.AsInteger();
+
+                checkinContext.SaveChanges();
+
+                //Rock.CheckIn.KioskDevice.Flush( KioskType.Id );
+
+                NavigateToParentPage();
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnCancel control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        protected void btnCancel_Click( object sender, EventArgs e )
+        {
+            NavigateToParentPage();
+        }
+
+        #endregion
+
+        #region Methods
+
+
+        /// <summary>
+        /// Shows the edit.
+        /// </summary>
+        /// <param name="DeviceId">The device identifier.</param>
+        public void ShowDetail( int KioskId )
+        {
+            pnlDetails.Visible = true;
+            Kiosk Kiosk = null;
+
+            var checkinContext = new FamilyCheckinContext();
+
+            if ( !KioskId.Equals( 0 ) )
+            {
+                Kiosk = new KioskService( checkinContext ).Get( KioskId );
+                lActionTitle.Text = ActionTitle.Edit( Kiosk.FriendlyTypeName ).FormatAsHtmlTitle();
+            }
+
+            if ( Kiosk == null )
+            {
+                Kiosk = new Kiosk { Id = 0 };
+                lActionTitle.Text = ActionTitle.Add( Kiosk.FriendlyTypeName ).FormatAsHtmlTitle();
+            }
+
+            hfKioskId.Value = Kiosk.Id.ToString();
+
+            tbName.Text = Kiosk.Name;
+            tbDescription.Text = Kiosk.Description;
+            tbIPAddress.Text = Kiosk.IPAddress;
+
+            // render UI based on Authorized and IsSystem
+            bool readOnly = false;
+
+            nbEditModeMessage.Text = string.Empty;
+            if ( !IsUserAuthorized( Authorization.EDIT ) )
+            {
+                readOnly = true;
+                nbEditModeMessage.Text = EditModeMessage.ReadOnlyEditActionNotAllowed( Kiosk.FriendlyTypeName );
+            }
+
+            if ( readOnly )
+            {
+                lActionTitle.Text = ActionTitle.View( Kiosk.FriendlyTypeName );
+                btnCancel.Text = "Close";
+            }
+
+            tbName.ReadOnly = readOnly;
+            tbDescription.ReadOnly = readOnly;
+
+            btnSave.Visible = !readOnly;
+            BindDropDownList( Kiosk );
+        }
+
+        private void BindDropDownList( Kiosk kiosk = null )
+        {
+            FamilyCheckinContext checkinContext = new FamilyCheckinContext();
+            KioskTypeService kioskTypeService = new KioskTypeService( checkinContext );
+            
+           
+            ddlKioskType.DataSource = kioskTypeService
+                .Queryable()
+                .OrderBy( t => t.Name )
+                .Select( t => new
+                {
+                    t.Name,
+                    t.Id
+                } )
+                .ToList();
+            ddlKioskType.DataBind();
+            if ( kiosk != null )
+            {
+                ddlKioskType.SetValue( kiosk.KioskTypeId );
+            }
+
+            ddlPrintFrom.BindToEnum<PrintFrom>();
+
+            ddlPrinter.Items.Clear();
+            ddlPrinter.DataSource = new DeviceService( new RockContext() )
+                .GetByDeviceTypeGuid( new Guid( Rock.SystemGuid.DefinedValue.DEVICE_TYPE_PRINTER ) )
+                .OrderBy( d => d.Name )
+                .ToList();
+            ddlPrinter.DataBind();
+            ddlPrinter.Items.Insert( 0, new ListItem( None.Text, None.IdValue ) );
+        }
+        #endregion
+
+        /// <summary>
+        /// Handles when the Print To selection is changed.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void ddlPrintTo_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            SetPrinterVisibility();
+        }
+
+
+        /// <summary>
+        /// Decide if the printer drop down list should be hidden.
+        /// </summary>
+        private void SetPrinterVisibility()
+        {
+            var printTo = ( PrintTo ) System.Enum.Parse( typeof( PrintTo ), ddlPrintTo.SelectedValue );
+            ddlPrinter.Visible = printTo != PrintTo.Location;
+        }
+    }
+}
---
+++ Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/KioskList.ascx
@@ -0,0 +1,27 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="KioskList.ascx.cs" Inherits="RockWeb.Plugins.org_secc.FamilyCheckin.KioskList" %>
+
+<asp:UpdatePanel ID="upDevice" runat="server">
+    <ContentTemplate>
+
+        <Rock:ModalAlert ID="mdGridWarning" runat="server" />
+        
+        <div class="panel panel-block">
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-power-off"></i> Kiosk List</h1>
+            </div>
+            <div class="panel-body">
+
+                <div class="grid grid-panel">
+                    <Rock:Grid ID="gKiosks" runat="server" AllowSorting="true" OnRowSelected="gKiosk_Edit">
+                        <Columns>
+                            <Rock:RockBoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                            <Rock:DeleteField OnClick="gKiosk_Delete" />
+                        </Columns>
+                    </Rock:Grid>
+                </div>
+
+            </div>
+        </div>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
