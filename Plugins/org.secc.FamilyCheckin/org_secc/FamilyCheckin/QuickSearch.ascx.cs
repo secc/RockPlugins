9c9bc0d176b557196e2ddcb13b34f70b8d0f02c8
mleesecc <mlee@secc.org>
2016-06-30 16:32:34 -0400

    ROCK-133 Updated family checkin to be compatible with KioskTypes

--- Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/QuickSearch.ascx.cs
+++ Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/QuickSearch.ascx.cs
@@ -10,71 +10,75 @@ using Rock.CheckIn;
 using Rock.Web.UI.Controls;
 using Rock.Web.Cache;
 using System.Web.UI;
+using org.secc.FamilyCheckin.Model;
+using Rock.Data;
 
 namespace RockWeb.Plugins.org_secc.FamilyCheckin
 {
-    [DisplayName("QuickSearch")]
-    [Category("SECC > Check-in")]
-    [Description("QuickSearch block for helping parents find their family quickly.")]
-    [IntegerField("Minimum Phone Number Length", "Minimum length for phone number searches (defaults to 4).", false, 4)]
-    [IntegerField("Maximum Phone Number Length", "Maximum length for phone number searches (defaults to 10).", false, 10)]
-    [IntegerField("Refresh Interval", "How often (seconds) should page automatically query server for new Check-in data", false, 10)]
-    [TextField("Search Regex", "Regular Expression to run the search input through before sending it to the workflow. Useful for stripping off characters.", false)]
-    [DefinedValueField(Rock.SystemGuid.DefinedType.CHECKIN_SEARCH_TYPE, "Search Type", "The type of search to use for check-in (default is phone number).", true, false, Rock.SystemGuid.DefinedValue.CHECKIN_SEARCH_TYPE_PHONE_NUMBER, order: 4)]
-    [CodeEditorField("Default Content", "Default content to display", CodeEditorMode.Html, CodeEditorTheme.Rock, 200, true, "", "",12)]
+    [DisplayName( "QuickSearch" )]
+    [Category( "SECC > Check-in" )]
+    [Description( "QuickSearch block for helping parents find their family quickly." )]
+    [IntegerField( "Minimum Phone Number Length", "Minimum length for phone number searches (defaults to 4).", false, 4 )]
+    [IntegerField( "Maximum Phone Number Length", "Maximum length for phone number searches (defaults to 10).", false, 10 )]
+    [IntegerField( "Refresh Interval", "How often (seconds) should page automatically query server for new Check-in data", false, 10 )]
+    [TextField( "Search Regex", "Regular Expression to run the search input through before sending it to the workflow. Useful for stripping off characters.", false )]
+    [DefinedValueField( Rock.SystemGuid.DefinedType.CHECKIN_SEARCH_TYPE, "Search Type", "The type of search to use for check-in (default is phone number).", true, false, Rock.SystemGuid.DefinedValue.CHECKIN_SEARCH_TYPE_PHONE_NUMBER, order: 4 )]
+    [CodeEditorField( "Default Content", "Default content to display", CodeEditorMode.Html, CodeEditorTheme.Rock, 200, true, "", "", 12 )]
     public partial class QuickSearch : CheckInBlock
     {
 
         protected int minLength;
         protected int maxLength;
+        protected KioskType KioskType;
 
 
-
-        protected override void OnInit(EventArgs e)
+        protected override void OnInit( EventArgs e )
         {
 
-            base.OnInit(e);
+            base.OnInit( e );
 
-            if (CurrentCheckInState == null)
+            if ( CurrentCheckInState == null )
             {
                 NavigateToPreviousPage();
                 return;
             }
 
-            RockPage.AddScriptLink("~/scripts/jquery.plugin.min.js");
-            RockPage.AddScriptLink("~/scripts/jquery.countdown.min.js");
+            if ( Session["KioskTypeId"] != null )
+            {
+                int kioskTypeId = ( int ) Session["KioskTypeId"];
+                KioskType = new KioskTypeService( new RockContext() ).Get( kioskTypeId );
+                if ( KioskType == null )
+                {
+                    NavigateToPreviousPage();
+                    return;
+                }
+            }
+            else
+            {
+                NavigateToPreviousPage();
+                return;
+            }
+
+            RockPage.AddScriptLink( "~/scripts/jquery.plugin.min.js" );
+            RockPage.AddScriptLink( "~/scripts/jquery.countdown.min.js" );
 
             RegisterScript();
         }
 
-        protected override void OnLoad(EventArgs e)
+        protected override void OnLoad( EventArgs e )
         {
-            base.OnLoad(e);
+            base.OnLoad( e );
 
-            minLength = int.Parse(GetAttributeValue("MinimumPhoneNumberLength"));
-            maxLength = int.Parse(GetAttributeValue("MaximumPhoneNumberLength"));
+            minLength = int.Parse( GetAttributeValue( "MinimumPhoneNumberLength" ) );
+            maxLength = int.Parse( GetAttributeValue( "MaximumPhoneNumberLength" ) );
 
-            if (Request["__EVENTTARGET"] == "ChooseFamily")
+            if ( Request["__EVENTTARGET"] == "ChooseFamily" )
             {
-                ChooseFamily(Request["__EVENTARGUMENT"]);
+                ChooseFamily( Request["__EVENTARGUMENT"] );
             }
 
-            if (!Page.IsPostBack && CurrentCheckInState != null)
+            if ( !Page.IsPostBack && CurrentCheckInState != null )
             {
-                string script = string.Format(@"
-    <script>
-        $(document).ready(function (e) {{
-            if (localStorage) {{
-                localStorage.theme = '{0}'
-                localStorage.checkInKiosk = '{1}';
-                localStorage.checkInGroupTypes = '{2}';
-            }}
-        }});
-    </script>
-", CurrentTheme, CurrentKioskId, CurrentGroupTypeIds.AsDelimited(","));
-                    phScript.Controls.Add(new LiteralControl(script));
-
-
                 CurrentWorkflow = null;
                 CurrentCheckInState.CheckIn = new CheckInStatus();
                 SaveState();
@@ -84,20 +88,20 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
             }
         }
 
-        private void ChooseFamily(string familyIdAsString)
+        private void ChooseFamily( string familyIdAsString )
         {
-            int familyId = Int32.Parse(familyIdAsString);
-            CurrentCheckInState = (CheckInState)Session["CheckInState"];
+            int familyId = Int32.Parse( familyIdAsString );
+            CurrentCheckInState = ( CheckInState ) Session["CheckInState"];
             ClearSelection();
-            CheckInFamily selectedFamily = CurrentCheckInState.CheckIn.Families.FirstOrDefault(f => f.Group.Id == familyId);
-            if (selectedFamily != null)
+            CheckInFamily selectedFamily = CurrentCheckInState.CheckIn.Families.FirstOrDefault( f => f.Group.Id == familyId );
+            if ( selectedFamily != null )
             {
                 try
                 {
                     //clear QCPeople session object and get it ready for quick checkin.
-                    Session.Remove("qcPeople");
+                    Session.Remove( "qcPeople" );
                 }
-                catch { } 
+                catch { }
                 selectedFamily.Selected = true;
                 SaveState();
                 NavigateToNextPage();
@@ -107,7 +111,7 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
 
         private void ClearSelection()
         {
-            foreach (var family in CurrentCheckInState.CheckIn.Families)
+            foreach ( var family in CurrentCheckInState.CheckIn.Families )
             {
                 family.Selected = false;
                 family.People = new List<CheckInPerson>();
@@ -119,7 +123,7 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
         /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected void lbRefresh_Click(object sender, EventArgs e)
+        protected void lbRefresh_Click( object sender, EventArgs e )
         {
             RefreshView();
         }
@@ -130,7 +134,7 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
         /// </summary>
         private void RefreshView()
         {
-            hfRefreshTimerSeconds.Value = GetAttributeValue("RefreshInterval");
+            hfRefreshTimerSeconds.Value = GetAttributeValue( "RefreshInterval" );
             pnlNotActive.Visible = false;
             pnlNotActiveYet.Visible = false;
             pnlClosed.Visible = false;
@@ -138,25 +142,41 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
             ManagerLoggedIn = false;
             lblActiveWhen.Text = string.Empty;
 
-            if (CurrentCheckInState == null)
+            if ( CurrentCheckInState == null )
             {
                 NavigateToPreviousPage();
                 return;
             }
 
-            if (CurrentCheckInState.Kiosk.FilteredGroupTypes(CurrentCheckInState.ConfiguredGroupTypes).Count == 0)
+
+            if ( !KioskType.IsOpen() )
+            {
+                DateTime? activeAt = KioskType.GetNextOpen();
+                if ( activeAt == null )
+                {
+                    pnlNotActive.Visible = true;
+                    HideSign();
+                }
+                else
+                {
+                    lblActiveWhen.Text = ( activeAt ?? RockDateTime.Today.AddDays( 1 ) ).ToString( "o" );
+                    pnlNotActiveYet.Visible = true;
+                    HideSign();
+                }
+            }
+            else if ( CurrentCheckInState.Kiosk.FilteredGroupTypes( CurrentCheckInState.ConfiguredGroupTypes ).Count == 0 )
             {
                 pnlNotActive.Visible = true;
                 HideSign();
             }
-            else if (!CurrentCheckInState.Kiosk.HasLocations(CurrentCheckInState.ConfiguredGroupTypes))
+            else if ( !CurrentCheckInState.Kiosk.HasLocations( CurrentCheckInState.ConfiguredGroupTypes ) )
             {
-                DateTime activeAt = CurrentCheckInState.Kiosk.FilteredGroupTypes(CurrentCheckInState.ConfiguredGroupTypes).Select(g => g.NextActiveTime).Min();
-                lblActiveWhen.Text = activeAt.ToString("o");
+                DateTime activeAt = CurrentCheckInState.Kiosk.FilteredGroupTypes( CurrentCheckInState.ConfiguredGroupTypes ).Select( g => g.NextActiveTime ).Min();
+                lblActiveWhen.Text = activeAt.ToString( "o" );
                 pnlNotActiveYet.Visible = true;
                 HideSign();
             }
-            else if (!CurrentCheckInState.Kiosk.HasActiveLocations(CurrentCheckInState.ConfiguredGroupTypes))
+            else if ( !CurrentCheckInState.Kiosk.HasActiveLocations( CurrentCheckInState.ConfiguredGroupTypes ) )
             {
                 pnlClosed.Visible = true;
                 HideSign();
@@ -186,7 +206,7 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
             // Note: the OnExpiry property of the countdown jquery plugin seems to add a new callback
             // everytime the setting is set which is why the clearCountdown method is used to prevent 
             // a plethora of partial postbacks occurring when the countdown expires.
-            string script = string.Format(@"
+            string script = string.Format( @"
 
 var timeoutSeconds = $('.js-refresh-timer-seconds').val();
 if (timeout) {{
@@ -227,8 +247,8 @@ if ($ActiveWhen.text() != '')
     }});
 }}
 
-", this.Page.ClientScript.GetPostBackEventReference(lbRefresh, ""));
-            ScriptManager.RegisterStartupScript(Page, Page.GetType(), "RefreshScript", script, true);
+", this.Page.ClientScript.GetPostBackEventReference( lbRefresh, "" ) );
+            ScriptManager.RegisterStartupScript( Page, Page.GetType(), "RefreshScript", script, true );
         }
     }
 }
