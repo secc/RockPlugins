5f2140d25aae498664a9360fba2d133f07a07830
mleesecc <mlee@secc.org>
2016-10-13 11:53:06 -0400

    ROCK-124 ROCK-172 ROCK-374 Updates to sports and fitness as requested

--- Plugins/org.secc.SportsAndFitness/org_secc/SportsAndFitness/ActivitiesCheckin.ascx.cs
+++ Plugins/org.secc.SportsAndFitness/org_secc/SportsAndFitness/ActivitiesCheckin.ascx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text.RegularExpressions;
 using System.Linq;
 
 using Rock;
@@ -12,13 +11,7 @@ using Rock.Web.Cache;
 using System.Web.UI.WebControls;
 using System.Web.UI;
 using Rock.Model;
-using System.Reflection;
-using System.Web.UI.HtmlControls;
-using System.Text;
-using System.Net.Sockets;
-using System.Net;
 using Rock.Data;
-using org.secc.FamilyCheckin.Utilities;
 
 namespace RockWeb.Plugins.org_secc.SportsAndFitness
 {
@@ -67,10 +60,10 @@ namespace RockWeb.Plugins.org_secc.SportsAndFitness
                 {
                     bool test = ProcessActivity( workflowActivity, out errors );
                 }
-                catch
+                catch ( Exception ex )
                 {
+                    LogException( ex );
                     NavigateToPreviousPage();
-                    Response.End();
                     return;
                 }
 
@@ -114,7 +107,7 @@ namespace RockWeb.Plugins.org_secc.SportsAndFitness
                 return;
             }
             var family = CurrentCheckInState.CheckIn.Families.Where( f => f.Selected ).FirstOrDefault();
-            if ( family == null )
+            if ( family == null || !family.People.Where( p => p.GroupTypes.Where( gt => gt.Groups.Any() ).Any() ).Any() )
             {
                 DisplayNoEligibleMembers();
                 return;
@@ -274,20 +267,22 @@ namespace RockWeb.Plugins.org_secc.SportsAndFitness
                 notes.Add( note );
             }
 
+            //This line is commented out because sports and fitnes has complained that they
+            //are seeing notes they did not add
 
-            notes.AddRange( new NoteService( _rockContext ).Get( _noteTypeId, groupMember.Id ).ToList() );
+            //notes.AddRange( new NoteService( _rockContext ).Get( _noteTypeId, groupMember.Id ).ToList() );
 
             switch ( GroupMembershipExpired( groupMember ) )
             {
                 case GroupMembershipStatus.Expired:
                     Note eNote = new Note();
-                    eNote.Text = "Membership Expired";
+                    eNote.Text = "Card expired or no longer church member.";
                     eNote.IsAlert = true;
                     notes.Add( eNote );
                     break;
                 case GroupMembershipStatus.NearExpired:
                     Note neNote = new Note();
-                    neNote.Text = "Membership will expire soon.";
+                    neNote.Text = "Card will expire soon.";
                     neNote.IsAlert = false;
                     notes.Add( neNote );
                     break;
