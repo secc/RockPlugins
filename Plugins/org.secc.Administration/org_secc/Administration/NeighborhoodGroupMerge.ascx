9ede4f6f7b28918707ac1c27fc78800acca63126
cbump <cbump@secc.org>
2017-01-10 16:46:46 -0500

    ROCK-602 - Merge Neighborhood Groups

---
+++ Plugins/org.secc.Administration/org_secc/Administration/NeighborhoodGroupMerge.ascx
@@ -0,0 +1,10 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="NeighborhoodGroupMerge.ascx.cs" Inherits="RockWeb.Plugins.org_secc.Administration.NeighborhoodGroupMerge" ViewStateMode="Enabled" EnableViewState="true" %>
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+
+        <Rock:GroupPicker ID="gpSource" Label="Source Group" runat="server" />
+        <Rock:GroupPicker ID="gpTarget" Label="Target Group" runat="server" />
+        <Rock:BootstrapButton ID="btnMerge" Text="Merge Groups" CssClass="btn btn-primary" runat="server" OnClick="btnMerge_Click"></Rock:BootstrapButton>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Plugins/org.secc.Administration/org_secc/Administration/NeighborhoodGroupMerge.ascx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Newtonsoft.Json;
+using Rock.Web.UI;
+using System.Data.Entity;
+
+namespace RockWeb.Plugins.org_secc.Administration
+{
+
+    [DisplayName( "Neighborhood Group Merge" )]
+    [Category( "SECC > Administration" )]
+    [Description( "Tool to merge neighborhood group rosters, attendance, and update the cycles attribute." )]
+
+    public partial class NeighborhoodGroupMerge : RockBlock
+    {
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+        }
+
+        protected void btnMerge_Click( object sender, EventArgs e )
+        {
+
+            int? targetGroupId = gpTarget.SelectedValueAsInt();
+            int? sourceGroupId = gpSource.SelectedValueAsInt();
+
+            RockContext rockContext = new RockContext();
+            GroupService groupService = new GroupService( rockContext );
+            AttendanceService attendanceService = new AttendanceService( rockContext );
+
+            var destLWYA = groupService.Get( targetGroupId.Value );
+            
+            var sourceLWYA = groupService.Get( sourceGroupId.Value );
+            
+            //Add source members to target group as inactive
+            foreach ( var member in sourceLWYA.Members.Where( m => !destLWYA.Members.Select( dM => dM.PersonId ).Contains( m.PersonId ) ) )
+            {
+                destLWYA.Members.Add( new GroupMember()
+                {
+                    PersonId = member.PersonId,
+                    GroupId = destLWYA.Id,
+                    GroupRoleId = member.GroupRoleId,
+                    GroupMemberStatus = GroupMemberStatus.Inactive
+                } );
+            }
+            rockContext.SaveChanges();
+
+            //Switch Groups Ids on attendance
+            foreach ( var member in destLWYA.Members )
+            {
+                var attendances = attendanceService.Queryable()
+                    .Where( a => a.PersonAlias.PersonId == member.PersonId && a.GroupId == sourceLWYA.Id );
+                foreach ( var attendance in attendances )
+                {
+                    attendance.GroupId = destLWYA.Id;
+                }
+            }
+            rockContext.SaveChanges();
+
+            //set cycle attribute
+            AddCycle( destLWYA, sourceLWYA );
+        }
+        
+
+        private void CopyLocations( Group destLWYA, Group sourceLWYA )
+        {
+            foreach (var groupLocation in sourceLWYA.GroupLocations )
+            {
+                var newGroupLocation = groupLocation.Clone( false );
+                newGroupLocation.Id = 0;
+                newGroupLocation.Guid = Guid.NewGuid();
+                newGroupLocation.GroupId = destLWYA.Id;
+                destLWYA.GroupLocations.Add( newGroupLocation );
+            }
+        }
+
+        private void CopyAttributes( Group destLWYA, Group sourceLWYA )
+        {
+            sourceLWYA.LoadAttributes();
+            destLWYA.LoadAttributes();
+
+            foreach ( var att in sourceLWYA.Attributes.Select( a => a.Key ) )
+            {
+                destLWYA.SetAttributeValue( att, sourceLWYA.GetAttributeValue( att ) );
+            }
+
+            destLWYA.SaveAttributeValues();
+        }
+
+        private void AddCycle( Group destLWYA, Group sourceLWYA )
+        {
+            destLWYA.LoadAttributes();
+            sourceLWYA.LoadAttributes();
+            var newCycles = destLWYA.GetAttributeValue( "LWYACycle" ).Split( new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries ).ToList();
+            var oldCycles = sourceLWYA.GetAttributeValue( "LWYACycle" ).Split( new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries ).ToList();
+            foreach ( var value in oldCycles )
+            {
+                if ( !newCycles.Contains( value ) )
+                {
+                    newCycles.Add( value );
+                }
+            }
+            destLWYA.SetAttributeValue( "LWYACycle", string.Join( ",", newCycles ) );
+            destLWYA.SaveAttributeValues();
+        }
+    }
+}
