d9217095d1631242ceede17101d7d825e49e0aa3
mleesecc <mlee@secc.org>
2017-01-10 09:16:20 -0500

    ROCK-601 Added cacheing to filter by dataview

--- Plugins/org.secc.FamilyCheckin/Workflows/FilterGroupsByDataView.cs
+++ Plugins/org.secc.FamilyCheckin/Workflows/FilterGroupsByDataView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Runtime.Caching;
 using Rock;
 using Rock.Attribute;
 using Rock.Data;
@@ -20,7 +21,7 @@ namespace org.secc.FamilyCheckin
     [Description( "Removes or excludes checkin groups that require the member to be in a dataview" )]
     [Export( typeof( ActionComponent ) )]
     [ExportMetadata( "ComponentName", "Filter Groups By DataView" )]
-    [AttributeField( Rock.SystemGuid.EntityType.GROUP, "DataView Group Attribute", "Select the attribute used to filter by DataView.", true, false)]
+    [AttributeField( Rock.SystemGuid.EntityType.GROUP, "DataView Group Attribute", "Select the attribute used to filter by DataView.", true, false )]
     [BooleanField( "Remove", "Select 'Yes' if groups should be be removed.  Select 'No' if they should just be marked as excluded.", true, order: 5 )]
     public class FilterGroupsByDataView : CheckInActionComponent
     {
@@ -41,6 +42,8 @@ namespace org.secc.FamilyCheckin
                 return false;
             }
 
+            ObjectCache cache = Rock.Web.Cache.RockMemoryCache.Default;
+
             var dataViewAttributeKey = string.Empty;
             var dataViewAttributeGuid = GetAttributeValue( action, "DataViewGroupAttribute" ).AsGuid();
             if ( dataViewAttributeGuid != Guid.Empty )
@@ -53,7 +56,6 @@ namespace org.secc.FamilyCheckin
             var family = checkInState.CheckIn.CurrentFamily;
             if ( family != null )
             {
-                GroupMemberService groupMemberService = new GroupMemberService( rockContext );
                 var remove = GetAttributeValue( action, "Remove" ).AsBoolean();
 
                 foreach ( var person in family.People )
@@ -73,15 +75,30 @@ namespace org.secc.FamilyCheckin
                                 continue;
                             }
 
-                            var approvedPeople = dataViewService.Get( approvedPeopleGuid.AsGuid() );
+                            //Get approved people dataview from cache or from db
+                            var approvedPeopleList = cache[approvedPeopleGuid] as List<int>;
 
-                            if ( approvedPeople == null )
+                            if ( approvedPeopleList == null )
                             {
-                                continue;
+                                DataView approvedPeople = dataViewService.Get( approvedPeopleGuid.AsGuid() );
+
+                                if ( approvedPeople == null )
+                                {
+                                    continue;
+                                }
+
+                                var errors = new List<string>();
+                                var approvedPeopleQry = approvedPeople.GetQuery( null, 30, out errors );
+                                if ( approvedPeopleQry != null )
+                                {
+                                    approvedPeopleList = approvedPeopleQry.Select( e => e.Id ).ToList();
+                                    var cachePolicy = new CacheItemPolicy();
+                                    cachePolicy.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes( 10 );
+                                    cache.Set( approvedPeopleGuid, approvedPeopleList, cachePolicy );
+                                }
                             }
-                            var errors = new List<string>();
-                            var approvedPeopleQry = approvedPeople.GetQuery( null, 30, out errors );
-                            if (approvedPeopleQry!=null && !approvedPeopleQry.Where( e => e.Id == person.Person.Id ).Any() )
+
+                            if ( approvedPeopleList != null && !approvedPeopleList.Contains( person.Person.Id ) )
                             {
                                 if ( remove )
                                 {
