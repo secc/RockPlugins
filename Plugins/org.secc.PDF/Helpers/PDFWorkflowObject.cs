ecc6b58cc0fccf9a2da372f7a0fd83eef7624dcc
mleesecc <mlee@secc.org>
2017-01-24 07:56:43 -0500

    ROCK-608 Updated workflow actions to be more in line with standards

--- Plugins/org.secc.PDF/Helpers/PDFWorkflowObject.cs
+++ Plugins/org.secc.PDF/Helpers/PDFWorkflowObject.cs
@@ -6,27 +6,20 @@ using Rock;
 using Rock.Model;
 using DotLiquid;
 using Rock.Data;
+using Rock.Web.Cache;
+using Rock.Field;
 
 namespace org.secc.PDF
 {
     public class PDFWorkflowObject
     {
-        public BinaryFile PDF { get; set; }
         public string LavaInput { get; set; }
         public Dictionary<string, object> MergeObjects { get; set; }
-        public string RenderedXHTML {
+        public string RenderedXHTML
+        {
             get
             {
                 return LavaInput.ResolveMergeFields( MergeObjects );
-            }
-        }
-
-        //The reason this is deprecated is to allow PDFs to be emailed or passed through without needing merged or generated first.
-        [Obsolete("This property is deprecated. Please reference PDFWorkflowObject.PDF instead.", false)]
-        public BinaryFile RenderedPDF {
-            get
-            {
-                return PDF;
             }
         }
 
@@ -34,46 +27,40 @@ namespace org.secc.PDF
         {
         }
 
-        public PDFWorkflowObject(WorkflowAction action, RockContext rockContext)
+        public PDFWorkflowObject( WorkflowAction action, RockContext rockContext )
         {
-            Utility.EnsureAttributes( action, rockContext );
-
             //load merge objects
             MergeObjects = new Dictionary<string, object>();
 
-            int personId = action.Activity.Workflow.GetAttributeValue( "PersonId" ).AsInteger();
-            if ( personId != 0 )
+            var activity = action.Activity;
+            MergeObjects.Add( "Activity", activity );
+            foreach ( var attribute in activity.AttributeValues )
             {
-                MergeObjects.Add( "Person", new PersonService( rockContext ).Get( personId ) );
-            }
-
-            int groupMemberId = action.Activity.Workflow.GetAttributeValue( "GroupMemberId" ).AsInteger();
-            if ( groupMemberId != 0 )
-            {
-                var groupMember = new GroupMemberService( rockContext ).Get( groupMemberId );
-                MergeObjects.Add( "GroupMember", groupMember );
-                MergeObjects.Add( "Group", groupMember.Group );
-            }
-
-            int registrationRegistrantId = action.Activity.Workflow.GetAttributeValue( "RegistrationRegistrantId" ).AsInteger();
-            if ( registrationRegistrantId != 0 )
-            {
-                var registrationRegistrant = new RegistrationRegistrantService( rockContext ).Get( registrationRegistrantId );
-                MergeObjects.Add( "RegistrationRegistrant", registrationRegistrant );
-                MergeObjects.Add( "Registration", registrationRegistrant.Registration );
-                MergeObjects.Add( "RegistrationInstance", registrationRegistrant.Registration.RegistrationInstance );
+                var a = AttributeCache.Read( attribute.Value.AttributeId );
+                if ( a != null && a.FieldType.Field is IEntityFieldType )
+                {
+                    MergeObjects.Add( attribute.Key, ( ( IEntityFieldType ) a.FieldType.Field ).GetEntity( attribute.Value.Value ) );
+                }
+                else
+                {
+                    MergeObjects.Add( attribute.Key, attribute.Value.ValueFormatted );
+                }
             }
 
             var workflow = action.Activity.Workflow;
-            workflow.LoadAttributes();
             MergeObjects.Add( "Workflow", workflow );
-
-            //load pdf binary file
-            Guid? pdfGuid = action.Activity.Workflow.GetAttributeValue( "PDFGuid" ).AsGuidOrNull();
-            PDF = new BinaryFileService( rockContext ).Get( pdfGuid ?? new Guid() );
-            MergeObjects.Add( "PDF", PDF );
-
-            LavaInput = action.Activity.Workflow.GetAttributeValue( "XHTML" );
+            foreach ( var attribute in workflow.AttributeValues )
+            {
+                var a = AttributeCache.Read( attribute.Value.AttributeId );
+                if ( a != null && a.FieldType.Field is IEntityFieldType )
+                {
+                    MergeObjects.Add( attribute.Key, ( ( IEntityFieldType ) a.FieldType.Field ).GetEntity( attribute.Value.Value ) );
+                }
+                else
+                {
+                    MergeObjects.Add( attribute.Key, attribute.Value.ValueFormatted );
+                }
+            }
         }
     }
 }
