de37bcfc6461a57b7204c3f3b9050aac4f0f74fb
mleesecc <mlee@secc.org>
2017-04-03 10:26:57 -0400

    ROCK-743 Init tool to start workflow for connection cards

---
+++ Plugins/org.secc.ConnectionCards/org_secc/ConnectionCards/ConnectionCardEntry.ascx
@@ -0,0 +1,43 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="ConnectionCardEntry.ascx.cs" Inherits="RockWeb.Plugins.org_secc.ConnectionCards.ConnectionCardEntry" %>
+
+<Rock:RockUpdatePanel ID="upMain" runat="server">
+    <ContentTemplate>
+        <asp:HiddenField ID="hfImageGuid" runat="server" />
+        <Rock:NotificationBox runat="server" Visible="false" ID="nbSuccess" NotificationBoxType="Success" Text="Workflows have been started"></Rock:NotificationBox>
+        <asp:Panel runat="server" ID="pnlUpload">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Connection Card Workflow
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <Rock:FileUploader ID="fMainSheet" runat="server" OnFileUploaded="fMainSheet_FileUploaded" />
+                    To begin, click or drag a pdf onto "<i class="fa fa-upload"></i> Upload".
+                </div>
+            </div>
+        </asp:Panel>
+        <asp:Panel runat="server" ID="pnlEdit" Visible="false">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <Rock:BootstrapButton runat="server" ID="btnRotLeft" CssClass="btn btn-lg btn-primary" ToolTip="Rotate Left"
+                        OnClick="btnRotLeft_Click" DataLoadingText="<i class='fa fa-undo fa-spin' style='animation-direction: reverse;'></i>"
+                        Text="<i class='fa fa-undo'></i>"></Rock:BootstrapButton>
+                    <Rock:BootstrapButton runat="server" ID="btnRotRight" CssClass="btn btn-lg btn-primary"  ToolTip="Rotate Right"
+                        OnClick="btnRotRight_Click" Text="<i class='fa fa-repeat'></i>"
+                        DataLoadingText="<i class='fa fa-repeat fa-spin'></i>"></Rock:BootstrapButton>
+                    <div class="pull-right">
+
+                        <Rock:BootstrapButton runat="server" ID="btnCrop" OnClick="btnCrop_Click" Text="Finished" CssClass="btn btn-lg btn-success">
+                        </Rock:BootstrapButton>
+
+                        <Rock:BootstrapButton runat="server" ID="btnBack" OnClick="btnBack_Click" Text="Cancel" CssClass="btn btn-lg btn-danger">
+                        </Rock:BootstrapButton>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <asp:Image runat="server" ID="iPicture" CssClass="img-responsive center-block" />
+                </div>
+            </div>
+        </asp:Panel>
+    </ContentTemplate>
+</Rock:RockUpdatePanel>
---
+++ Plugins/org.secc.ConnectionCards/org_secc/ConnectionCards/ConnectionCardEntry.ascx.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+
+using Rock;
+using Rock.Web.UI;
+using System.Web.UI.HtmlControls;
+using Rock.Web.UI.Controls;
+using Rock.Data;
+using Rock.Model;
+using Rock.Attribute;
+using Rock.Web.Cache;
+using org.secc.ConnectionCards.Utilities;
+using Ghostscript.NET.Rasterizer;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace RockWeb.Plugins.org_secc.ConnectionCards
+{
+    [DisplayName( "Connection Card Entry" )]
+    [Category( "SECC > Connection Cards" )]
+    [Description( "Entry form for adding connection cards in a sheet." )]
+
+    //Settings
+    [WorkflowTypeField( "Workflow Type", "The workflow type to activate to merge pdf" )]
+    [BinaryFileTypeField( "Binary File Type", "The binary file type to set images as." )]
+    public partial class ConnectionCardEntry : RockBlock
+    {
+        protected override void OnLoad( EventArgs e )
+        {
+            nbSuccess.Visible = false;
+            if ( string.IsNullOrWhiteSpace( hfImageGuid.Value ) )
+            {
+                pnlEdit.Visible = false;
+                pnlUpload.Visible = true;
+            }
+            else
+            {
+                ShowImage();
+            }
+        }
+
+        private void ShowImage()
+        {
+            pnlEdit.Visible = true;
+            pnlUpload.Visible = false;
+            var imageGuid = hfImageGuid.Value.AsGuid();
+            RockContext rockContext = new RockContext();
+            BinaryFileService binaryFileService = new BinaryFileService( rockContext );
+            var binaryFile = binaryFileService.Get( imageGuid );
+            if ( binaryFile != null )
+            {
+                iPicture.ImageUrl = string.Format( "{0}&cache={1}", binaryFile.Url, Guid.NewGuid() );
+                iPicture.Visible = true;
+            }
+        }
+
+        protected void fMainSheet_FileUploaded( object sender, EventArgs e )
+        {
+            int? mainSheetId = fMainSheet.BinaryFileId;
+            if ( mainSheetId != null || mainSheetId != 0 )
+            {
+                RockContext rockContext = new RockContext();
+                BinaryFileService binaryFileService = new BinaryFileService( rockContext );
+                var mainSheet = binaryFileService.Get( mainSheetId ?? 0 );
+                if ( mainSheet != null )
+                {
+                    BinaryFile binaryFile = ConnectionCardsUtilties.ConvertPDFToImage( mainSheet );
+                    if ( binaryFile != null )
+                    {
+                        BinaryFileTypeService binaryFileTypeService = new BinaryFileTypeService( rockContext );
+                        var binaryFileTypeGuid = GetAttributeValue( "BinaryFileType" ).AsGuid();
+                        var binaryFileType = binaryFileTypeService.Get( binaryFileTypeGuid );
+                        binaryFile.BinaryFileTypeId = binaryFileType.Id;
+                        binaryFile.IsTemporary = true;
+                        binaryFileService.Add( binaryFile );
+                        binaryFileService.Delete( mainSheet );
+                        rockContext.SaveChanges();
+                        hfImageGuid.Value = binaryFile.Guid.ToString();
+                        ShowImage();
+                    }
+                }
+            }
+        }
+
+        protected void btnRotLeft_Click( object sender, EventArgs e )
+        {
+            var imageGuid = hfImageGuid.Value.AsGuid();
+            RockContext rockContext = new RockContext();
+            BinaryFileService binaryFileService = new BinaryFileService( rockContext );
+            var binaryFile = binaryFileService.Get( imageGuid );
+            binaryFile = ConnectionCardsUtilties.RotateImage( binaryFile, System.Drawing.RotateFlipType.Rotate270FlipNone, rockContext );
+            ShowImage();
+        }
+
+        protected void btnRotRight_Click( object sender, EventArgs e )
+        {
+            var imageGuid = hfImageGuid.Value.AsGuid();
+            RockContext rockContext = new RockContext();
+            BinaryFileService binaryFileService = new BinaryFileService( rockContext );
+            var binaryFile = binaryFileService.Get( imageGuid );
+            binaryFile = ConnectionCardsUtilties.RotateImage( binaryFile, System.Drawing.RotateFlipType.Rotate90FlipNone, rockContext );
+            ShowImage();
+        }
+
+
+        protected void btnCrop_Click( object sender, EventArgs e )
+        {
+            var imageGuid = hfImageGuid.Value.AsGuid();
+            RockContext rockContext = new RockContext();
+            BinaryFileService binaryFileService = new BinaryFileService( rockContext );
+            var binaryFile = binaryFileService.Get( imageGuid );
+            var binaryFiles = ConnectionCardsUtilties.ChopImage( binaryFile, 3, 2, rockContext );
+            binaryFileService.Delete( binaryFile );
+            rockContext.SaveChanges();
+            foreach (var connectionCard in binaryFiles )
+            {
+                StartWorkflow( connectionCard );
+            }
+            pnlEdit.Visible = false;
+            pnlUpload.Visible = true;
+            hfImageGuid.Value = "";
+            fMainSheet.BinaryFileId = 0;
+            nbSuccess.Visible = true;
+        }
+
+        protected void StartWorkflow( BinaryFile binaryFile)
+        {
+            var errorMessages = new List<string>();
+
+            Guid? guid = GetAttributeValue( "WorkflowType" ).AsGuidOrNull();
+            if ( guid.HasValue )
+            {
+                using ( var rockContext = new RockContext() )
+                {
+                    var workflowTypeService = new WorkflowTypeService( rockContext );
+                    var workflowService = new WorkflowService( rockContext );
+
+                    var workflowType = workflowTypeService.Queryable( "ActivityTypes" )
+                        .Where( w => w.Guid.Equals( guid.Value ) )
+                        .FirstOrDefault();
+
+                    if ( workflowType != null )
+                    {
+                        var workflow = Rock.Model.Workflow.Activate( workflowType, "New Connection Card Workflow", rockContext );
+
+
+                        var activityTypes = workflowType.ActivityTypes.Where( a => a.IsActivatedWithWorkflow == true ).ToList();
+                        foreach ( var activityType in activityTypes )
+                        {
+
+                            WorkflowActivity.Activate( activityType, workflow, rockContext );
+                            if ( workflowService.Process( workflow, binaryFile, out errorMessages ) )
+                            {
+                                // Keep workflow active for continued processing
+                                workflow.CompletedDateTime = null;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void btnBack_Click( object sender, EventArgs e )
+        {
+            var imageGuid = hfImageGuid.Value.AsGuid();
+            RockContext rockContext = new RockContext();
+            BinaryFileService binaryFileService = new BinaryFileService( rockContext );
+            var binaryFile = binaryFileService.Get( imageGuid );
+            binaryFileService.Delete( binaryFile );
+            rockContext.SaveChanges();
+
+            pnlEdit.Visible = false;
+            pnlUpload.Visible = true;
+            hfImageGuid.Value = "";
+            fMainSheet.BinaryFileId = 0;
+        }
+    }
+}
