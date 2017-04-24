e70bac77187ec0bbab289ad60be67b1cff76a691
mleesecc <mlee@secc.org>
2017-04-24 08:56:25 -0400

    ROCK-757 Added current user rest API

---
+++ Plugins/org.secc.Security/Rest/Handlers/SessionRouteHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Routing;
+
+namespace org.secc.Security.Rest.Handlers
+{
+    public class SessionRouteHandler : IRouteHandler
+    {
+        IHttpHandler IRouteHandler.GetHttpHandler(RequestContext requestContext)
+        {
+           return new SessionControllerHandler(requestContext.RouteData);
+        }
+    }
+}
