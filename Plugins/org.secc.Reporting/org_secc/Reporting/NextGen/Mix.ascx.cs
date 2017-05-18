6ab41a13bec2ead7835879ea7605ea04878f168d
cbump <cbump@secc.org>
2017-05-18 17:08:09 -0400

    ROCK-766 - Adding signature document status.

--- Plugins/org.secc.Reporting/org_secc/Reporting/NextGen/Mix.ascx.cs
+++ Plugins/org.secc.Reporting/org_secc/Reporting/NextGen/Mix.ascx.cs
@@ -268,7 +268,7 @@ namespace RockWeb.Blocks.Reporting.NextGen
                         return row;
                     })(),
                     School = DefinedValueCache.Read(g.School.AsGuid()).Value,
-                    LegalRelease = tmp2.Where(gm => gm.GroupMember.Id == g.GroupMember.Id).SelectMany(gm => gm.SignatureDocuments).OrderByDescending(sd => sd.SignatureDocument.CreatedDateTime).Where(sd => signatureDocumentIds.Contains(sd.SignatureDocument.SignatureDocumentTemplateId)).Select(sd => sd.SignatureDocument.SignatureDocumentTemplate.Name).FirstOrDefault(),
+                    LegalRelease = tmp2.Where(gm => gm.GroupMember.Id == g.GroupMember.Id).SelectMany(gm => gm.SignatureDocuments).OrderByDescending(sd => sd.SignatureDocument.CreatedDateTime).Where(sd => signatureDocumentIds.Contains(sd.SignatureDocument.SignatureDocumentTemplateId)).Select(sd => sd.SignatureDocument.SignatureDocumentTemplate.Name + " (" + sd.SignatureDocument.Status.ToString() + ")").FirstOrDefault(),
                     Departure = "TBD", // (hopefully based on bus, otherwise a dropdown with 1-4) 
                     Campus = group.Campus, // 
                     Role = group.ParentGroup.GroupType.Name.Contains("Serving")?"Leader":"Student", // 
