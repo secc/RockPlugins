258b1334bc32f82d9c8b23acafb215083d0732a9
mleesecc <mlee@secc.org>
2017-07-28 16:01:23 -0400

    ROCK-776 Added remote storage of who is in worship

---
+++ Plugins/org.secc.RoomScanner/Models/Response.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.secc.RoomScanner.Models
+{
+    public class Response
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public bool Overridable { get; set; }
+        public bool RequireConfirmation { get; set; }
+        public int PersonId { get; set; }
+
+        public Response( bool success, string message, bool overridable, bool requireConfirmation = false, int personId = 0 )
+        {
+            this.Success = success;
+            this.Message = message;
+            this.Overridable = overridable;
+            this.RequireConfirmation = requireConfirmation;
+            this.PersonId = personId;
+        }
+    }
+}
