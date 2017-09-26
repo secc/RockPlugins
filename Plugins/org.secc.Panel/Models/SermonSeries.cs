a3b17b22c4b74fe8f204a9ed23ad6b6eee57b610
mleesecc <mlee@secc.org>
2017-09-25 23:53:52 -0400

    ROCK-885 Init Panel to Rock sync

---
+++ Plugins/org.secc.Panel/Models/SermonSeries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.secc.Panel.Models
+{
+    class SermonSeries
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string slug { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        public long lastupdated { get; set; }
+        public bool deleted { get; set; }
+        public List<Sermon> sermons { get; set; }
+
+        public override string ToString()
+        {
+            return this.title;
+        }
+    }
+}
---
+++ Plugins/org.secc.Panel/Models/Story.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.secc.Panel.Models
+{
+    class Story
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string slug { get; set; }
+        public string description { get; set; }
+        public int duration { get; set; }
+        public long vimeo_id { get; set; }
+        public string vimeo_sd_url { get; set; }
+        public string vimeo_live_url { get; set; }
+        public string tags { get; set; }
+        public long datecreated { get; set; }
+    }
+}
