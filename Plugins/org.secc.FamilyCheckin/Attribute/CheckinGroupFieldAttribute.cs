15c3d9c6c6a1e167b7234a79ee75627cff0f5865
mleesecc <mlee@secc.org>
2017-09-29 14:06:22 -0400

    ROCK-893 Moved checkin group attribute from core

---
+++ Plugins/org.secc.FamilyCheckin/Attribute/CheckinGroupFieldAttribute.cs
@@ -0,0 +1,40 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+namespace Rock.Attribute
+{
+    /// <summary>
+    /// Field Attribute to select a single (or null) Group
+    /// Stored as Group.Guid
+    /// </summary>
+    public class CheckinGroupFieldAttribute : FieldAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckinGroupFieldAttribute" /> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="required">if set to <c>true</c> [required].</param>
+        /// <param name="defaultGroupGuid">The default group unique identifier.</param>
+        /// <param name="category">The category.</param>
+        /// <param name="order">The order.</param>
+        /// <param name="key">The key.</param>
+        public CheckinGroupFieldAttribute( string name, string description = "", bool required = true, string defaultGroupGuid = "", string category = "", int order = 0, string key = null )
+            : base( name, description, required, defaultGroupGuid, category, order, key, typeof( org.secc.FamilyCheckin.FieldTypes.CheckinGroupFieldType ).FullName )
+        {
+        }
+    }
+}
