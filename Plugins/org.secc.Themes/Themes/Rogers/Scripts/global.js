37ca4531bbb25c89661ae68e4947c11c221c411e
ColoradoWebGuy <sschulte@secc.org>
2018-01-26 15:13:03 -0500

    Added Child Theme for Camp Freedom, Updated Logo to SVG, and polished up menu scroll (animation scroll WIP).

--- Plugins/org.secc.Themes/Themes/Rogers/Scripts/global.js
+++ Plugins/org.secc.Themes/Themes/Rogers/Scripts/global.js
@@ -146,6 +146,32 @@ $(document).ready(function() {
 
       if (typeof ch !== 'undefined') {
           $(window).scroll(function() {
+              /*
+              Animation Scroll Project - WIP
+
+              ch = $('.cd-hero').height() - (40);
+
+              
+              if ($(window).width() < 960) {
+                  if( $(this).scrollTop() > ch ) {
+                      cmh.addClass("nav-is-fixed");
+                      cmc.addClass("nav-fixed-padding");
+                      cmh.addClass("animated slideInDown");
+                  } else if($(this).scrollTop() < ch - $('.cd-main-header').height()) {
+                      cmh.removeClass("nav-is-fixed");
+                      cmc.removeClass("nav-fixed-padding");
+                      cmh.removeClass("animated slideInDown");
+                  }
+              } else {
+                  if( $(this).scrollTop() > ch - $('.cd-main-header').height()) {
+                      cmh.addClass("nav-is-fixed");
+                      cmc.addClass("nav-fixed-padding");
+                  } else {
+                      cmh.removeClass("nav-is-fixed");
+                      cmc.removeClass("nav-fixed-padding");
+                  }
+              }
+              */
               if( $(this).scrollTop() > ch ) {
                   cmh.addClass("nav-is-fixed");
                   cmc.addClass("nav-fixed-padding");
@@ -166,4 +192,36 @@ $(document).ready(function() {
               }
           });
       }
+
+      /*
+     * Replace all SVG images with inline SVG
+     */
+        jQuery('img.svg').each(function(){
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+
+            jQuery.get(imgURL, function(data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
+
+                // Add replaced image's ID to the new SVG
+                if(typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if(typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass+' replaced-svg');
+                }
+
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+
+            }, 'xml');
+
+        });
 });
