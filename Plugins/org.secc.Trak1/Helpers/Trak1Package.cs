2ae1f3ec8e72f8388a885f9d4ab0e22ff40f948e
Mark Lee <mlee@secc.org>
2018-03-14 17:48:15 -0400

    ROCK-68 Init Trak-1 Integration

---
+++ Plugins/org.secc.Trak1/Helpers/Trak1Package.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.secc.Trak1.Helpers
+{
+    public class Trak1Package
+    {
+        public string PackageServiceNumber { get; set; }
+        public string PackageName { get; set; }
+        public string PackagePrice { get; set; }
+        public string PackageDescription { get; set; }
+        public string TierOrder { get; set; }
+        public List<Trak1Component> Components { get; set; }
+    }
+}
---
+++ Plugins/org.secc.Trak1/Helpers/Trak1ReportStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.secc.Trak1.Helpers
+{
+    public class Trak1ReportStatus
+    {
+        public string RequestValidation { get; set; }
+        public string ReportStatus { get; set; }
+        public string HitColor { get; set; }
+        public string Recommendation { get; set; }
+        public string ReportLastUpdate { get; set; }
+        public string TransactionId { get; set; }
+    }
+}
