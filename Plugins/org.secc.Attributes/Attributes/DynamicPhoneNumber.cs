5f9087308da5474e2bdb718673a6b583af5b85e4
Mark Lee <mlee@secc.org>
2018-11-02 14:17:36 -0400

    ROCK-1865 Created new dynamic phone number picker

---
+++ Plugins/org.secc.Attributes/Attributes/DynamicPhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using org.secc.Attributes.FieldTypes;
+using Rock.Attribute;
+
+namespace org.secc.Attributes.Attributes
+{
+    public class DynamicPhoneNumber : FieldAttribute
+    {
+        public DynamicPhoneNumber( string name, string description = "", bool required = true, string defaultGroupGuid = "", string category = "", int order = 0, string key = null )
+            : base( name, description, required, defaultGroupGuid, category, order, key, typeof( DynamicPhoneNumberFieldType ).FullName )
+        {
+        }
+    }
+}
