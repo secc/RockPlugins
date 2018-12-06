34fa8ad17a990005a161d74fa0f1f551ec4dd23b
ColoradoWebGuy <sschulte@secc.org>
2018-12-05 22:39:46 -0500

    ROCK-2313 - Rearranged Footer Items and Updated Styles with it.

--- Plugins/org.secc.Themes/Themes/SECC2019/Scripts/global-dist.js
+++ Plugins/org.secc.Themes/Themes/SECC2019/Scripts/global-dist.js
@@ -803,6 +803,71 @@ $(document).ready(function() {
 		}
 	});
 
+	/**********************************************************/
+	/******** Start: Expandable DIVs for the footer ***********/
+	/**********************************************************/
+
+		function collapseSection(element) {
+		  // get the height of the element's inner content, regardless of its actual size
+		  var sectionHeight = element.scrollHeight;
+		  // temporarily disable all css transitions
+		  var elementTransition = element.style.transition;
+		  element.style.transition = '';
+		  // on the next frame (as soon as the previous style change has taken effect),
+		  // explicitly set the element's height to its current pixel height, so we
+		  // aren't transitioning out of 'auto'
+		  requestAnimationFrame(function() {
+		    element.style.height = sectionHeight + 'px';
+		    element.style.transition = elementTransition;
+		    // on the next frame (as soon as the previous style change has taken effect),
+		    // have the element transition to height: 0
+		    requestAnimationFrame(function() {
+		      element.style.height = 0 + 'px';
+		    });
+		  });
+		  // mark the section as "currently collapsed"
+		  element.setAttribute('data-collapsed', 'true');
+		}
+
+		function expandSection(element) {
+		  // get the height of the element's inner content, regardless of its actual size
+		  var sectionHeight = element.scrollHeight;
+		  // have the element transition to the height of its inner content
+		  element.style.height = sectionHeight + 'px';
+		  // when the next css transition finishes (which should be the one we just triggered)
+		  element.addEventListener('transitionend', function(e) {
+		    // remove this event listener so it only gets triggered once
+		    element.removeEventListener('transitionend', arguments.callee);
+		    // remove "height" from the element's inline styles, so it can return to its initial value
+		    element.style.height = null;
+		  });
+		  // mark the section as "currently not collapsed"
+		  element.setAttribute('data-collapsed', 'false');
+		}
+
+		$('.se-expandable-header').on( "click", function(e) {
+			e.stopPropagation();
+  			e.preventDefault();
+			
+			if (this) {
+				var section = this.parentNode.querySelector('.se-expandable-content')
+			}
+			if (section) {
+				var isCollapsed = section.getAttribute('data-collapsed') === 'true';
+			}
+
+			if(isCollapsed) {
+				expandSection(section)
+				this.setAttribute('data-expanded', 'true')
+				section.setAttribute('data-collapsed', 'false')
+			} else {
+				collapseSection(section)
+				this.setAttribute('data-expanded', 'false')
+			}
+		});
+
+	/**********************************************************/
+	/******** End: Expandable DIVs for the footer ***********/
+	/**********************************************************/
 
 });
-
