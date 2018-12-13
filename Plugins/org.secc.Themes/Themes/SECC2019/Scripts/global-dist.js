7895490c42b0de73e1555fffd173c127f76089d2
ColoradoWebGuy <sschulte@secc.org>
2018-12-13 15:35:56 -0500

    ROCK-2334, ROCK-2379, ROCK-2380 -- Finished adding a newsletter signup section and a back-to-top button for a better website experience when browsing.

--- Plugins/org.secc.Themes/Themes/SECC2019/Scripts/global-dist.js
+++ Plugins/org.secc.Themes/Themes/SECC2019/Scripts/global-dist.js
@@ -848,7 +848,7 @@ $(document).ready(function() {
 		$('.se-expandable-header').on( "click", function(e) {
 			e.stopPropagation();
   			e.preventDefault();
-			
+
 			if (this) {
 				var section = this.parentNode.querySelector('.se-expandable-content')
 			}
@@ -869,5 +869,7 @@ $(document).ready(function() {
 	/**********************************************************/
 	/******** End: Expandable DIVs for the footer ***********/
 	/**********************************************************/
+
+	$(document).ready(function(o){var t=300,a=1200,s=700,l=o(".js__back-to-top");o(window).scroll(function(){o(this).scrollTop()>t?l.addClass("-is-visible"):l.removeClass("-is-visible -zoom-out"),o(this).scrollTop()>a&&l.addClass("-zoom-out")}),l.on("click",function(t){t.preventDefault(),o("body,html").animate({scrollTop:0},s)})});
 
 });
