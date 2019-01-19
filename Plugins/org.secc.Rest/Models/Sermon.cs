a484a05bfd750bb1678e26567bee1bda606686e5
Mark Lee <mlee@secc.org>
2019-01-19 12:32:34 -0500

    ROCK-2487 Added a rest endpoint for a Kyle Idleman

---
+++ Plugins/org.secc.Rest/Models/Sermon.cs
@@ -0,0 +1,22 @@
+﻿namespace org.secc.SermonFeed.Models
+{
+    class Sermon
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string slug { get; set; }
+        public string description { get; set; }
+        public string speaker { get; set; }
+        public int duration { get; set; }
+        public long vimeo_id { get; set; }
+        public string audio_link { get; set; }
+        public int auio_size { get; set; } // not a typo
+        public string campus { get; set; } = "920";
+        public long date { get; set; }
+        public int deleted { get; set; } = 0;
+        public string notes { get; set; }
+        public string questions { get; set; }
+        public string image { get; set; }
+        public string image_url { get; set; }
+    }
+}
