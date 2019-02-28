7603342d7f5d49c0b01eca7eeef4aacf8548f168
Chuck Bump <cbump@secc.org>
2019-02-28 07:22:45 -0500

    ROCK-2787 - Adding the ability to launch workflows from dataview in the WorkflowLauncher job.

--- Plugins/org.secc.WorkflowLauncher/WorkflowLauncher.cs
+++ Plugins/org.secc.WorkflowLauncher/WorkflowLauncher.cs
@@ -24,8 +24,10 @@ using System.Web;
 
 namespace org.secc.Jobs
 {
-    [CodeEditorField("SQL Query", "The SQL to execute.", Rock.Web.UI.Controls.CodeEditorMode.Sql)]
-    [WorkflowTypeField("Workflow", "The workflow to launch for each row.", false, true)]
+    [CodeEditorField( "SQL Query", "The SQL to execute.  Each row will launch a workflow and columns will be automatically mapped to workflow attributes.", Rock.Web.UI.Controls.CodeEditorMode.Sql, required: false, category:"Source", order:1 )]
+    [DataViewField( "Data View", "A Dataview to use for launching workflows with the selected entity.", false, category: "Source", order: 2)]
+    [IntegerField( "Command Timeout", "Maximum amount of time (in seconds) to wait for the SQL Query or DataView to complete.", false, 60 * 60, "Source", 2, "CommandTimeout" )]
+    [WorkflowTypeField( "Workflow", "The workflow to launch for each row.", false, true, category:"Target" )]
 
     [DisallowConcurrentExecution]
     public class WorkflowLauncher : IJob
@@ -35,51 +37,81 @@ namespace org.secc.Jobs
             JobDataMap dataMap = context.JobDetail.JobDataMap;
 
             string query = dataMap.GetString( "SQLQuery" );
+
+            Guid? dataViewGuid = dataMap.GetString( "DataView" ).AsGuidOrNull();
             Guid? workflowTypeGuid = dataMap.GetString( "Workflow" ).AsGuidOrNull();
 
             // run a SQL query to do something
             int? commandTimeout = dataMap.GetString( "CommandTimeout" ).AsIntegerOrNull();
             int failed = 0;
             int successful = 0;
+
+            List<string> errors = new List<string>();
             try
             {
                 if ( workflowTypeGuid.HasValue )
                 {
-                    var workflowType = Rock.Web.Cache.WorkflowTypeCache.Read( workflowTypeGuid.Value );
+                    var workflowType = Rock.Web.Cache.WorkflowTypeCache.Get( workflowTypeGuid.Value );
                     if ( workflowType != null && ( workflowType.IsActive ?? true ) )
                     {
-                        DataSet rows = DbService.GetDataSet( query, System.Data.CommandType.Text, null, commandTimeout );
-                        
-                        List<string> workflowErrors;
-                        using (var rockContext = new RockContext() )
+                        using ( var rockContext = new RockContext() )
                         {
-                            // Fire off a workflow for each row in the query
-                            foreach(DataRow row in rows.Tables[0].Rows)
+                            // Handle the SQL Query
+                            if ( !query.IsNullOrWhiteSpace() )
                             {
-                                var workflow = Workflow.Activate( workflowType, workflowType.Name );
-                                workflow.LoadAttributes(rockContext);
+                                DataSet rows = DbService.GetDataSet( query, System.Data.CommandType.Text, null, commandTimeout );
 
-                                // Iterate over each column setting attribute values in the workflow
-                                foreach ( DataColumn column in rows.Tables[0].Columns )
+                                // Fire off a workflow for each row in the query
+                                foreach ( DataRow row in rows.Tables[0].Rows )
                                 {
-                                    if ( workflow.Attributes.ContainsKey( column.ColumnName ) )
+                                    var workflow = Workflow.Activate( workflowType, workflowType.Name );
+                                    workflow.LoadAttributes( rockContext );
+
+                                    // Iterate over each column setting attribute values in the workflow
+                                    foreach ( DataColumn column in rows.Tables[0].Columns )
                                     {
-                                        workflow.SetAttributeValue( column.ColumnName, Convert.ToString( row[column] ) );
+                                        if ( workflow.Attributes.ContainsKey( column.ColumnName ) )
+                                        {
+                                            workflow.SetAttributeValue( column.ColumnName, Convert.ToString( row[column] ) );
+                                        }
                                     }
-                                }
 
-                                // Now process the workflow
-                                var processed = new WorkflowService( rockContext ).Process( workflow, out workflowErrors );
-                                if ( processed )
-                                {
-                                    successful++;
-                                } else
-                                {
-                                    failed++;
+                                    // Now process the workflow
+                                    var processed = new WorkflowService( rockContext ).Process( workflow, out errors );
+                                    if ( processed )
+                                    {
+                                        successful++;
+                                    }
+                                    else
+                                    {
+                                        failed++;
+                                    }
+
                                 }
 
                             }
 
+                            // If a dataview is specified, then we'll fire off workflows for that dataview
+                            if ( dataViewGuid != null )
+                            {
+                                var dataView = new DataViewService( rockContext ).Get( dataViewGuid.Value );
+                                
+                                var entityList = dataView.GetQuery( null, commandTimeout, out errors );
+                                foreach(IEntity entity in entityList)
+                                {
+                                    var workflow = Workflow.Activate( workflowType, workflowType.Name );
+
+                                    var processed = new WorkflowService( rockContext ).Process( workflow, entity, out errors );
+                                    if ( processed )
+                                    {
+                                        successful++;
+                                    }
+                                    else
+                                    {
+                                        failed++;
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -90,8 +122,8 @@ namespace org.secc.Jobs
                 ExceptionLogService.LogException( ex, context2 );
                 throw;
             }
-            
-            context.Result = string.Format( "Workflow Launch Results:<br />Success: " + successful +"<br />Failed: "+failed );
+
+            context.Result = string.Format( "Workflow Launch Results:<br />Success:  {0}<br />Failed: {1}{2}", successful, failed, errors.Count>0? "<br />Errors: " + string.Join("<br />", errors ):"" );
         }
     }
 }
