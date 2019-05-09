e9d5639121482378053ca1c0e52f23772863564c
Mark Lee <mlee@secc.org>
2019-05-09 11:44:23 -0400

    ROCK-3191 Added endpoint for Content items

---
+++ Plugins/org.secc.Rest/Models/ChannelItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.secc.Rest.Models
+{
+    public class ChannelItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<string> Tags { get; set; }
+        public DateTime DateTime { get; set; }
+        public List<int> ChildItems { get; set; }
+        public List<int> ParentItems { get; set; }
+        public int Order { get; set; }
+        public int Priority { get; set; }
+        public Dictionary<string, string> Attributes { get; set; } = new Dictionary<string, string>();
+        public string Slug { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
