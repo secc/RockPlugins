728cfd8308194b87028669eaa5a28b6ff5abde25
Mark Lee <mlee@secc.org>
2019-07-01 10:04:15 -0400

    ROCK-3447 Added group publishing model

---
+++ Plugins/org.secc.GroupManager/Model/PublishGroup.cs
@@ -0,0 +1,91 @@
+﻿// <copyright>
+// Copyright Southeast Christian Church
+//
+// Licensed under the  Southeast Christian Church License (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License shoud be included with this file.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Runtime.Serialization;
+using Rock.Data;
+using Rock.Model;
+
+namespace org.secc.GroupManager.Model
+{
+    [Table( "_org_secc_GroupManager_PublishGroup" )]
+    [DataContract]
+    public class PublishGroup : Model<PublishGroup>
+    {
+        [Index]
+        [DataMember]
+        public int GroupId { get; set; }
+
+        public virtual Group Group { get; set; }
+
+        [DataMember]
+        public int ImageId { get; set; }
+        public BinaryFile Image { get; set; }
+
+        [Index]
+        [DataMember]
+        public DateTime StartDateTime { get; set; }
+
+        [Index]
+        [DataMember]
+        public DateTime EndDateTime { get; set; }
+
+        public virtual ICollection<DefinedValue> AudienceValues
+        {
+            get { return _audienceValues ?? ( _audienceValues = new Collection<DefinedValue>() ); }
+            set { _audienceValues = value; }
+        }
+        private ICollection<DefinedValue> _audienceValues;
+
+        [Index]
+        [DataMember]
+        public int RequestorAliasId { get; set; }
+        public virtual PersonAlias RequestorAlias { get; set; }
+
+        [Index]
+        [DataMember]
+        public int ContactPersonAliasId { get; set; }
+        public PersonAlias ContactPersonAlias { get; set; }
+
+        [DataMember]
+        public string ContactEmail { get; set; }
+
+        [DataMember]
+        public string ContactPhoneNumber { get; set; }
+
+        [DataMember]
+        public string ConfirmationFromName { get; set; }
+
+        [DataMember]
+        public string ConfirmationEmail { get; set; }
+
+        [DataMember]
+        public string ConfirmationSubject { get; set; }
+
+        [DataMember]
+        public string ConfirmationBody { get; set; }
+    }
+
+    public partial class PublishGroupConfiguration : EntityTypeConfiguration<PublishGroup>
+    {
+        public PublishGroupConfiguration()
+        {
+            this.HasMany( pg => pg.AudienceValues ).WithMany().Map( pg => { pg.MapLeftKey( "PublishGroupdId" ); pg.MapRightKey( "DefinedValueId" ); pg.ToTable( "_org_secc_GroupManager_PublishGroupAudienceValue" ); } );
+        }
+    }
+}
