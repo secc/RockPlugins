a62b6cd4a1c703080c5c2b71867643d07e2f9081
Ben Goshow <ben@fishhook.us>
2019-09-13 09:27:25 -0400

    prep files for handoff
    Removed SECC2019 Theme imports and just linked to those compiled css files directly in Site.Master. Much smaller seportal.css file

---
+++ Plugins/org.secc.Themes/Themes/SECC2019Portal/config.rb
@@ -0,0 +1,20 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "Styles"
+sass_dir = "Styles"
+images_dir = "Assets/img"
+javascripts_dir = "Scripts"
+fonts_dir = "fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :scss
