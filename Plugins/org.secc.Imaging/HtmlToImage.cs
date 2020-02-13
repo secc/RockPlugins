26556212a208994ccd42a6f525e2d882cffb1a73
Mark Lee <mlee@secc.org>
2020-02-12 20:59:25 -0500

    ROCK-4272 Init work on lava to image

---
+++ Plugins/org.secc.Imaging/HtmlToImage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NReco.ImageGenerator;
+using Rock;
+using Rock.Web.Cache;
+
+namespace org.secc.Imaging
+{
+    public class HtmlToImage
+    {
+        public static byte[] GenerateImage( string html, string format = "png", int? width = null, int? height = null )
+        {
+            var htmlToImageConv = new HtmlToImageConverter();
+
+            if ( width.HasValue )
+            {
+                htmlToImageConv.Width = width.Value;
+            }
+
+            if ( height.HasValue )
+            {
+                htmlToImageConv.Height = height.Value;
+            }
+
+
+            var image = htmlToImageConv.GenerateImage( html, ImageFormat.Png );
+
+            return image;
+        }
+    }
+}
