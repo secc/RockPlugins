34a6cf29cc815d2edfc03fa5b29a8ae1a2ac61b9
Mark Lee <mlee@secc.org>
2020-02-24 17:47:12 -0500

    ROCK-3545 Added sql data filter

---
+++ Plugins/org.secc.Reporting/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.secc.Reporting
+{
+    public static class Helpers
+    {
+        public static string Md5Hash( string str )
+        {
+            using ( var crypt = MD5.Create() )
+            {
+                var hash = crypt.ComputeHash( Encoding.UTF8.GetBytes( str ) );
+
+                StringBuilder sb = new StringBuilder();
+                foreach ( byte b in hash )
+                {
+                    // Can be "x2" if you want lowercase
+                    sb.Append( b.ToString( "x2" ) );
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
