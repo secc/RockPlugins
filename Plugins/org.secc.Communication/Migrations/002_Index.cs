63c28281a3709bdd955382493eada119d14f2952
Mark Lee <mlee@secc.org>
2020-05-19 14:52:21 -0400

    ROCK-4595 Added table and tools to sync back Twilio data

---
+++ Plugins/org.secc.Communication/Migrations/002_Index.cs
@@ -0,0 +1,20 @@
+﻿namespace org.secc.Communication.Migrations
+{
+    using Rock.Plugin;
+
+    [MigrationNumber( 2, "1.10.0" )]
+    public partial class Index : Migration
+    {
+        public override void Up()
+        {
+            Sql( "ALTER TABLE [dbo].[_org_secc_Communication_TwilioHistory] ADD CONSTRAINT [PK_dbo.org.secc.Communication.TwilioHistory] PRIMARY KEY CLUSTERED (Id)" );
+            Sql( "CREATE INDEX [IX_SID] ON [dbo].[_org_secc_Communication_TwilioHistory] (SID)" );
+            Sql( "CREATE INDEX [IX_DateCreated] ON [dbo].[_org_secc_Communication_TwilioHistory] (DateCreated)" );
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
---
+++ Plugins/org.secc.Communication/Twilio/TwilioDownloader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using org.secc.Communication.Model;
+using Rock.Communication;
+using Rock.Communication.Transport;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Twilio;
+using Twilio.Base;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace org.secc.Communication.Twilio
+{
+    public class TwilioDownloader
+    {
+        private string accountSid = "";
+        private string authToken = "";
+
+        public TwilioDownloader()
+        {
+            TransportComponent transportComponent = null;
+            foreach ( var serviceEntry in TransportContainer.Instance.Components )
+            {
+                transportComponent = serviceEntry.Value.Value;
+                var entityType = EntityTypeCache.Get( transportComponent.GetType() );
+                if ( entityType != null && entityType.Guid.Equals( EntityTypeCache.Get( typeof( Rock.Communication.Transport.Twilio ) ).Guid ) )
+                {
+                    accountSid = transportComponent.GetAttributeValue( "SID" );
+                    authToken = transportComponent.GetAttributeValue( "Token" );
+                }
+            }
+        }
+
+        public int SyncItems( DateTime dateTime )
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            TwilioClient.Init( accountSid, authToken );
+
+            ResourceSet<MessageResource> messages = null;
+
+            try
+            {
+                messages = MessageResource.Read( dateSent: dateTime );
+            }
+            catch ( Exception e )
+            {
+                ExceptionLogService.LogException( new Exception( "Could not download Twilio messages see inner exception for details.", e ) );
+                return 0;
+            }
+
+            var count = 0;
+
+            foreach ( var record in messages )
+            {
+                count++;
+                using ( RockContext rockContext = new RockContext() )
+                {
+                    TwilioHistoryService twilioHistoryService = new TwilioHistoryService( rockContext );
+                    twilioHistoryService.AddOrUpdateResource( record );
+                    rockContext.SaveChanges();
+                }
+            }
+            return count;
+        }
+    }
+}
