70db6cf6564a7532a6f3f7633d2ed837494f6572
Mark Lee <mlee@secc.org>
2020-06-08 11:07:56 -0400

    ROCK-4046 Added extensions to allow migrations to complete

---
+++ Plugins/org.secc.DevLib/Extensions/Migration/RockMigrationExtensions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.Migrations.Model;
+using System.Data.Entity.Migrations.Sql;
+using System.Data.Entity.SqlServer;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Rock;
+using Rock.Lava.Blocks;
+
+namespace org.secc.DevLib.Extensions.Migration
+{
+    public static class RockMigrationExtensions
+    {
+        public static void AddPrimaryKey( this Rock.Plugin.Migration migration, string table, string column, string name = null, bool clustered = true, object anonymousArguments = null )
+        {
+            var mini = new MiniMigration();
+            mini.AddPrimaryKey( table, column, name, clustered, anonymousArguments );
+            migration.Sql( mini.GetMigrationSql( migration.SqlConnection ) );
+        }
+
+        public static void AddForeignKey( this Rock.Plugin.Migration migration, string dependentTable, string dependentColumn, string principalTable, string principalColumn = null, bool cascadeDelete = false, string name = null, object anonymousArguments = null )
+        {
+            var mini = new MiniMigration();
+            mini.AddForeignKey( dependentTable, dependentColumn, principalTable, principalColumn, cascadeDelete, name, anonymousArguments );
+            migration.Sql( mini.GetMigrationSql( migration.SqlConnection ) );
+        }
+
+        public static void AddForeignKey( this Rock.Plugin.Migration migration, string dependentTable, string[] dependentColumns, string principalTable, string[] principalColumns = null, bool cascadeDelete = false, string name = null, object anonymousArguments = null )
+        {
+            var mini = new MiniMigration();
+            mini.AddForeignKey( dependentTable, dependentColumns, principalTable, principalColumns, cascadeDelete, name, anonymousArguments );
+            migration.Sql( mini.GetMigrationSql( migration.SqlConnection ) );
+        }
+
+        public static void CreateIndex( this Rock.Plugin.Migration migration, string table, string column, bool unique = false, string name = null, bool clustered = false, object anonymousArguments = null )
+        {
+            var mini = new MiniMigration();
+            mini.CreateIndex( table, column, unique, name, clustered, anonymousArguments );
+            migration.Sql( mini.GetMigrationSql( migration.SqlConnection ) );
+        }
+
+        public static void CreateIndex( this Rock.Plugin.Migration migration, string table, string[] columns, bool unique = false, string name = null, bool clustered = false, object anonymousArguments = null )
+        {
+            var mini = new MiniMigration();
+            mini.CreateIndex( table, columns, unique, name, clustered, anonymousArguments );
+            migration.Sql( mini.GetMigrationSql( migration.SqlConnection ) );
+        }
+    }
+
+    class MiniMigration : System.Data.Entity.Migrations.DbMigration
+    {
+        internal new void AddPrimaryKey( string table, string column, string name = null, bool clustered = true, object anonymousArguments = null )
+        {
+            base.AddPrimaryKey( table, column, name, clustered, anonymousArguments );
+        }
+
+        internal new void AddForeignKey( string dependentTable, string dependentColumn, string principalTable, string principalColumn = null, bool cascadeDelete = false, string name = null, object anonymousArguments = null )
+        {
+            base.AddForeignKey( dependentTable, dependentColumn, principalTable, principalColumn, cascadeDelete, name, anonymousArguments );
+        }
+
+        internal new void AddForeignKey( string dependentTable, string[] dependentColumns, string principalTable, string[] principalColumns = null, bool cascadeDelete = false, string name = null, object anonymousArguments = null )
+        {
+            base.AddForeignKey( dependentTable, dependentColumns, principalTable, principalColumns, cascadeDelete, name, anonymousArguments );
+        }
+
+        internal new void CreateIndex( string table, string column, bool unique = false, string name = null, bool clustered = false, object anonymousArguments = null )
+        {
+            base.CreateIndex( table, column, unique, name, clustered, anonymousArguments );
+        }
+
+        internal new void CreateIndex( string table, string[] columns, bool unique = false, string name = null, bool clustered = false, object anonymousArguments = null )
+        {
+            base.CreateIndex( table, columns, unique, name, clustered, anonymousArguments );
+        }
+
+
+        public override void Up()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string GetMigrationSql( SqlConnection sqlConnection )
+        {
+            StringBuilder sql = new StringBuilder();
+            var prop = this.GetType().GetProperty( "Operations", BindingFlags.NonPublic | BindingFlags.Instance );
+            if ( prop != null )
+            {
+                IEnumerable<MigrationOperation> operations = prop.GetValue( this ) as IEnumerable<MigrationOperation>;
+                foreach ( var operation in operations )
+                {
+                    if ( operation is AddForeignKeyOperation && ( ( AddForeignKeyOperation ) operation ).PrincipalColumns.Count == 0 )
+                    {
+                        ( ( AddForeignKeyOperation ) operation ).PrincipalColumns.Add( "Id" );
+                    }
+                }
+                var generator = new SqlServerMigrationSqlGenerator();
+                var statements = generator.Generate( operations, sqlConnection.ServerVersion.AsInteger() > 10 ? "2008" : "2005" );
+                foreach ( MigrationStatement item in statements )
+                {
+                    sql.Append( item.Sql );
+                }
+            }
+            return sql.ToString();
+        }
+    }
+}
---
+++ Plugins/org.secc.DevLib/Extensions/Migration/TableBuilderExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.Migrations.Builders;
+using System.Data.Entity.Migrations.Model;
+using System.Data.Entity.Migrations.Sql;
+using System.Data.Entity.SqlServer;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Rock;
+
+namespace org.secc.DevLib.Extensions.Migration
+{
+    public static class TableBuilderExtensions
+    {
+        public static void Run<TColumns>( this TableBuilder<TColumns> tableBuilder, Rock.Plugin.Migration migration )
+        {
+            FieldInfo fiMigration = tableBuilder.GetType().GetField( "_migration", BindingFlags.NonPublic | BindingFlags.Instance );
+            DbMigration dbMigration = fiMigration.GetValue( tableBuilder ) as DbMigration;
+
+            migration.Sql( GetMigrationSql( dbMigration, migration.SqlConnection ) );
+        }
+
+        internal static string GetMigrationSql( DbMigration dbMigration, SqlConnection sqlConnection )
+        {
+            StringBuilder sql = new StringBuilder();
+            var prop = dbMigration.GetType().GetProperty( "Operations", BindingFlags.NonPublic | BindingFlags.Instance );
+            if ( prop != null )
+            {
+                IEnumerable<MigrationOperation> operations = prop.GetValue( dbMigration ) as IEnumerable<MigrationOperation>;
+                foreach ( var operation in operations )
+                {
+                    if ( operation is AddForeignKeyOperation && ( ( AddForeignKeyOperation ) operation ).PrincipalColumns.Count == 0 )
+                    {
+                        // In Rock, the principal column should always be the Id.  This isn't always the case . . . .
+                        ( ( AddForeignKeyOperation ) operation ).PrincipalColumns.Add( "Id" );
+                    }
+                }
+                var generator = new SqlServerMigrationSqlGenerator();
+                var statements = generator.Generate( operations, sqlConnection.ServerVersion.AsInteger() > 10 ? "2008" : "2005" );
+                foreach ( MigrationStatement item in statements )
+                {
+                    if ( item.Sql.StartsWith( "CREATE TABLE" ) )
+                    {
+                        //So the way this works is ROCK makes our table for us.
+                        //But table builder just tacks on the Primary Key constraint
+                        //So remove most of the table text and change it to an alter table instead
+                        var start = item.Sql.IndexOf( "CREATE TABLE" ) + 12;
+                        var end = item.Sql.IndexOf( "(" );
+                        var tableName = item.Sql.Substring( start, end - start );
+                        var constraint = item.Sql.Substring( item.Sql.IndexOf( "CONSTRAINT" ) );
+                        constraint = constraint.Substring( 0, constraint.Length - 1 );
+                        sql.Append( string.Format( $"ALTER TABLE {tableName} ADD {constraint};" ) );
+                    }
+                    else
+                    {
+                        sql.Append( item.Sql + ";" );
+                    }
+                }
+            }
+            return sql.ToString();
+        }
+
+
+
+    }
+}
