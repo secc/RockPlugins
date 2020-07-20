b5234af5e23ee2de2f23a7fc7613639d1ce1ffc2
Mark Lee <mlee@secc.org>
2020-07-20 09:38:09 -0400

    ROCK-4784 Improved check-in state serialization

---
+++ Plugins/org.secc.RedisSession/CheckInStateSessionManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rock.CheckIn;
+using static org.secc.RedisSession.Converters.CheckInStateConverter;
+
+namespace org.secc.RedisSession
+{
+    public static class CheckInStateSessionManager
+    {
+        private static ConcurrentDictionary<string, CheckInStateAnalog> Data { get; set; } = new ConcurrentDictionary<string, CheckInStateAnalog>();
+
+        public static void Set( CheckInStateAnalog stateAnalog )
+        {
+            Data.AddOrUpdate( stateAnalog.LocalKey, stateAnalog, ( l, analog ) => stateAnalog );
+            Clean();
+        }
+
+        public static CheckInStateAnalog Get( string localKey )
+        {
+            CheckInStateAnalog stateAnalog;
+            Data.TryGetValue( localKey, out stateAnalog );
+            return stateAnalog;
+        }
+
+        public static void Clean()
+        {
+            Task.Run( () =>
+            {
+                var keys = Data.Keys;
+                foreach(var key in keys )
+                {
+                    var item = Get( key );
+                    if (item.ExpiresDateTime < Rock.RockDateTime.Now )
+                    {
+                        Data.TryRemove( key, out item );
+                    }
+                }
+            }
+            );
+        }
+    }
+}
--- Plugins/org.secc.RedisSession/Converters/ObjectConverter.cs
+++ Plugins/org.secc.RedisSession/Converters/ObjectConverter.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Rock.CheckIn;
+using Rock.Model;
 using Rock.Web.Cache;
 
 namespace org.secc.RedisSession.Converters
@@ -13,6 +15,9 @@ namespace org.secc.RedisSession.Converters
     public class ObjectConverter : Newtonsoft.Json.JsonConverter
     {
         private readonly Type[] _types;
+
+        public Type IgnoreType { get; set; }
+
         public ObjectConverter( params Type[] types )
         {
             _types = types;
@@ -26,14 +31,20 @@ namespace org.secc.RedisSession.Converters
             serializerSettings.Converters.Add( new CacheItemConverter<DefinedTypeCache, Rock.Model.DefinedType>( typeof( DefinedTypeCache ) ) );
             serializerSettings.Converters.Add( new CacheItemConverter<DefinedValueCache, Rock.Model.DefinedValue>( typeof( DefinedValueCache ) ) );
 
-            foreach ( var converter in serializer.Converters )
-            {
-                // Don't include the Object Converter at this level
-                if ( converter.GetType() != GetType() )
-                {
-                    serializerSettings.Converters.Add( converter );
-                }
-            }
+            var objectConverter = new ObjectConverter( typeof( Group ),
+                                                       typeof( GroupType ),
+                                                       typeof( GroupTypeCache ),
+                                                       typeof( GroupType ),
+                                                       typeof( Location ),
+                                                       typeof( Schedule ),
+                                                       typeof( Person ),
+                                                       typeof( CheckInStatus ),
+                                                       typeof( CheckInFamily ),
+                                                       typeof( CheckInSchedule ),
+                                                       typeof( CheckInPerson ),
+                                                       typeof( CheckInLocation ) );
+            objectConverter.IgnoreType = value.GetType();
+            serializerSettings.Converters.Add( objectConverter );
 
             SessionObject sessionObject = new SessionObject();
             sessionObject.Type = value.GetType();
@@ -64,7 +75,7 @@ namespace org.secc.RedisSession.Converters
         }
         public override bool CanConvert( Type objectType )
         {
-            return _types.Any( t => t == objectType );
+            return objectType != IgnoreType && _types.Any( t => t == objectType );
         }
     }
 
