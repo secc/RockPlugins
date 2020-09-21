3274b6a74f12ca417a79a8fee44834c47b340979
Chuck Bump <cbump@secc.org>
2020-09-21 09:03:20 -0400

    ROCK-4900 - Adding FFmpeg and Image Montage actions for video and image manipulation.

---
+++ Plugins/org.secc.Workflow/Media/FFmpeg.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FFmpeg.NET;
+using FFmpeg.NET.Events;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Workflow;
+
+namespace org.secc.Workflow.media
+{
+    [ExportMetadata( "ComponentName", "FFmpeg" )]
+    [ActionCategory( "SECC > Media" )]
+    [Description( "Runs FFmpeg commands." )]
+    [Export( typeof( ActionComponent ) )]
+    [WorkflowTextOrAttribute( "File", "File Attribute", "The source file to use with FFmpeg.", true, "", "", 1, "File", new string[] { "Rock.Field.Types.TextFieldType" } )]
+    [TextField( "Command", "The FFmpeg command to run (parameters).  The {{file}} and {{outputPath}} merge fields are available here. <span class='tip tip-lava'></span>", true, "", "", 2, "Command" )]
+    [TextField( "FFmpeg Executable", "The FFmpeg executable location.", true, "C:\\Program Files\\ImageMagick-7.0.10-Q16-HDRI\\ffmpeg.exe", "", 3, "FFmpegExecutable" )]
+    [WorkflowAttribute( "Output Path", "An optional output path where the image montages should be stored.  If the attribute is empty a temporary directory will be created and the workflow attribute will be set to that directory.", true, "", "", 4, "OutputPath", new string[] { "Rock.Field.Types.TextFieldType" } )]
+
+    public class FFmpeg : ActionComponent
+    {
+        StringBuilder output = new StringBuilder();
+
+        public override bool Execute( RockContext rockContext, WorkflowAction action, object entity, out List<string> errorMessages )
+        {
+            errorMessages = new List<string>();
+
+            var file = GetAttributeValue( action, "File", true );
+            var command = GetAttributeValue( action, "Command" );
+            var executable = GetAttributeValue( action, "FFmpegExecutable" );
+
+            var outputPath = GetAttributeValue( action, "OutputPath", true );
+            if ( string.IsNullOrWhiteSpace(outputPath) )
+            {
+                outputPath = Path.Combine( Path.GetTempPath(), Path.GetRandomFileName() ) + Path.DirectorySeparatorChar;
+            } 
+            else
+            {
+                outputPath = outputPath.TrimEnd( Path.DirectorySeparatorChar ) + Path.DirectorySeparatorChar;
+            }
+
+            Directory.CreateDirectory( outputPath );
+
+            var mergeFields = GetMergeFields( action );
+            mergeFields.Add( "file", file );
+            mergeFields.Add( "outputPath", outputPath );
+            
+            Task.Run( async () => await StartConverting( command.ResolveMergeFields( mergeFields ), executable ) ).Wait();
+            action.AddLogEntry( output.ToString() );
+
+            // Now store the attribute
+            var targetAttribute = AttributeCache.Get( GetActionAttributeValue( action, "OutputPath" ).AsGuid(), rockContext );
+            if ( targetAttribute.EntityTypeId == new Rock.Model.Workflow().TypeId )
+            {
+                action.Activity.Workflow.SetAttributeValue( targetAttribute.Key, outputPath );
+            }
+            else if ( targetAttribute.EntityTypeId == new WorkflowActivity().TypeId )
+            {
+                action.Activity.SetAttributeValue( targetAttribute.Key, outputPath );
+            }
+
+            return true;
+        }
+
+        public async Task StartConverting( string command, string executable )
+        {
+
+
+            var ffmpeg = new Engine( executable );
+
+            ffmpeg.Progress += OnProgress;
+            ffmpeg.Data += OnData;
+            ffmpeg.Error += OnError;
+            ffmpeg.Complete += OnComplete;
+            await ffmpeg.ExecuteAsync( command );
+        }
+
+        private void OnProgress( object sender, ConversionProgressEventArgs e )
+        {
+            output.AppendFormat( "Bitrate: {0}\n", e.Bitrate );
+            output.AppendFormat( "Fps: {0}\n", e.Fps );
+            output.AppendFormat( "Frame: {0}\n", e.Frame );
+            output.AppendFormat( "ProcessedDuration: {0}\n", e.ProcessedDuration );
+            output.AppendFormat( "Size: {0} kb\n", e.SizeKb );
+            output.AppendFormat( "TotalDuration: {0}\n", e.TotalDuration );
+        }
+
+        private void OnData( object sender, ConversionDataEventArgs e )
+        {
+            output.AppendFormat( "{0}\n", e.Data );
+        }
+
+        private void OnComplete( object sender, ConversionCompleteEventArgs e )
+        {
+            output.AppendLine( "Done!\n" );
+        }
+
+        private void OnError( object sender, ConversionErrorEventArgs e )
+        {
+            output.AppendFormat( "Error: {0}\n{1}\n", e.Exception.ExitCode, e.Exception.InnerException );
+        }
+    }
+}
---
+++ Plugins/org.secc.Workflow/Media/ImageMontage.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.IO;
+using ImageMagick;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Workflow;
+
+namespace org.secc.Workflow.media
+{
+    [ExportMetadata( "ComponentName", "Image Montage" )]
+    [ActionCategory( "SECC > Media" )]
+    [Description( "Creates one or more JPG montages of image tiles.  Each montage file contain up to width*height images and will be sequentially numbered on output." )]
+    [Export( typeof( ActionComponent ) )]
+    [WorkflowTextOrAttribute( "Image Columns", "Image Columns Attribute", "How many images columns should be generated in each montage.", true, "5", "", 1, "Wide" )]
+    [WorkflowTextOrAttribute( "Image Rows", "Image Rows Attribute", "How many images rows should be generated in each montage.", true, "4", "", 2, "High" )]
+    [WorkflowTextOrAttribute( "Filename Prefix", "Filename Prefix Attribute", "What is the prefix name for each montage.", true, "montage", "", 3, "Filename" )]
+    [WorkflowTextOrAttribute( "Input Path", "Input Path Attribute", "The input path where the source images are saved.", true, "", "", 4, "InputPath", new string[] { "Rock.Field.Types.TextFieldType" } )]
+    [WorkflowTextOrAttribute( "Output Path", "Output Path Attribute", "The output path where the image montages should be stored.", true, "", "", 5, "OutputPath", new string[] { "Rock.Field.Types.TextFieldType" } )]
+    [WorkflowAttribute( "Output", "The output attribute where the a CSV string containing each image montage will be saved.", true, "", "", 6, "Output", new string[] { "Rock.Field.Types.TextFieldType" } )]
+
+    public class ImageMontage : ActionComponent
+    {
+
+        public override bool Execute( RockContext rockContext, WorkflowAction action, object entity, out List<string> errorMessages )
+        {
+            int wide = GetAttributeValue( action, "Wide", true ).AsInteger();
+            int high = GetAttributeValue( action, "High", true ).AsInteger();
+            string filename = GetAttributeValue( action, "Filename", true );
+            string inputPath = GetAttributeValue( action, "InputPath", true );
+            var outputPath = GetAttributeValue( action, "OutputPath", true ).TrimEnd( Path.DirectorySeparatorChar ) + Path.DirectorySeparatorChar;
+            Directory.CreateDirectory( outputPath );
+
+            errorMessages = new List<string>();
+            var outputFiles = new List<string>();
+
+            using ( var images = new MagickImageCollection() )
+            {
+                int i = 1;
+                int j = 0;
+                foreach ( string file in Directory.EnumerateFiles( inputPath, "*" ) )
+                {
+                    images.Add( new MagickImage( file ) );
+                    if ( i % ( wide * high) == 0)
+                    {
+                        outputFiles.Add( OutputMontage( filename, outputPath, images, j) );
+                        j++;
+                    }
+                    i++;
+                }
+
+                if (images.Count > 0)
+                {
+                    outputFiles.Add( OutputMontage( filename, outputPath, images, j ) );
+                }
+
+
+            }
+
+            // Now store the attribute
+            var targetAttribute = AttributeCache.Get( GetActionAttributeValue( action, "Output" ).AsGuid(), rockContext );
+            if ( targetAttribute.EntityTypeId == new Rock.Model.Workflow().TypeId )
+            {
+                action.Activity.Workflow.SetAttributeValue( targetAttribute.Key, outputFiles.JoinStrings(",") );
+            }
+            else if ( targetAttribute.EntityTypeId == new WorkflowActivity().TypeId )
+            {
+                action.Activity.SetAttributeValue( targetAttribute.Key, outputFiles.JoinStrings( "," ) );
+            }
+
+            return true;
+        }
+
+        private string OutputMontage( string filename, string outputPath, MagickImageCollection images, int j )
+        {
+            // Keep the original image's width/height in the Montage
+            var settings = new MontageSettings();
+            settings.Geometry = new MagickGeometry( images[0].Width, images[0].Height );
+            string outputfile = string.Format( "{0}{1}_{2}.jpg", outputPath, filename, j );
+
+            // Create a montage from the images
+            using ( var result = images.Montage( settings ) )
+            {
+                // Save the result
+                result.Write( outputfile );
+            }
+            images.Clear();
+            return outputfile;
+        }
+    }
+}
