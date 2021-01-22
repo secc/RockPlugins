875ea8b9e72a3924d28217f474653555615308ca
Mark Lee <mlee@secc.org>
2021-01-22 18:08:01 -0500

    ROCK-5169 Removed hard coded EMS password and general clean up

--- Plugins/org.secc.EMS/Events.cs
+++ Plugins/org.secc.EMS/Events.cs
@@ -13,40 +13,32 @@
 // </copyright>
 //
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace org.secc.EMS {
-    public class zoneEvent {
-        public zoneEvent(string HVACZone, DateTime TimeBookingStart, DateTime TimeEventEnd, string Rooms) {
+namespace org.secc.EMS
+{
+    public class zoneEvent
+    {
+        public zoneEvent( string HVACZone, DateTime TimeBookingStart, DateTime TimeEventEnd, string Rooms )
+        {
             this.HVACZone = HVACZone;
             this.TimeBookingStart = TimeBookingStart;
             this.TimeEventEnd = TimeEventEnd;
             this.Rooms = Rooms;
         }
 
-        //public zoneEvent(string HVACZone, DateTime TimeEventStart, DateTime TimeEventEnd, string Rooms) {
-        //    this.HVACZone = HVACZone;
-        //    this.TimeEventStart = TimeEventStart;
-        //    this.TimeEventEnd = TimeEventEnd;
-        //    this.Rooms = Rooms;
-        //}
-
         public string HVACZone;
         public DateTime TimeBookingStart;   //Reserved Start Time
         public DateTime TimeBookingEnd;   //Reserved End Time
         public DateTime TimeEventStart;   //Event Start Time
         public DateTime TimeEventEnd;       //Event End Time
-        //public DateTime OpenTime; //Building Open Time
-        //public DateTime CloseTime; //Building Close Time
-
         public string Rooms;
     }
 
-    public class webEvent {
+    public class webEvent
+    {
         public webEvent() { }
-        public webEvent( DateTime timeBookingStart, DateTime timeBookingEnd, DateTime timeEventStart, DateTime timeEventEnd, string activityName, string locationName, bool? displayOnWeb) {
+        public webEvent( DateTime timeBookingStart, DateTime timeBookingEnd, DateTime timeEventStart, DateTime timeEventEnd, string activityName, string locationName, bool? displayOnWeb )
+        {
             this.TimeBookingStart = timeBookingStart;
             this.TimeBookingEnd = timeBookingEnd;
             this.TimeEventStart = timeEventStart;
@@ -56,8 +48,9 @@ namespace org.secc.EMS {
             this.DisplayOnWeb = displayOnWeb;
         }
 
-        public webEvent( DateTime timeBookingStart, DateTime timeBookingEnd, DateTime timeEventStart, DateTime timeEventEnd, string activityName, string locationName) : 
-            this(timeBookingStart, timeBookingEnd, timeEventStart, timeEventEnd, activityName, locationName, null){
+        public webEvent( DateTime timeBookingStart, DateTime timeBookingEnd, DateTime timeEventStart, DateTime timeEventEnd, string activityName, string locationName ) :
+            this( timeBookingStart, timeBookingEnd, timeEventStart, timeEventEnd, activityName, locationName, null )
+        {
         }
 
         public DateTime TimeBookingStart { get; set; }
