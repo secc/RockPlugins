0acef360ca5ce1fcf7de9c363adddcee0f1b6db5
Mark Lee <mlee@secc.org>
2021-06-26 14:45:21 -0400

    ROCK-5471 Added tool to crop photos and add them to profiles

---
+++ Plugins/org.secc.Imaging/org_secc/Imaging/UpdatePersonImage.ascx
@@ -0,0 +1,9 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="UpdatePersonImage.ascx.cs" Inherits="RockWeb.Plugins.org_secc.Imaging.UpdatePersonImage" %>
+
+<asp:UpdatePanel runat="server">
+    <ContentTemplate>
+        <Rock:CodeEditor runat="server" EditorMode="Sql" ID="ceQuery"
+            Help="Return colums of [PersonId] and [BinaryFileId]" Label="SQL Query" />
+        <Rock:BootstrapButton runat="server" ID="btnRun" OnClick="btnRun_Click" Text="Run" CssClass="btn btn-primary" />
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Plugins/org.secc.Imaging/org_secc/Imaging/UpdatePersonImage.ascx.cs
@@ -0,0 +1,88 @@
+﻿// <copyright>
+// Copyright Southeast Christian Church
+//
+// Licensed under the  Southeast Christian Church License (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License shoud be included with this file.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.UI;
+using Rock;
+using Rock.Attribute;
+using Rock.Communication;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.UI.Controls;
+using System.Web;
+using System.Security.Claims;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
+using org.secc.Imaging.AI;
+
+namespace RockWeb.Plugins.org_secc.Imaging
+{
+    /// <summary>
+    /// OAuth login prompts user for login credentials.
+    /// </summary>
+    [DisplayName( "Update Person Image" )]
+    [Category( "SECC > Imaging" )]
+    [Description( "Uses sql to update person images from binary files cropping the images with AI" )]
+
+    public partial class UpdatePersonImage : Rock.Web.UI.RockBlock
+    {
+        #region Base Control Methods
+
+
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+
+        }
+
+        #endregion
+
+        protected void btnRun_Click( object sender, EventArgs e )
+        {
+            FaceCrop face = new FaceCrop();
+
+            var sql = ceQuery.Text;
+
+            RockContext rockContext = new RockContext();
+            var personService = new PersonService( rockContext );
+            var binaryFileService = new BinaryFileService( rockContext );
+            var rows = rockContext.Database.SqlQuery<ResponseData>( sql );
+            foreach ( var row in rows )
+            {
+                var person = personService.GetNoTracking( row.PersonId );
+                var binaryFile = binaryFileService.GetNoTracking( row.BinaryFileId );
+                if ( person != null && binaryFile != null )
+                {
+                    face.UpdatePhoto(person,binaryFile);
+                }
+            }
+
+        }
+
+        class ResponseData
+        {
+            public int PersonId { get; set; }
+            public int BinaryFileId { get; set; }
+        }
+
+    }
+}
