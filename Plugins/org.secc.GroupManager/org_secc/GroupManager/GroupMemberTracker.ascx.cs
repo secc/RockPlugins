891ee83def5283c7b78b4e187fce24c4ce91bcfc
Chris Funk <cfunk@secc.org>
2022-08-17 12:27:09 -0500

    Update GM Tracker

--- Plugins/org.secc.GroupManager/org_secc/GroupManager/GroupMemberTracker.ascx.cs
+++ Plugins/org.secc.GroupManager/org_secc/GroupManager/GroupMemberTracker.ascx.cs
@@ -21,9 +21,12 @@ namespace RockWeb.Plugins.org_secc.GroupManager
     [Description( "Allows a group leader confirm that a member attended after checking in." )]
 
     [BooleanField( "Display Group Leaders", "Show Group Leaders in attendee List", false )]
-    [BooleanField( "Add unassigned people", "Allows user to confirm unassigned people who came to their group.", false )]
+
     public partial class GroupMemberTracker : GroupManagerBlock
     {
+
+        GroupOccurrence currentOccurrence = null;
+
         protected override void OnInit( EventArgs e )
         {
             base.OnInit( e );
@@ -49,7 +52,19 @@ namespace RockWeb.Plugins.org_secc.GroupManager
                 return;
             }
 
+            var attendanceOccurrence = GetAttendanceOccurrenceByGroupId( CurrentGroup.Id );
+
+            if ( attendanceOccurrence == default( AttendanceOccurrence ) )
+            {
+                DisplayNoOccurrenceMessage();
+                return;
+            }
+
+            LoadCurrentOccurrence();
+
+
             pnlMain.Visible = true;
+
 
             if ( !IsPostBack )
             {
@@ -60,9 +75,49 @@ namespace RockWeb.Plugins.org_secc.GroupManager
         private void DisplayNotAuthorizedMessage()
         {
             nbMain.Text = "<strong><<i class='far fa-exclamation-triangle'></i> Not Authorized</strong> You are not authorized to view this group.";
+            nbMain.NotificationBoxType = NotificationBoxType.Warning;
             nbMain.Visible = true;
 
         }
+
+        private void DisplayNoOccurrenceMessage()
+        {
+            nbMain.Text = "<strong><i class='far fa-exclamation-triangle'></i> No Check-ins</strong> No Check-ins found.";
+            nbMain.NotificationBoxType = NotificationBoxType.Info;
+            nbMain.Visible = true;
+        }
+
+        private void LoadCurrentOccurrence()
+        {
+            var today = RockDateTime.Now.Date;
+            using ( var rockContext = new RockContext() )
+            {
+                var occurrence = new AttendanceOccurrenceService( rockContext )
+                    .Queryable().AsNoTracking()
+                    .Where( o => o.OccurrenceDate == today )
+                    .Where( o => o.GroupId == CurrentGroup.Id )
+                    .Select( o => new GroupOccurrence
+                    {
+                        Id = o.Id,
+                        GroupId = o.GroupId,
+                        ScheduleId = o.ScheduleId,
+                        LocationId = o.LocationId,
+                        OccurrenceDate = o.OccurrenceDate,
+                        OccurrenceAttendees = o.Attendees.Select( a => new OccurrenceAttendeeItem
+                        {
+                            Id = a.Id,
+                            PersonAliasId = a.PersonAliasId,
+                            PersonId = a.PersonAlias != null ? a.PersonAlias.PersonId : (int?)null,
+                            DidAttend = a.DidAttend == null ? false : true,
+                            StartDateTime = a.StartDateTime,
+                            EndDateTime = a.EndDateTime
+                        } )
+                    } ).FirstOrDefault();
+                ;
+
+            }
+        }
+
 
         private void LoadGroupMembers()
         {
@@ -74,7 +129,18 @@ namespace RockWeb.Plugins.org_secc.GroupManager
 
             var includeLeaders = GetAttributeValue( "DisplayGroupLeaders" ).AsBoolean();
 
+
             var groupMembersQry = CurrentGroupMembers
+                .GroupJoin( currentOccurrence.OccurrenceAttendees, gm => gm.PersonId, a => a.PersonId,
+                    (gm, o) => new
+                    {
+                        Id = gm.Id,
+                        PersonId = gm.PersonId,
+                        Person = gm.Person,
+                        GroupRole = gm.GroupRole,
+                        AttendanceId = o.FirstOrDefault() == null ? (int?)null : o.FirstOrDefault().Id,
+                        AttendanceState = o.FirstOrDefault() == null ? AttendanceState.CheckedOut : o.FirstOrDefault().AttendanceState
+                    } )
                 .GroupJoin( mobilePhoneQry, gm => gm.PersonId, mp => mp.PersonId,
                     ( gm, mp ) => new { GroupMember = gm, MobilePhone = mp.DefaultIfEmpty() } );
 
@@ -83,7 +149,6 @@ namespace RockWeb.Plugins.org_secc.GroupManager
                 groupMembersQry = groupMembersQry.Where( gm => !gm.GroupMember.GroupRole.IsLeader );
             }
 
-            var gm1 = groupMembersQry.ToList();
 
             var groupMembers = groupMembersQry
                 .Select( gm => new GroupMemberInfo
@@ -94,7 +159,8 @@ namespace RockWeb.Plugins.org_secc.GroupManager
                     LastName = gm.GroupMember.Person.LastName,
                     MobilePhone = gm.MobilePhone.FirstOrDefault() != null ? gm.MobilePhone.FirstOrDefault().NumberFormatted : String.Empty,
                     PhotoUrl = gm.GroupMember.Person.PhotoUrl,
-                    AttendanceStateValue = "Checked Out"
+                    State = gm.GroupMember.AttendanceState,
+                    AttendanceId = gm.GroupMember.AttendanceId    
                 } )
                 .OrderBy( gm => gm.NameReversed )
                 .ToList();
@@ -102,6 +168,16 @@ namespace RockWeb.Plugins.org_secc.GroupManager
             rGroupMember.DataSource = groupMembers;
             rGroupMember.DataBind();
 
+        }
+
+        private AttendanceOccurrence GetAttendanceOccurrenceByGroupId(int groupId)
+        {
+            var today = RockDateTime.Now.Date;
+            return new AttendanceOccurrenceService( new RockContext() )
+                .Queryable()
+                .Where( o => o.GroupId == groupId )
+                .Where( o => o.OccurrenceDate == today )
+                .FirstOrDefault();
         }
 
         protected void rGroupMember_ItemDataBound( object sender, System.Web.UI.WebControls.RepeaterItemEventArgs e )
@@ -142,9 +218,24 @@ namespace RockWeb.Plugins.org_secc.GroupManager
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public AttendanceState? State { get; set; } = AttendanceState.CheckedOut;
-            public string AttendanceStateValue { get; set; } 
+            public string AttendanceStateValue
+            {
+                get
+                {
+                    switch ( State )
+                    {
+                        case AttendanceState.InRoom:
+                            return "Checked In";
+                        case AttendanceState.EnRoute:
+                            return "EnRoute";
+                        default:
+                            return "Checked Out";
+                    }
+                }
+            }
             public string MobilePhone { get; set; }
             public string PhotoUrl { get; set; }
+            public int? AttendanceId { get; set; }
 
             public string Name
             {
@@ -159,6 +250,45 @@ namespace RockWeb.Plugins.org_secc.GroupManager
                 get
                 {
                     return $"{LastName}, {FirstName}";
+                }
+            }
+        }
+
+        class GroupOccurrence
+        {
+            public int Id { get; set; }
+            public int? GroupId { get; set; }
+            public int? ScheduleId { get; set; }
+            public int? LocationId { get; set; }
+            public DateTime OccurrenceDate { get; set; }
+            public IEnumerable<OccurrenceAttendeeItem> OccurrenceAttendees { get; set; }
+
+        }
+
+        class OccurrenceAttendeeItem
+        {
+            public int Id { get; set; }
+            public int? PersonAliasId { get; set; }
+            public int? PersonId { get; set; }
+            public bool DidAttend { get; set; }
+            public int? QualifierValueId { get; set; }
+            public DateTime StartDateTime { get; set; }
+            public DateTime? EndDateTime { get; set; }
+            public AttendanceState AttendanceState
+            {
+                get
+                {
+                    if(EndDateTime.HasValue)
+                    {
+                        return AttendanceState.CheckedOut;
+                    }
+                    if ( DidAttend == true )
+                    {
+                        return AttendanceState.InRoom;
+                    }
+                    return AttendanceState.EnRoute;
+                    
+
                 }
             }
         }
