a28c259ee3bc7393c0a6630c3467cb099b07b5a5
Chris Funk <cfunk@secc.org>
2023-08-29 14:25:18 -0500

    ROCK-6360 - Worship Analytics
    Load worship schedules and form

---
+++ Plugins/org.secc.Reporting/org_secc/Reporting/AttendanceQuickEntry.ascx.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+
+namespace RockWeb.Plugins.org_secc.Reporting
+
+{
+    [DisplayName( "Worship Attendance Entry" )]
+    [Category( "SECC > Reporting" )]
+    [Description( "Block for campuses to quickly enter their worship service attenance." )]
+
+    [MetricField( "Worship Attendance Metric",
+        Description = "The weekly metric for worship attendance.",
+        IsRequired = true,
+        Order = 0,
+        Key = AttributeKey.WorshipAttendance )]
+    [TextField( "Title",
+        Description = "Panel Title",
+        IsRequired = false,
+        DefaultValue = "Worship Attendance Entry",
+        Order = 1,
+        Key = AttributeKey.PanelTitle )]
+    [CategoryField( "Worship Attendance Parent Category",
+        Description = "Parent Schedule Category for Weekly Worship Service Attendance.",
+        EntityTypeName = "Rock.Model.Schedule",
+        IsRequired = true,
+        Order = 2,
+        Key = AttributeKey.ScheduleCategory )]
+    [AttributeField( "Schedule Campus Attribute",
+        Description = "Attribute indicating which campus(es) that a schedule belongs to.",
+        EntityTypeGuid = "0b2c38a7-d79c-4f85-9757-f1b045d32c8a",
+        IsRequired = true,
+        Key = AttributeKey.ScheduleCampus,
+        Order = 3 )]
+
+    public partial class AttendanceQuickEntry : RockBlock
+    {
+        public static class AttributeKey
+        {
+            public const string PanelTitle = "PanelTitle";
+            public const string ScheduleCategory = "ScheduleCategory";
+            public const string WorshipAttendance = "WorshipAttendance";
+            public const string ScheduleCampus = "ScheduleCampus";
+        }
+
+        CampusCache _selectedCampus = null;
+        string _serviceCachceKey;
+
+        Metric _worshipAttendanceMetric = null;
+        List<WorshipScheduleSummary> _worshipSchedules = null;
+
+
+        #region Properties
+        private CampusCache SelectedCampus
+        {
+            get
+            {
+                if (_selectedCampus == null)
+                {
+                    SetCampus( false );
+                }
+
+                return _selectedCampus;
+            }
+            set
+            {
+                _selectedCampus = value;
+            }
+        }
+
+        private Metric WorshipAttendanceMetric
+        {
+            get
+            {
+                if (_worshipAttendanceMetric == null)
+                {
+                    LoadMetric();
+                }
+                return _worshipAttendanceMetric;
+            }
+        }
+
+        private List<WorshipScheduleSummary> WorshipSchedules
+        {
+            get
+            {
+                if (_worshipSchedules == null)
+                {
+                    LoadSchedules();
+                }
+                return _worshipSchedules;
+            }
+        }
+
+
+
+
+        #endregion
+
+        #region "Base Control Methods"
+
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+            _serviceCachceKey = $"WorshipServiceTimes_Page{CurrentPageReference.PageId}";
+            lbSubmit.Click += lbSubmit_Click;
+            lbReset.Click += lbReset_Click;
+            ddlSchedule.SelectedIndexChanged += ddlSchedule_SelectedIndexChanged;
+        }
+
+
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if (!Page.IsPostBack)
+            {
+                SetCampus( true );
+                LoadForm();
+            }
+ 
+            pnlEntry.Visible = true;
+        }
+
+        #endregion
+        #region Events
+        private void ddlSchedule_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            var scheduleID = ddlSchedule.SelectedValueAsInt();
+
+            if(scheduleID.HasValue)
+            {
+                var schedule = WorshipSchedules.SingleOrDefault( s => s.ScheduleId == scheduleID && s.CampusId == SelectedCampus.Id );
+
+                tbAttendance.Text = schedule.Attendance.HasValue ? schedule.Attendance.ToString() : String.Empty;
+
+            }
+        }
+
+        private void lbReset_Click( object sender, EventArgs e )
+        {
+            throw new NotImplementedException();
+        }
+
+        private void lbSubmit_Click( object sender, EventArgs e )
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Internal Methods
+
+        private int? GetAttendance( DateTime serviceDate, int campusId, int scheduleId )
+        {
+            using (var rockContext = new RockContext())
+            {
+                var campusEntityType = EntityTypeCache.Get( typeof( Rock.Model.Campus ) );
+                var scheduleEntityType = EntityTypeCache.Get( typeof( Rock.Model.Schedule ) );
+                var sundayDate = serviceDate.SundayDate();
+
+                var valueService = new MetricValueService( rockContext );
+                var metricValue = valueService.Queryable().AsNoTracking()
+                    .Where( v => v.MetricId == WorshipAttendanceMetric.Id )
+                    .Where( v => v.MetricValueDateTime == sundayDate )
+                    .Where( v => v.MetricValuePartitions.Where( p => p.MetricPartition.EntityTypeId == campusEntityType.Id ).Select( p => p.EntityId ).Contains( campusId ) )
+                    .Where( v => v.MetricValuePartitions.Where( p => p.MetricPartition.EntityTypeId == scheduleEntityType.Id ).Select( p => p.EntityId ).Contains( scheduleId ) )
+                    .Select( v => v.YValue )
+                    .FirstOrDefault();
+
+                return (int?) metricValue;
+            }
+
+        }
+
+        private void LoadForm()
+        {
+            lCampus.Text = SelectedCampus.Name;
+            lDate.Text = RockDateTime.Now.ToShortDateString();
+
+            var campusSchedules = WorshipSchedules.Where( s => s.CampusId == SelectedCampus.Id ).ToList();
+
+            ddlSchedule.Items.Clear();
+            ddlSchedule.Items.Add( new ListItem( string.Empty, string.Empty ) );
+            foreach (var item in campusSchedules)
+            {
+                var listItem = new ListItem( $"{item.StartDateTime: h:mm tt}", item.ScheduleId.ToString() );
+                ddlSchedule.Items.Add( listItem );
+            }
+            tbAttendance.Text = string.Empty;
+
+        }
+
+        private void LoadMetric()
+        {
+            using (var rockContext = new RockContext())
+            {
+                var metricGuid = GetAttributeValue( AttributeKey.WorshipAttendance ).AsGuid();
+
+                _worshipAttendanceMetric = new MetricService( rockContext ).Get( metricGuid );
+            }
+        }
+
+        private void LoadSchedules()
+        {
+            
+
+            var cachedValue = RockCache.Get( _serviceCachceKey, true ) as List<WorshipScheduleSummary>;
+
+            if(cachedValue !=  null)
+            {
+                _worshipSchedules = cachedValue;
+                return;
+            }
+            
+
+            var rockContext = new RockContext();
+            var baseCategoryGuid = GetAttributeValue( AttributeKey.ScheduleCategory ).AsGuid();
+
+            var categoryService = new CategoryService( rockContext );
+            var scheduleService = new ScheduleService( rockContext );
+            var categoryIds = categoryService.GetAllDescendents( baseCategoryGuid )
+                .Select( c => c.Id ).ToList();
+
+            var campusAttributeGuid = GetAttributeValue( AttributeKey.ScheduleCampus ).AsGuid();
+            var campusAttributeValues = new AttributeValueService( rockContext ).Queryable().AsNoTracking()
+                .Where( v => v.Attribute.Guid == campusAttributeGuid );
+
+
+            var rockSchedules = scheduleService.Queryable().AsNoTracking()
+                .Join( campusAttributeValues, s => s.Id, v => v.EntityId,
+                    ( s, v ) => new { Schedule = s, CampusValue = v.Value } )
+                .Where( s => categoryIds.Contains( s.Schedule.CategoryId ?? -1 ) )
+                .Where( s => s.Schedule.IsActive )
+                .Where( s => s.CampusValue != null && s.CampusValue != "" )
+                .ToList();
+
+            var worshipSchedule = new List<WorshipScheduleSummary>();
+
+            foreach (var schedule in rockSchedules)
+            {
+                var todaysOccurrences = schedule.Schedule
+                    .GetScheduledStartTimes( RockDateTime.Today, RockDateTime.Today.AddDays( 1 ).AddSeconds( -1 ) );
+                if (todaysOccurrences.Count == 0)
+                {
+                    continue;
+                }
+                foreach (var campusGuid in schedule.CampusValue.SplitDelimitedValues())
+                {
+                    var service = new WorshipScheduleSummary()
+                    {
+                        ScheduleId = schedule.Schedule.Id,
+                        CampusId = CampusCache.Get( campusGuid ).Id,
+                        WorshipSchedule = schedule.Schedule,
+                        StartDateTime = todaysOccurrences.First(),
+                    };
+
+                    service.Attendance = GetAttendance( service.StartDateTime, service.CampusId, service.ScheduleId );
+
+                    worshipSchedule.Add( service );
+
+                }
+            }
+
+            var expirationMinutes = 60;
+            //expire cache before midnight
+            if(RockDateTime.Now.AddMinutes(expirationMinutes) > RockDateTime.Today.AddDays(1))
+            {
+                expirationMinutes = RockDateTime.Today.AddDays( 1 ).Subtract( Rock.RockDateTime.Now ).Minutes;
+            }
+
+            _worshipSchedules = worshipSchedule;
+
+            RockCache.AddOrUpdate( _serviceCachceKey, "", worshipSchedule, new TimeSpan(0, expirationMinutes, 0) );
+
+
+        }
+
+        private void SetCampus( bool reload )
+        {
+            var campusKey = $"{CurrentPageReference.PageId}_CampusId";
+            int? campusId = null;
+
+            if (reload)
+            {
+                ViewState.Remove( campusKey );
+            }
+
+
+            campusId = (int?) ViewState[campusKey];
+
+            if (!campusId.HasValue)
+            {
+                campusId = PageParameter( "Campus" ).AsInteger();
+                ViewState[campusKey] = campusId;
+            }
+
+            if (!campusId.HasValue)
+            {
+                SelectedCampus = null;
+            }
+            else
+            {
+                SelectedCampus = CampusCache.Get( campusId.Value );
+            }
+        }
+
+
+        #endregion
+    }
+
+    class WorshipScheduleSummary
+    {
+        public int ScheduleId { get; set; }
+        public int CampusId { get; set; }
+        public Schedule WorshipSchedule { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public int? Attendance { get; set; }
+
+    }
+}
