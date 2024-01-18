e7fe8f4a8c2da5f853334d7be16774a84beb208b
Chris Funk <cfunk@secc.org>
2024-01-18 13:03:13 -0600

    Rock-6636 carousel test

---
+++ Plugins/org.secc.Event/org_secc/Event/EventPass.ascx
@@ -0,0 +1,66 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="EventPass.ascx.cs" Inherits="RockWeb.Plugins.org_secc.Event.EventPass" %>
+<style>
+    .QRImage {
+        text-align:center;
+    }
+    .QRImage img {
+        height:250px;
+        width:250px;
+    }
+    img.PersonPhoto
+    {
+        display:block;
+        height:150px;
+        width:150px;
+        margin-left:auto;
+        margin-right:auto;
+        
+    }
+    .personHeader {
+        background-color:#000000;
+        color:#ffffff;
+        font-size:2em;
+        font-weight:bold;
+        text-align:center;
+    }
+
+</style>
+<asp:UpdatePanel ID="upMain" runat="server">
+    <ContentTemplate>
+        <asp:Panel ID="pnlPass" runat="server" Visible="true" >
+            <div Id="carousel-EventItem" class="carousel slide" data-ride="carousel" data-interval="false">
+                <div class="carousel-inner" role="listbox">
+                    <asp:Repeater ID="rPasses" runat="server" >
+                        <ItemTemplate>
+                            <asp:Panel ID="pnlItem" runat="server" CssClass="item">
+                                <div class="col-xs-12 personHeader">
+                                    <%# Eval("RegistrantPerson.FullName") %>
+                                </div>
+                                <div class="col-xs-6">
+                                    <img src="<%# Eval("RegistrantPerson.PhotoUrl") %>" style="width:150px" />
+                                </div>
+                                <div class="col-xs-6">
+                                    <i class="fas fa-ticket"></i> <%# Eval("EventName") %> <br />
+                                    <i class="far fa-calendar-alt"></i> <%# ((DateTime)Eval("EventDate")).ToLongDateString() %> <br />
+                                    <i class="far fa-map-marker"></i> <%# Eval("EventLocation") %>
+                                </div>
+                                <div class="col-xs-12 QRImage">
+                                    <p>Please scan the QR Code to check-in at the event</p>
+                                    <img src='<%# Eval("QRUrl") %>' />
+                                </div>
+                            </asp:Panel>
+                        </ItemTemplate>
+                    </asp:Repeater>
+                </div>
+                <a class="left carousel-control" href="#carousel-EventItem" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-EventItem" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Plugins/org.secc.Event/org_secc/Event/EventPass.ascx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Model;
+using Rock.Web;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Rock.Web.UI;
+using Rock.Data;
+
+
+
+namespace RockWeb.Plugins.org_secc.Event
+{
+    [DisplayName("Event Pass")]
+    [Category("SECC > Event")]
+    [Description("Displays a QR pass for registrants of an event.")]
+
+    public partial class EventPass : RockBlock
+    {
+
+        bool isActiveItemSet = false;
+
+        protected override void OnInit( EventArgs e )
+        {
+
+            rPasses.ItemDataBound += rPasses_ItemDataBound;
+            string script = @"$('.carousel').carousel();";
+            ScriptManager.RegisterClientScriptBlock( upMain, upMain.GetType(), "Carousel" + RockDateTime.Now.Ticks, script, true );
+
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+            LoadPasses();
+        }
+
+        private void rPasses_ItemDataBound( object sender, RepeaterItemEventArgs e )
+        {
+            if(!isActiveItemSet)
+            {
+                var pnlItem = e.Item.FindControl( "pnlItem" ) as Panel;
+                pnlItem.CssClass = pnlItem.CssClass + " active";
+                isActiveItemSet = true;
+            }
+        }
+
+        private void LoadPasses()
+        {
+            var qrBasePath = GlobalAttributesCache.Value( "PublicApplicationRoot" ) + "GetQRCode.ashx?data=";
+            var personIds = new int[] { 172382, 243950, 255085 };
+            var alternateIdDV = DefinedValueCache.Get( Rock.SystemGuid.DefinedValue.PERSON_SEARCH_KEYS_ALTERNATE_ID.AsGuid() );
+
+            var rockContext = new RockContext();
+            var personService = new PersonService( rockContext );
+
+            var personSearchKeyQry = new PersonSearchKeyService( rockContext ).Queryable().AsNoTracking()
+                .Where( k => k.SearchTypeValueId == alternateIdDV.Id );
+
+  
+            var passes = new List<EventPassData>();
+            foreach (var personId in personIds)
+            {
+                var person = personService.Get( personId );
+                var alternateId = personSearchKeyQry.Where( k => k.PersonAlias.PersonId == person.Id )
+                    .Select( k => k.SearchValue ).FirstOrDefault();
+
+                var eventPassData = new EventPassData
+                {
+                    RegistrationTemplateId = 1,
+                    RegistrationInstanceId = 1,
+                    RegistrationId = 1,
+                    RegistrantId = 1,
+                    RegistrantPerson = person,
+                    EventName = "All Staff",
+                    EventDate = new DateTime( 2024, 2, 13, 9, 30, 0 ),
+                    EventLocation = "The Block",
+                    QRUrl = $"{qrBasePath}{alternateId}"
+
+                };
+
+                passes.Add( eventPassData );
+            }
+
+            isActiveItemSet = false;
+
+            rPasses.DataSource = passes;
+            rPasses.DataBind();
+
+            rockContext = null;
+
+        }
+
+        public class EventPassData
+        {
+            public int RegistrationTemplateId { get; set; }
+            public int RegistrationInstanceId { get; set; }
+            public int RegistrationId { get; set; }
+            public int RegistrantId { get; set; }
+            public Person RegistrantPerson { get; set; }
+            public string EventName { get; set; }
+            public DateTime? EventDate { get; set; }
+            public string EventLocation { get; set; }
+            public string QRUrl { get; set; }
+
+        }
+    }
+}
