de07628b0344c9562a7cf4ae20641eeaf0f7053f
stphnlee <slee@secc.org>
2024-06-13 17:13:41 -0400

    ROCK-6944 Re-added attendance occurrence API endpoint

--- Plugins/org.secc.Rest/Controllers/GroupAppAttendanceController.Partial.cs
+++ Plugins/org.secc.Rest/Controllers/GroupAppAttendanceController.Partial.cs
@@ -12,6 +12,8 @@
 // limitations under the License.
 // </copyright>
 using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Http;
@@ -113,7 +115,6 @@ namespace org.secc.Rest.Controllers
             if ( !group.IsAuthorized( Rock.Security.Authorization.EDIT, currentUser.Person ) || !group.IsAuthorized( Rock.Security.Authorization.MANAGE_MEMBERS, currentUser.Person ))
                 return StatusCode( HttpStatusCode.Forbidden );
 
-            scheduleId = scheduleId.HasValue ? scheduleId.Value : group.ScheduleId;
             locationId = locationId.HasValue ? locationId.Value : group.GroupLocations?.FirstOrDefault()?.Id;
 
             var groupMember = new GroupMemberService( _context ).Get( groupMemberId );
@@ -122,8 +123,13 @@ namespace org.secc.Rest.Controllers
             var personAliasService = new PersonAliasService( _context );
 
             var attendanceData = attendanceService
-                        .Queryable( "PersonAlias" )
-                        .Where( a => a.Occurrence.GroupId == groupId && a.Occurrence.LocationId == locationId && a.Occurrence.ScheduleId == scheduleId && a.Occurrence.OccurrenceDate == occurrenceDate );
+                .Queryable( "PersonAlias" )
+                .Where( a => a.Occurrence.GroupId == groupId && a.Occurrence.LocationId == locationId && a.Occurrence.OccurrenceDate == occurrenceDate );
+
+            if ( scheduleId.HasValue )
+            {
+                attendanceData = attendanceData.Where( a => a.Occurrence.ScheduleId == scheduleId );
+            }
 
             var attendanceItem = attendanceData.Where( a => a.PersonAlias.PersonId == groupMember.Person.Id )
                                 .FirstOrDefault();
@@ -150,6 +156,188 @@ namespace org.secc.Rest.Controllers
             _context.SaveChanges();
 
             return Ok();
+        }
+
+        /// <summary>
+        /// Returns the possible schedules for the provided group ID
+        /// </summary>
+        /// <param name="groupId">The group ID</param>
+        /// <returns>List<Group></returns> 
+        [HttpGet]
+        [System.Web.Http.Route( "api/GroupApp/AttendanceSchedules/{groupId}" )]
+        public IHttpActionResult GetAttendanceSchedules( int groupId )
+        {
+            var currentUser = UserLoginService.GetCurrentUser();
+            if ( currentUser == null )
+                return StatusCode( HttpStatusCode.Unauthorized );
+
+            var group = new GroupService( _context ).Get( groupId );
+            if ( group == null )
+                return NotFound();
+
+            if ( !group.IsAuthorized( Rock.Security.Authorization.VIEW, currentUser.Person ) )
+                return StatusCode( HttpStatusCode.Forbidden );
+
+            if ( group.Schedule == null )
+                return NotFound();
+
+            var groupScheduleOccurrences = GetListOfOccurrences( group );
+
+            return Ok( groupScheduleOccurrences );
+        }
+
+        private List<GroupScheduleOccurence> GetListOfOccurrences( Group group )
+        {
+            RockContext rockContext = _context;
+            var startDate = RockDateTime.Today.AddYears( -1 );
+            var enddate = RockDateTime.Today.AddDays( 1 );
+
+            var existingOccurrences = new AttendanceOccurrenceService( rockContext )
+                .Queryable()
+                .Include( o => o.Schedule )
+                .AsNoTracking()
+                .Where( g => g.GroupId == group.Id )
+                .Where( o => o.OccurrenceDate >= startDate && o.OccurrenceDate < enddate )
+                .ToList();
+
+            var occurrences = new List<GroupScheduleOccurence>();
+
+            if ( group.Schedule != null )
+            {
+                if ( group.Schedule.ScheduleType == ScheduleType.Custom || group.Schedule.ScheduleType == ScheduleType.Named )
+                {
+                    var previousScheduleDates = group.Schedule.GetScheduledStartTimes( startDate, enddate )
+                        .OrderByDescending( o => o )
+                        .Take( 50 )
+                        .ToList();
+
+
+                    occurrences.AddRange( previousScheduleDates
+                        .Select( p => new GroupScheduleOccurence
+                        {
+                            OccurrenceDate = p,
+                            GroupId = group.Id,
+                            ScheduleId = group.ScheduleId,
+                            LocationId = group.GroupLocations.Any() ? ( int? ) group.GroupLocations.Select( l => l.LocationId ).FirstOrDefault() : null,
+                            StartDateTime = group.Schedule.GetNextStartDateTime( p )
+                        } )
+                        .ToList() );
+
+
+
+                    foreach ( var occurrence in existingOccurrences )
+                    {
+                        var selectedOccurrence = occurrences
+                            .Where( o => o.OccurrenceDate.Date == occurrence.OccurrenceDate.Date )
+                            .Where( o => o.ScheduleId == occurrence.ScheduleId )
+                            .FirstOrDefault();
+
+                        if ( selectedOccurrence != null )
+                        {
+                            selectedOccurrence.OccurrenceId = occurrence.Id;
+                            if ( !selectedOccurrence.LocationId.HasValue )
+                            {
+                                selectedOccurrence.LocationId = occurrence.LocationId;
+                            }
+                        }
+                    }
+                }
+                else if ( group.Schedule.ScheduleType == ScheduleType.Weekly )
+                {
+                    var lastSchedule = RockDateTime.Today;
+                    while ( lastSchedule.DayOfWeek != group.Schedule.WeeklyDayOfWeek )
+                    {
+                        lastSchedule = lastSchedule.AddDays( -1 );
+                    }
+
+                    while ( lastSchedule > startDate )
+                    {
+                        occurrences.Add( new GroupScheduleOccurence
+                        {
+                            OccurrenceDate = lastSchedule,
+                            GroupId = group.Id,
+                            ScheduleId = group.ScheduleId,
+                            LocationId = group.GroupLocations.Any() ? ( int? ) group.GroupLocations.Select( l => l.LocationId ).FirstOrDefault() : null,
+                            StartDateTime = lastSchedule.Add( group.Schedule.WeeklyTimeOfDay ?? new TimeSpan( 0, 0, 0 ) )
+                        } );
+
+                        lastSchedule = lastSchedule.AddDays( -7 );
+                    }
+
+                    foreach ( var occurrence in existingOccurrences )
+                    {
+                        var selectedOccurrence = occurrences
+                            .Where( o => o.OccurrenceDate == occurrence.OccurrenceDate )
+                            .Where( o => o.LocationId == occurrence.LocationId )
+                            .Where( o => o.ScheduleId == occurrence.ScheduleId )
+                            .FirstOrDefault();
+                        if ( selectedOccurrence != null )
+                        {
+                            selectedOccurrence.OccurrenceId = occurrence.Id;
+                        }
+                    }
+                }
+            }
+
+            var tmpOccurrence = existingOccurrences
+                .Where( o => !occurrences.Select( o1 => o1.OccurrenceId ).Contains( o.Id ) )
+                .ToList();
+
+            foreach ( var occ in tmpOccurrence )
+            {
+                var summary = new GroupScheduleOccurence
+                {
+                    OccurrenceId = occ.Id,
+                    GroupId = occ.GroupId,
+                    LocationId = occ.LocationId,
+                    ScheduleId = occ.ScheduleId,
+                    OccurrenceDate = occ.OccurrenceDate
+                };
+
+                if ( occ.Schedule == null )
+                {
+                    summary.StartDateTime = occ.OccurrenceDate;
+                }
+                else
+                {
+                    if ( !occ.Schedule.IsActive )
+                    {
+                        occ.Schedule.IsActive = true;
+                    }
+                    var tmpStartDate = occ.Schedule.GetNextStartDateTime( occ.OccurrenceDate );
+
+                    if ( tmpStartDate.HasValue )
+                    {
+                        summary.StartDateTime = tmpStartDate.Value;
+                    }
+                    else if ( occ.Schedule.WeeklyTimeOfDay.HasValue )
+                    {
+                        summary.StartDateTime = occ.OccurrenceDate.Add( occ.Schedule.WeeklyTimeOfDay.Value );
+                    }
+                    else
+                    {
+                        summary.StartDateTime = occ.OccurrenceDate;
+                    }
+                }
+                occurrences.Add( summary );
+            }
+
+            return occurrences;
+        }
+    }
+
+    internal class GroupScheduleOccurence
+    {
+        public int? OccurrenceId { get; set; }
+        public int? GroupId { get; set; }
+        public int? LocationId { get; set; }
+        public int? ScheduleId { get; set; }
+        public DateTime OccurrenceDate { get; set; }
+        public DateTime? StartDateTime { get; set; }
+
+        public override string ToString()
+        {
+            return $"G:{GroupId}^L:{LocationId}^S:{ScheduleId}^D:{OccurrenceDate:yyyyMMdd}";
         }
     }
 }
