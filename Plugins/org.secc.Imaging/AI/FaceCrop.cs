0d7b1acfb6999ac7a48ccb8be45acd9ec94a26ca
stphnlee <slee@secc.org>
2024-06-18 10:08:51 -0400

    Revert "Updated large images to be rendered at 2000 pixels in case half of a large image is still too large"; the logic is more complicated than just setting the large image size to 2000 pixels
    This reverts commit 96315d9f605bb01da19b4e65508a00aab399048b.

--- Plugins/org.secc.Imaging/AI/FaceCrop.cs
+++ Plugins/org.secc.Imaging/AI/FaceCrop.cs
@@ -62,7 +62,7 @@ namespace org.secc.Imaging.AI
 
             if ( isLargeImage )
             {
-                url = url + "&w=2000";
+                url = url + "&w=" + ( binaryFile.Width.Value / 2 ).ToString();
             }
 
             var detectedFace = await DetectFace( url );
