82ce29efa9a415acff58fa420192c60fac8f5705
Chris Funk <cfunk@secc.org>
2024-09-25 20:03:25 -0500

    ROCK-7196 added medical consent and verify info functionality

--- Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/FamilyCheckinConsent.ascx
+++ Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/FamilyCheckinConsent.ascx
@@ -1,37 +1,75 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="FamilyCheckinConsent.ascx.cs" Inherits="RockWeb.Plugins.org_secc.FamilyCheckin.FamilyCheckinConsent" %>
 <style>
-    #divVerifyInfo
+    #pnlCheckinPrompt
     {
-        background-color: #545454;
-        opacity:0.4;
+        background-color: #fff;
+        min-height:450px;
+        margin:auto;
+        padding:15px;
     }
-    #divVerifyInfo p
+    #pnlCheckinPrompt p
     {
-        color: #fff;
-        font-size: 1em;
+        color: #000;
+        /*font-size: 1em;*/
+    }
+
+    #pnlCheckinPrompt .actions {
+        padding-top:15px;
+        padding-bottom:15px;
+    }
+
+    #pnlCheckinPrompt .btn-danger-solid {
+        background-color: #d4442e !important ;
+    }
+    #pnlCheckinPrompt .btn-success {
+        background-color:#16c98d !important;
+
+    }
+
+    #pnlCheckinPrompt .btn-skip {
+        background-color: #1D1F21 !important;
+    }
+    #pnlCheckinPrompt .btn {
+        width:150px !important;
+        color: #ffffff !important;
+        font-weight:bolder !important;
     }
 </style>
 <asp:UpdatePanel ID="upMain" runat="server" >
     <ContentTemplate>
         <div class="checkin-header">
-            <h1>Medical Consent</h1>
+<%--            <h1>Medical Consent</h1>--%>
         </div>
         <div class="checkin-body">
             <div class="checkin-scroll-panel">
                 <div class="scroller">
-                    <asp:Panel ID="pnlVerifyInfo" runat="server" Visible="false">
-                        <asp:Literal ID="lVerifyInfoHtml" runat="server" />
-                    </asp:Panel>
-                    <asp:Panel ID="pnlMedicalConsent" runat="server" Visible="false">
-                        <asp:Literal ID="lMedConsentHTML" runat="server" />
+                    <asp:Panel ID="pnlCheckinPrompt" ClientIDMode="Static" runat="server" >
+                        <asp:Literal ID="lHouseholdInfo" runat="server" />
+
+                        <asp:Panel ID="pnlVerifyInfo" runat="server" CssClass="row" Visible="false">
+                            <div class="col-sm-12">
+                                <div class="actions">
+                                    <Rock:BootstrapButton ID="btnInfoYes" runat="server" CssClass="btn btn-lg btn-success" Text="Yes"  />
+                                    <span class="pull-right">
+                                        <Rock:BootstrapButton ID="btnInfoNo" runat="server" CssClass="btn btn-lg btn-danger-solid" Text="No" />
+                                    </span>
+                                </div>
+                            </div>
+                        </asp:Panel>
+
+                        <asp:Panel ID="pnlMedicalConsent" runat="server"  Visible="false" Style="padding-top:25px;" >
+                            <asp:Literal ID="lConsentMessage" runat="server" />
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <Rock:BootstrapButton ID="btnMedicalIConsentYes" runat="server" CssClass="btn btn-lg btn-success" Text="I Consent" />
+                                    <span class="pull-right">
+                                        <Rock:BootstrapButton ID="btnMedicalConsentSkip" runat="server" CssClass="btn btn-lg btn-skip" Text="Skip" />
+                                    </span>
+                                </div>
+                            </div>
+                        </asp:Panel>
                     </asp:Panel>
                 </div>
-            </div>
-        </div>
-        <div class="checkin-footer">
-            <div class="checkin-actions">
-
-                <asp:LinkButton ID="lbCancel" runat="server" CssClass="btn btn-default" Text="Cancel" />
             </div>
         </div>
     </ContentTemplate>
--- Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/FamilyCheckinConsent.ascx.cs
+++ Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/FamilyCheckinConsent.ascx.cs
@@ -1,25 +1,15 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
-
-
+using org.secc.FamilyCheckin.Cache;
 using Rock;
 using Rock.Attribute;
 using Rock.CheckIn;
 using Rock.Data;
 using Rock.Model;
-using Rock.Web;
 using Rock.Web.Cache;
-using Rock.Web.UI;
-using org.secc.FamilyCheckin.Cache;
-using org.secc.FamilyCheckin.Exceptions;
-using org.secc.FamilyCheckin.Model;
-using org.secc.FamilyCheckin.Utilities;
-using System.Data.Entity;
 
 
 
@@ -35,21 +25,37 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
         EditorTheme = Rock.Web.UI.Controls.CodeEditorTheme.Rock,
         Key = "VerifyInfoText",
         Order = 0 )]
+    [CodeEditorField("Medical Consent Text",
+        Description = "Message to display for medical consent.",
+        EditorMode = Rock.Web.UI.Controls.CodeEditorMode.Lava,
+        EditorTheme = Rock.Web.UI.Controls.CodeEditorTheme.Rock,
+        Key = "MedicalConsentText",
+        Order = 1)]
     [LavaCommandsField( "Lava Commands",
         Description = "Lava commands that are enabled on this block.",
         IsRequired = false,
         Key = "LavaCommands",
-        Order = 1 )]
+        Order = 2 )]
 
 
 
     public partial class FamilyCheckinConsent : CheckInBlock
     {
+        #region Fields
         KioskTypeCache KioskType = null;
+        int AdultAge = 18;
+        #endregion
 
+        #region Control Events
         protected override void OnInit( EventArgs e )
         {
             base.OnInit( e );
+
+            btnInfoNo.Click += btnInfo;
+            btnInfoYes.Click += btnInfo;
+            btnMedicalIConsentYes.Click += btnMedicalIConsentYes_Click;
+            btnMedicalConsentSkip.Click += btnMedicalConsentSkip_Click;
+
             var kioskTypeCookie = this.Page.Request.Cookies["KioskTypeId"];
             if (kioskTypeCookie != null)
             {
@@ -67,7 +73,7 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
         {
             base.OnLoad( e );
 
-            if(!IsPostBack)
+            if (!IsPostBack)
             {
                 if (!FamilyNeedsConsent())
                 {
@@ -78,50 +84,132 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
 
             }
         }
+        #endregion 
 
-        private void BuildInfoVerifyPanel()
+        #region Event
+        private void btnInfo( object sender, EventArgs e )
+        {
+            pnlVerifyInfo.Visible = false;
+            BuildMedicalConsent();
+        }
+
+        private void btnMedicalIConsentYes_Click( object sender, EventArgs e )
         {
             var rockContext = new RockContext();
+            Person parentGuardian = null;
+
             var familyId = CurrentCheckInState.CheckIn.CurrentFamily.Group.Id;
             var phoneSearchType = DefinedTypeCache.Get( Rock.SystemGuid.DefinedValue.CHECKIN_SEARCH_TYPE_PHONE_NUMBER.AsGuid() );
-            int? personId = null;
+            var personActiveStatus = DefinedTypeCache.Get( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_ACTIVE.AsGuid() );
 
             if(CurrentCheckInState.CheckIn.SearchType.Id == phoneSearchType.Id)
             {
-                personId = GetPhoneOwnerId( familyId, CurrentCheckInState.CheckIn.SearchValue );
+                var searchValue = CurrentCheckInState.CheckIn.SearchValue;
+                parentGuardian = new PersonService( rockContext ).Queryable().AsNoTracking()
+                    .Where( p => p.PrimaryFamilyId == familyId )
+                    .Where( p => p.AgeClassification == AgeClassification.Adult )
+                    .Where( p => p.PhoneNumbers.Where( n => n.Number.EndsWith( searchValue ) ).Any() )
+                    .FirstOrDefault();
+            }
+            else if(CurrentCheckInState.CheckIn.CheckedInByPersonAliasId.HasValue)
+            {
+                parentGuardian = new PersonAliasService( rockContext )
+                    .GetPerson( CurrentCheckInState.CheckIn.CheckedInByPersonAliasId.Value );
+            }
+            var adultBirthDate = RockDateTime.Today.AddYears( -AdultAge );
+            var children = new PersonService(rockContext).Queryable()
+                .Where(c => c.PrimaryFamilyId == familyId)
+                .Where(c => c.RecordStatusValueId == personActiveStatus.Id)
+                .Where( c => c.AgeClassification == AgeClassification.Child )
+                .Where( c => c.BirthDate > adultBirthDate )
+                .ToList();
 
-                if(!personId.HasValue)
+            foreach (var child in children)
+            {
+                string consentAttributeValue = $"{parentGuardian.FullName} {RockDateTime.Today.ToShortDateString()}";
+                child.LoadAttributes( rockContext );
+                child.SetAttributeValue( "MedicalConsent", consentAttributeValue );
+                child.SaveAttributeValue( "MedicalConsent", rockContext );
+                rockContext.SaveChanges();
+            }
+            NavigateToNextPage();
+
+        }
+
+        private void btnMedicalConsentSkip_Click( object sender, EventArgs e )
+        {
+            NavigateToNextPage();
+        }
+        #endregion
+
+        #region Methods
+        private void BuildInfoVerifyPanel()
+        {
+            var rockContext = new RockContext();
+            var personService = new PersonService( rockContext );
+
+            var familyId = CurrentCheckInState.CheckIn.CurrentFamily.Group.Id;
+            var phoneSearchType = DefinedTypeCache.Get( Rock.SystemGuid.DefinedValue.CHECKIN_SEARCH_TYPE_PHONE_NUMBER.AsGuid() );
+            Person phoneOwner = null;
+
+            if (CurrentCheckInState.CheckIn.SearchType.Id == phoneSearchType.Id)
+            {
+                var searchValue = CurrentCheckInState.CheckIn.SearchValue;
+                phoneOwner = personService.Queryable().AsNoTracking()
+                    .Where( p => p.PrimaryFamilyId == familyId )
+                    .Where( p => p.AgeClassification == AgeClassification.Adult )
+                    .Where( p => p.PhoneNumbers.Where( n => n.Number.EndsWith( searchValue ) ).Any() )
+                    .FirstOrDefault();
+
+                if (phoneOwner == null)
                 {
                     NavigateToNextPage();
                     return;
                 }
             }
-            var activePersonStatusId = DefinedValueCache.Get( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_ACTIVE.AsGuid() ).Id;
-            var phoneOwner = new PersonService( rockContext ).Get( personId.Value );
+            else if(CurrentCheckInState.CheckIn.CheckedInByPersonAliasId.HasValue)
+            {
+                phoneOwner = new PersonAliasService( rockContext ).GetPerson( CurrentCheckInState.CheckIn.CheckedInByPersonAliasId.Value );
+            }
+            else
+            {
+                NavigateToNextPage();
+                return;
+            }
 
-            var familyMembers = new GroupMemberService( rockContext )
-                .Queryable().AsNoTracking()
-                .Where( gm => gm.GroupId == familyId )
-                .Where( gm => gm.GroupMemberStatus == GroupMemberStatus.Active )
-                .Where( gm => gm.Person.RecordStatusValueId == activePersonStatusId )
-                .Select( gm => gm.Person )
+
+            var activePersonStatusId = DefinedValueCache.Get( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_ACTIVE.AsGuid() ).Id;
+            var adultBirthdate = RockDateTime.Today.AddYears( -AdultAge );
+            var family = personService.Queryable().AsNoTracking()
+                .Where(p => p.PrimaryFamilyId == familyId)
+                .Where(p => p.RecordStatusValueId == activePersonStatusId)
+                .Where(p => p.Id != phoneOwner.Id)                
                 .ToList();
 
+
             var mergeFields = Rock.Lava.LavaHelper.GetCommonMergeFields( this.RockPage );
-            mergeFields.Add( "FamilyMembers", familyMembers );
+            mergeFields.Add( "Person", phoneOwner );
+            mergeFields.Add( "Spouse", family.Where( f => f.AgeClassification == AgeClassification.Adult && f.BirthDate <= adultBirthdate ).FirstOrDefault() );
+            mergeFields.Add( "Children", family.Where( p => p.BirthDate > adultBirthdate ).OrderBy( p => p.BirthDate ).ToList() );
+            mergeFields.Add( "FamilyName", CurrentCheckInState.CheckIn.CurrentFamily.Group.Name );
 
             var lavaTemplate = GetAttributeValue( "VerifyInfoText" );
-            lVerifyInfoHtml.Text = lavaTemplate.ResolveMergeFields( mergeFields,phoneOwner, GetAttributeValue( "LavaCommands" ) );
+            lHouseholdInfo.Text = lavaTemplate.ResolveMergeFields( mergeFields, GetAttributeValue( "LavaCommands" ) );
             pnlVerifyInfo.Visible = true;
+        }
 
-
-            
+        private void BuildMedicalConsent()
+        {
+            var lavaTemplate = GetAttributeValue( "MedicalConsentText" );
+            var mergeFields = Rock.Lava.LavaHelper.GetCommonMergeFields( this.RockPage );
+            lConsentMessage.Text = lavaTemplate.ResolveMergeFields( mergeFields, GetAttributeValue( "LavaCommands" ) );
+            pnlMedicalConsent.Visible = true;
         }
 
         private bool FamilyNeedsConsent()
         {
 
-            var context = new RockContext();          
+            var context = new RockContext();
 
             if (!KioskType.RequiresMedicalConsent)
             {
@@ -134,7 +222,7 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
                 .Where( v => v.Attribute.EntityTypeId == personEntityTypeId )
                 .Where( v => v.Attribute.Key == "MedicalConsent" );
 
-            var minAdultBirthdate = RockDateTime.Today.AddYears( -18 );
+            var minAdultBirthdate = RockDateTime.Today.AddYears( -AdultAge );
             var family = CurrentCheckInState.CheckIn.CurrentFamily;
             var activeRecordStatusLUID = DefinedValueCache.GetId( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_ACTIVE.AsGuid() );
             var familyHasMinorsNeedingConsent = new GroupMemberService( context )
@@ -145,30 +233,14 @@ namespace RockWeb.Plugins.org_secc.FamilyCheckin
                 .Where( gm => gm.Group.IsArchived == false )
                 .Where( gm => gm.Person.RecordStatusValueId == activeRecordStatusLUID )
                 .Where( gm => gm.Person.BirthDate > minAdultBirthdate )
-                .GroupJoin(avQuery, gm => gm.Person.Id, p => p.Id,
-                    (p,v) => new {PersonId = p.Id, Value = v.Select(v1 => v1.Value).FirstOrDefault() ?? ""})
-                .Where(v => v.Value == "")
-                .Any();
+                .GroupJoin( avQuery, gm => gm.Person.Id, v => v.EntityId,
+                    ( p, v ) => new { PersonId = p.Id, Value = v.Select( v1 => v1.Value ).FirstOrDefault() ?? "" } )
+                .Where( v => v.Value == "" )
+                .ToList();
 
-            return familyHasMinorsNeedingConsent;
-
+            return familyHasMinorsNeedingConsent.Any();
         }
 
-        private int? GetPhoneOwnerId(int familyId, string phoneNumber)
-        {
-            var rockContext = new RockContext();
-            var personIds = CurrentCheckInState.CheckIn.CurrentFamily.Group.ActiveMembers()
-                .Select( p => p.PersonId ).ToList();
-            var ownerPersonId = new PhoneNumberService( rockContext )
-                .Queryable().AsNoTracking()
-                .Include( p => p.Person )
-                .Where( n => n.Number.EndsWith( phoneNumber ) )
-                .Where( n => personIds.Contains( n.PersonId ) )
-                .Where( n => n.Person.AgeClassification == AgeClassification.Adult )
-                .Select( n => n.PersonId )
-                .FirstOrDefault();
-
-            return ownerPersonId > 0 ? ownerPersonId : (int?)null;
-        }
+        #endregion
     }
 }
