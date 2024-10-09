f935a43741be22a4b7c4c8d85239c2ec420013ea
Chris Funk <cfunk@secc.org>
2024-10-09 10:57:58 -0500

    Rock7184 - Add deployment scripts

--- Tools/org.secc.WebsiteCleanup/scripts/DeployScripts.sql
+++ Tools/org.secc.WebsiteCleanup/scripts/DeployScripts.sql
@@ -1,89 +1,43 @@
-IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = '_org_secc_WebPageCleanupLog')
-BEGIN
-	CREATE TABLE dbo._org_secc_WebPageCleanupLog
-	(
-		Id INT NOT NULL Identity(1,1) PRIMARY KEY,
-		PageId INT NOT NULL,
-		InternalName NVARCHAR(100),
-		ParentPageId INT, 
-		LayoutId INT,
-		[Guid] UNIQUEIDENTIFIER,
-		InteractionCount INT,
-		PageStatus SMALLINT
-	)
-	
-
-	ALTER TABLE dbo._org_secc_WebPageCleanupLog 
-	ADD CONSTRAINT UniqueKey_Guid UNIQUE ([Guid])
-END 
-GO
-
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE RoutineName='_org_secc_WebPageCleanupLog_Insert')
-BEGIN
-	DROP PROCEDURE [dbo].[_org_secc_WebPageCleanupLog_Insert]
-END
-
-GO
-
-/****** Object:  StoredProcedure [dbo].[_org_secc_WebPageCleanupLog_Insert]    Script Date: 10/7/2024 2:21:19 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE PROCEDURE [dbo].[_org_secc_WebPageCleanupLog_Insert]
+CREATE TABLE [dbo].[_org_secc_WebPageCleanupLog](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[PageId] [int] NOT NULL,
+	[InternalName] [nvarchar](100) NULL,
+	[ParentPageId] [int] NULL,
+	[LayoutId] [int] NULL,
+	[Guid] [uniqueidentifier] NULL,
+	[InteractionCount] [int] NULL,
+	[PageStatus] [smallint] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[_org_secc_WebPageCleanupLog] ADD PRIMARY KEY CLUSTERED 
 (
-	@PageId INT, 
-	@InternalName NVARCHAR(100),
-	@ParentPageId INT,
-	@LayoutId INT,
-	@InteractionCount INT NULL,
-	@PageStatus SMALLINT NULL,
-	@Id INT OUTPUT
-)
-AS
-
-INSERT INTO dbo._org_secc_WebPageCleanupLog
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[_org_secc_WebPageCleanupLog] ADD  CONSTRAINT [UniqueKey_Guid] UNIQUE NONCLUSTERED 
 (
-	PageId,
-	InternalName,
-	ParentPageId,
-	LayoutId,
-	Guid,
-	InteractionCount,
-	PageStatus
-)
-VALUES
-(
-	@PageId,
-	@InternalName,
-	@ParentPageId,
-	@LayoutId,
-	NewId(),
-	@InteractionCount,
-	@PageStatus
-)
-
-SET @ID = SCOPE_IDENTITY()
+	[Guid] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
 
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE RoutineName='_org_secc_WebPageCleanup_Get_SitePages')
-BEGIN
-	DROP PROCEDURE [dbo].[_org_secc_WebPageCleanup_Get_SitePages]
-END
 
+SET ANSI_NULLS ON
 GO
-
-CREATE PROCEDURE dbo._org_secc_WebPageCleanup_Get_SitePages
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[_org_secc_WebPageCleanup_Get_SitePages]
 (
 	@SiteId INT 
 )
 
-AS 
+as 
 
 SELECT 
-	p.Id, 
+	p.Id AS PageId, 
 	ParentPageId,
 	InternalName,
 	LayoutId
@@ -93,5 +47,100 @@ FROM
 	INNER JOIN [dbo].[Site] s on l.SiteId = s.Id
 WHERE 
 	s.Id = @SiteId
+GO
 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[_org_secc_WebPageCleanup_Get_SitePages]
+(
+	@SiteId INT 
+)
+
+as 
+
+SELECT 
+	p.Id AS PageId, 
+	ParentPageId,
+	InternalName,
+	LayoutId
+FROM	
+	[dbo].[Page] p
+	INNER JOIN [dbo].[Layout] l on p.LayoutId = l.Id
+	INNER JOIN [dbo].[Site] s on l.SiteId = s.Id
+WHERE 
+	s.Id = @SiteId
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[_org_secc_WebPageCleanup_Get_SitePages]
+(
+	@SiteId INT 
+)
+
+as 
+
+SELECT 
+	p.Id AS PageId, 
+	ParentPageId,
+	InternalName,
+	LayoutId
+FROM	
+	[dbo].[Page] p
+	INNER JOIN [dbo].[Layout] l on p.LayoutId = l.Id
+	INNER JOIN [dbo].[Site] s on l.SiteId = s.Id
+WHERE 
+	s.Id = @SiteId
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[_org_secc_WebPageCleanup_Get_SitePages]
+(
+	@SiteId INT 
+)
+
+as 
+
+SELECT 
+	p.Id AS PageId, 
+	ParentPageId,
+	InternalName,
+	LayoutId
+FROM	
+	[dbo].[Page] p
+	INNER JOIN [dbo].[Layout] l on p.LayoutId = l.Id
+	INNER JOIN [dbo].[Site] s on l.SiteId = s.Id
+WHERE 
+	s.Id = @SiteId
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[_org_secc_WebPageCleanup_Get_SitePages]
+(
+	@SiteId INT 
+)
+
+as 
+
+SELECT 
+	p.Id AS PageId, 
+	ParentPageId,
+	InternalName,
+	LayoutId
+FROM	
+	[dbo].[Page] p
+	INNER JOIN [dbo].[Layout] l on p.LayoutId = l.Id
+	INNER JOIN [dbo].[Site] s on l.SiteId = s.Id
+WHERE 
+	s.Id = @SiteId
 GO
