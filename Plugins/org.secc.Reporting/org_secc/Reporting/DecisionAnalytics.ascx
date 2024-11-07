ff29c5cb24b6b5d371e0d3c9a39854f4583ca7e3
Chris Funk <cfunk@secc.org>
2024-11-06 19:11:13 -0500

    ROCK-7292 decision report initial comit

---
+++ Plugins/org.secc.Reporting/org_secc/Reporting/DecisionAnalytics.ascx
@@ -0,0 +1,51 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="DecisionAnalytics.ascx.cs" Inherits="RockWeb.Plugins.org_secc.Reporting.DecisionAnalytics" %>
+<asp:UpdatePanel ID="upMain" runat="server">
+    <ContentTemplate>
+        <asp:Panel ID="pnlView" runat="server">
+            <div class="panel panel-block panel-analytics">
+                <div class="panel-heading panel-follow">
+                    <h1 class="panel-title">
+                        <i class="fa fa-list">Decision Analytics</i>
+                    </h1>
+                    <div class="panel-labels"></div>
+                    <div class="rock-fullscreen-toggle js-fullscreen-trigger"></div>
+                </div>
+                <div class="panel-body">
+                    <div class="row row-eq-height-md">
+                        <div class="col-md-3 filter-options">
+                            <Rock:DateRangePicker ID="drpDecisionDate" runat="server" Label="Decision Date" />
+                            <Rock:PersonPicker ID="ppDecisions" runat="server" Label="Person" />
+                            <Rock:RockCheckBoxList ID="lGender" runat="server" Label="Gender">
+                                <asp:ListItem Text="Male" Value="1" Selected="True" />
+                                <asp:ListItem Text="Female" Value="2" Selected="True" />
+                            </Rock:RockCheckBoxList>
+                            <Rock:NumberRangeEditor ID="nreAgeRange" runat="server" Label="Age Range" MinimumValue="0" MaximumValue="110" NumberType="Integer" />
+                            <label class="control-label">Grade</label>
+                            <div class="form-control-group">
+                                <Rock:RockDropDownList ID="ddlLowerGrade" runat="server" CssClass="input-width-md" />
+                                <asp:Label ID="lblGradeRangeTo" runat="server" Text="to" CssClass="to" />
+                                <Rock:RockDropDownList ID="ddlUpperGrade" runat="server" CssClass="input-width-md" />
+                            </div>
+                            <Rock:DefinedValuesPicker ID="dvpConnectionStatus" runat="server" Label="Connection Status" />
+                            <Rock:CampusPicker ID="pkFamilyCampus" runat="server" Label="Family Campus" IncludeInactive="false" />
+                            <Rock:CampusPicker ID="pkDecisionCampus" runat="server" Label="Decision Campus" IncludeInactive="false" />
+                            <Rock:RockDropDownList ID="ddlDecisionType" runat="server" Label="Decision Type" />
+                            <Rock:RockDropDownList ID="ddlEventType" runat="server" Label="Event" />
+                            <Rock:DefinedValuesPicker ID="dvpBaptismType" runat="server" Label="Baptism Type" />
+
+                        </div>
+                        <div class="col-md-9">
+                            <%--content--%>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </asp:Panel>
+        <script>
+            Sys.Application.add_load( function ()
+            {
+                Rock.controls.fullScreen.initialize();
+            } );
+        </script>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Plugins/org.secc.Reporting/org_secc/Reporting/DecisionAnalytics.ascx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+using Rock.Data;
+using System.Data.Entity;
+
+
+namespace RockWeb.Plugins.org_secc.Reporting
+{
+
+    [DisplayName("Decision Analytics")]
+    [Category("SECC > Reporting")]
+    [Description("Reports of people who made a next step decision.")]
+
+    [WorkflowTypeField("Decision Guide Workflow",
+        Description = "The workflow form that the decision guide uses to record a person's decision.",
+        AllowMultiple = false,
+        IsRequired = true,
+        DefaultValue = "57F2615F-133D-4FC0-8024-181CA3E596E2",
+        Key = AttributeKeys.DecisionWorkflowKey,
+        Order = 0)]
+    [TextField("Decision Type Key",
+        Description = "The key for the Decision Type attribute.",
+        IsRequired = true,
+        DefaultValue = "WhatDecision",
+        Key = AttributeKeys.DecisionTypeAttributeKey,
+        Order = 1)]
+    [TextField("Campus Key",
+        Description = "Attribute Key of the campus where the decision was made.",
+        IsRequired = true,
+        DefaultValue ="Campus",
+        Key = AttributeKeys.CampusAttributeKey,
+        Order = 2)]
+    [TextField("Event Key",
+        Description = "The key of the Event attribute.",
+        IsRequired = true,
+        DefaultValue = "Event",
+        Key = AttributeKeys.EventAttributeKey,
+        Order = 3 )]
+
+
+    public partial class DecisionAnalytics : RockBlock
+    {
+
+        public class AttributeKeys
+        {
+            public const string DecisionWorkflowKey = "DecisionGuideWorkflow";
+            public const string DecisionTypeAttributeKey = "DecisionTypeAttributeKey";
+            public const string CampusAttributeKey = "CampusAttributeKey";
+            public const string EventAttributeKey = "EventAttributeKey";
+        }
+
+        private int workflowTypeId;
+        private string decisionTypeAttributeKey;
+        private string campusAttributeKey;
+        public string eventAttributeKey;
+
+        #region Base Control Methods
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+            dvpConnectionStatus.DefinedTypeId = DefinedTypeCache.GetId( Rock.SystemGuid.DefinedType.PERSON_CONNECTION_STATUS.AsGuid() );
+            dvpBaptismType.DefinedTypeId = DefinedTypeCache.GetId( Guid.Parse( "75FEF4A0-90E2-46AC-9A83-76672F0FB402" ) );
+            
+        }
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            workflowTypeId = WorkflowTypeCache.Get( GetAttributeValue( AttributeKeys.DecisionWorkflowKey ).AsGuid() ).Id;
+            decisionTypeAttributeKey = GetAttributeValue( AttributeKeys.DecisionTypeAttributeKey );
+            campusAttributeKey = GetAttributeValue( AttributeKeys.CampusAttributeKey );
+            eventAttributeKey = GetAttributeValue( AttributeKeys.EventAttributeKey );
+
+            if(!IsPostBack)
+            {
+                LoadBoundFields();
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Methods
+        private void LoadBoundFields()
+        {
+            LoadGradeRanges();
+            LoadDecisionType();
+        }
+
+        private void LoadDecisionType()
+        {
+            var decisionTypes = new List<string>();
+            using (var rockContext = new RockContext())
+            {
+                var workflowETID = EntityTypeCache.GetId( typeof( Workflow) );
+                var workflowTypeIdStr = workflowTypeId.ToString();
+
+                var values = new AttributeService( rockContext ).Queryable()
+                    .Include( a => a.AttributeQualifiers )
+                    .Where( a => a.EntityTypeId == workflowETID )
+                    .Where( a => a.EntityTypeQualifierColumn == "WorkflowTypeId" )
+                    .Where( a => a.EntityTypeQualifierValue == workflowTypeIdStr )
+                    .Where( a => a.Key == decisionTypeAttributeKey )
+                    .FirstOrDefault()
+                    .AttributeQualifiers.Where( q => q.Key == "values" )
+                    .Select( q => q.Value )
+                    .FirstOrDefault();
+
+                decisionTypes.AddRange( values.SplitDelimitedValues() );                
+            }
+
+            foreach (var value in decisionTypes)
+            {
+                ddlDecisionType.Items.Add( new ListItem( value, value ) );
+            }
+            ddlDecisionType.Items.Insert( 0, new ListItem( "", "" ) );
+        }
+
+        private void LoadGradeRanges()
+        {
+            var rockContext = new RockContext();
+            var dvservice = new DefinedValueService( rockContext );
+            var avService = new AttributeValueService( rockContext );
+
+            var definedTypeId = DefinedTypeCache.GetId( Rock.SystemGuid.DefinedType.SCHOOL_GRADES.AsGuid() );
+            var definedValueETID = EntityTypeCache.Get( typeof( DefinedValue ) ).Id;
+            var definedTypeIdStr = definedTypeId.ToString();
+
+            var avQry = avService.Queryable().AsNoTracking()
+                .Where( av => av.Attribute.EntityTypeId == definedValueETID )
+                .Where( av => av.Attribute.EntityTypeQualifierColumn == "DefinedTypeId" )
+                .Where( av => av.Attribute.EntityTypeQualifierValue == definedTypeIdStr )
+                .Where( av => av.Attribute.Key == "Abbreviation" );
+
+            var grades = dvservice.Queryable().AsNoTracking()
+                .Where( dv => dv.DefinedTypeId == definedTypeId )
+                .Where( dv => dv.IsActive )
+                .Join( avQry, dv => dv.Id, av => av.EntityId,
+                    ( d, av ) => new { d.Id, d.Description, d.Order, Abbreviation = av.Value } )
+                .OrderBy( d => d.Order )
+                .ToList();
+
+            ddlLowerGrade.Items.Clear();
+            ddlUpperGrade.Items.Clear();
+            foreach (var grade in grades)
+            {
+                ddlLowerGrade.Items.Add( new ListItem( grade.Abbreviation, grade.Id.ToString() ) );
+                ddlUpperGrade.Items.Add( new ListItem( grade.Abbreviation, grade.Id.ToString() ) );
+
+            }
+            ddlLowerGrade.Items.Insert( 0, new ListItem( "", "" ) );
+            ddlUpperGrade.Items.Insert( 0, new ListItem( "", "" ) );
+                                                                                                                                           
+        }
+        #endregion
+
+    }
+}
