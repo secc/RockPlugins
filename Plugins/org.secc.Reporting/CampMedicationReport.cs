cb3c946cc9f3f8ce5469c2dd8116a5aa205f6126
Chris Funk <cfunk@secc.org>
2025-02-11 14:22:29 -0500

    Medication Report Updates

--- Plugins/org.secc.Reporting/CampMedicationReport.cs
+++ Plugins/org.secc.Reporting/CampMedicationReport.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 using Rock;
 using Rock.Data;
-using Rock.Model;
-using Rock.Tasks;
 using Rock.Web.Cache;
 
 
@@ -17,11 +13,7 @@ namespace org.secc.Reporting
     public class CampMedicationReport
     {
         private readonly CampMedicationReportFilter _filter;
-        private char _delimiter = ',';
-        private char _textQualifier = '\"';
-        private string _medicationAttributeKey = "Medications";
-        private string _medicationScheduleDefinedTypeGuid = "81b51822-50d7-4be6-a462-04077405bb7e";
-        private Dictionary<int, List<int>> _instanceSmallGroups = new Dictionary<int, List<int>>();
+        private Dictionary<int, CampusCache> _instanceCampuses = new Dictionary<int, CampusCache>();
 
 
         public CampMedicationReport( CampMedicationReportFilter filter )
@@ -29,226 +21,171 @@ namespace org.secc.Reporting
             _filter = filter;
         }
 
-        public string GenerateMedicationReport()
+        public List<CampMedicationReportItem> GenerateMedicationReportData()
         {
-
-            using (var context = new RockContext())
+            if(_filter.RegistrationTemplateId <= 0)
             {
-                var campuses = CampusCache.All().Select( c => new { c.Id, c.Name } );
-                var medSchedules = DefinedTypeCache.Get( _medicationScheduleDefinedTypeGuid.AsGuid() )
-                    .DefinedValues;
-                var camperMedications = new List<CampMedicationReportItem>();
-
-
-                var personEntityTypeId = EntityTypeCache.GetId( typeof( Person ) );
-                var medicationMatrixAttribute = new AttributeService( context ).GetByEntityTypeId( personEntityTypeId )
-                    .Where( a => a.Key == _medicationAttributeKey )
-                    .FirstOrDefault();
-
-                var registrantService = new RegistrationRegistrantService( context );
-
-                var medAttributeQry = new AttributeValueService( context ).Queryable().AsNoTracking()
-                    .Where( v => v.AttributeId == medicationMatrixAttribute.Id );
-
-                var registrantQry = registrantService.Queryable().AsNoTracking()
-                    .Include( r => r.Registration.RegistrationInstance )
-                    .Include( r => r.Registration.RegistrationInstance.RegistrationTemplate )
-                    .Include( r => r.PersonAlias.Person )
-                    .Where( r => r.OnWaitList == false );
-
-                if (_filter.RegistrationInstanceId.HasValue)
-                {
-                    registrantQry = registrantQry.Where( r => r.Registration.RegistrationInstanceId == _filter.RegistrationInstanceId.Value );
-                }
-
-                else if (_filter.RegistrationTemplateId.HasValue)
-                {
-                    registrantQry = registrantQry.Where( r => r.Registration.RegistrationInstance.RegistrationTemplateId == _filter.RegistrationTemplateId );
-                }
-
-                var registrantsWithMedAttributes = registrantQry.Join( medAttributeQry, r => r.PersonAlias.PersonId,
-                    v => v.EntityId, ( r, v ) => new { Registrant = r, AttributeGridGuid = v.Value } )
-                    .AsEnumerable();
-
-                List<CampMedicationReportItem> reportItems = new List<CampMedicationReportItem>();
-
-                foreach (var r in registrantsWithMedAttributes)
-                {
-
-                    if (r.AttributeGridGuid.IsNullOrWhiteSpace())
-                    {
-                        continue;
-                    }
-
-                    var smallGroup = GetSmallGroup( r.Registrant.PersonAlias.PersonId, r.Registrant.Registration.RegistrationInstanceId );
-                    var meds = GetMedications( r.AttributeGridGuid.AsGuid() );
-                    var campus = campuses.Where( c => r.Registrant.Registration.RegistrationInstance.Name.EndsWith( c.Name, StringComparison.InvariantCultureIgnoreCase ) ).FirstOrDefault();
-                    foreach (var med in meds)
-                    {
-                        var scheduleGuids = med.ScheduleGuids
-                            .SplitDelimitedValues()
-                            .AsGuidList();
-
-                        reportItems.Add( new CampMedicationReportItem
-                        {
-                            PersonId = r.Registrant.PersonAlias.Person.Id,
-                            LastName = r.Registrant.PersonAlias.Person.LastName,
-                            NickName = r.Registrant.PersonAlias.Person.NickName,
-                            BirthDate = r.Registrant.PersonAlias.Person.BirthDate,
-                            SmallGroup = smallGroup != null ? smallGroup.GroupName : null,
-                            GroupLeader = smallGroup != null ? smallGroup.GroupLeader : null,
-                            RegistrationInstanceName = r.Registrant.Registration.RegistrationInstance.Name,
-                            CampusId = campus != null ? (int?) campus.Id : null,
-                            CampusName = campus != null ? campus.Name : null,
-                            MedicationName = med.Medication,
-                            Instructions = med.Instructions,
-                            ScheduleDefinedValueIds = medSchedules.Where( s => scheduleGuids.Contains( s.Guid ) ).Select( s => s.Id ).ToList()
-                        } );
-
-
-                    }
-
-                }
-
-                return reportItems.Take( 10 ).ToJson();
+                throw new ArgumentException( "Registration Template Id must be greater than 0." );
             }
-
-
-
-        }
-
-        public List<CampMedicationSummary> GetMedications( Guid medicationGridGuid )
-        {
             using (var context = new RockContext())
             {
-                var matrixItemEntityId = EntityTypeCache.GetId( typeof( AttributeMatrixItem ) );
-                var matrixItemService = new AttributeMatrixItemService( context );
-
-                var attributeValueQry = new AttributeValueService( context )
-                    .Queryable().AsNoTracking()
-                    .Where( q => q.Attribute.EntityTypeId == matrixItemEntityId.Value );
-
-                var meds = matrixItemService.Queryable().AsNoTracking()
-                    .Where( i => i.AttributeMatrix.Guid == medicationGridGuid )
-                    .Join( attributeValueQry, i => i.Id, v => v.EntityId,
-                        ( i, v ) => new { ItemId = i.Id, AttributeKey = v.Attribute.Key, AttributeValue = v.Value } )
-                    .GroupBy( i => i.ItemId )
-                    .Select( i => new CampMedicationSummary
-                        {
-                            ItemId = i.Key,
-                            Medication = i.Where( i1 => i1.AttributeKey == "Medication" ).Select( i1 => i1.AttributeValue ).FirstOrDefault(),
-                            Instructions = i.Where( i1 => i1.AttributeKey == "Instructions" ).Select( i1 => i1.AttributeValue ).FirstOrDefault(),
-                            ScheduleGuids = i.Where( i1 => i1.AttributeKey == "Schedule" ).Select( i1 => i1.AttributeValue ).FirstOrDefault()
-                        } )
+                var campuses = CampusCache.All( context )
+                    .Select( c => new { c.Id, c.Name } )
                     .ToList();
 
-                return meds;
-            }
+                var sqlParam = new List<SqlParameter>();
+                sqlParam.Add( new SqlParameter( "@RegistrationTemplateId", _filter.RegistrationTemplateId ) );
+                sqlParam.Add( new SqlParameter( "@RegistrationInstanceId", _filter.RegistrationInstanceId ) );
 
 
-            
-        }
+                var camperMeds = context.Database.SqlQuery<CampMedicationReportItem>( "dbo._org_secc_CampManager_GetMedicationReport @RegistrationTemplateId, @RegistrationInstanceId ",
+                    sqlParam.ToArray() ).ToList();
 
-        public CampSmallGroupSummary GetSmallGroup( int personId, int registrationInstanceId )
-        {
-            var smallGroupTypeGuid = Guid.Parse( "2936a009-2552-448e-9c3c-17d9cc0f8742" );
-
-            using (var context = new RockContext())
-            {
-
-                if (!_instanceSmallGroups.ContainsKey( registrationInstanceId ))
+                foreach (var c in camperMeds)
                 {
-                    var smallGroupGroupTypeId = GroupTypeCache.GetId( smallGroupTypeGuid ).Value;
-
-                    var placementService = new RegistrationTemplatePlacementService( context );
-                    var placementId = placementService.Queryable().AsNoTracking()
-                        .Where( p => p.GroupTypeId == smallGroupGroupTypeId )
-                        .Where( p => p.RegistrationTemplate.Instances.Select( i => i.Id ).Contains( registrationInstanceId ) )
-                        .Select( p => p.Id )
-                        .FirstOrDefault();
-
-                    var registrationInstanceSmallGroupIds = new RegistrationInstanceService( context )
-                        .GetRegistrationInstancePlacementGroupsByPlacement( registrationInstanceId, placementId )
-                        .Select( g => g.Id )
-                        .ToList();
-
-                    _instanceSmallGroups.Add( registrationInstanceId, registrationInstanceSmallGroupIds );
+                    var campus = GetCamperCampus( c );
+                    if (campus != null)
+                    {
+                        c.CampusId = campus.Id;
+                        c.CampusName = campus.Name;
+                    }
                 }
 
-                var groupMemberSerivce = new GroupMemberService( context );
-                var group = groupMemberSerivce.Queryable().AsNoTracking()
-                    .Where( m => _instanceSmallGroups[registrationInstanceId].Contains( m.GroupId ) )
-                    .Where( m => m.PersonId == personId && m.GroupMemberStatus == GroupMemberStatus.Active )
-                    .Select( m => m.Group )
+                var camperMedsQry = camperMeds.AsQueryable();
+
+                if(_filter.CampusId.HasValue)
+                {
+                    camperMedsQry = camperMedsQry.Where( c => c.CampusId == _filter.CampusId.Value );
+                }
+                
+
+                if(_filter.ScheduleGuid.HasValue)
+                {
+                    camperMedsQry = camperMedsQry.Where( c => c.DelimitedScheduleGuids.Contains( _filter.ScheduleGuid.Value ) );
+                }
+
+
+                return camperMedsQry.ToList();
+            }
+
+          
+        }
+
+        private CampusCache GetCamperCampus(CampMedicationReportItem c)
+        {
+            if(!_instanceCampuses.ContainsKey(c.RegistrationInstanceId))
+            {
+                var instanceCampus = CampusCache.All()
+                    .Where( c1 => c.RegistrationInstanceName.EndsWith( c1.Name, StringComparison.InvariantCultureIgnoreCase ) )
                     .FirstOrDefault();
 
-                if (group == null)
+                if(instanceCampus != null)
+                {
+                    _instanceCampuses.Add( c.RegistrationInstanceId, instanceCampus );
+                }
+                else
                 {
                     return null;
                 }
 
-                var sms = new CampSmallGroupSummary
-                {
-                    GroupId = group.Id,
-                    GroupName = group.Name
-                };
-
-
-                var leader = groupMemberSerivce.Queryable().AsNoTracking()
-                    .Where( m => m.GroupId == sms.GroupId )
-                    .Where( m => m.GroupMemberStatus == GroupMemberStatus.Active )
-                    .Where( m => m.GroupRole.IsLeader )
-                    .Select( m => m.Person )
-                    .FirstOrDefault();
-
-                if (leader != null)
-                {
-                    sms.GroupLeader = leader.FullName;
-                }
-
-                return sms;
             }
-
+            return _instanceCampuses[c.RegistrationInstanceId];
 
         }
 
-        public class CampSmallGroupSummary
-        {
-            public int GroupId { get; set; }
-            public string GroupName { get; set; }
-            public string GroupLeader { get; set; }
-        }
-
-        public class CampMedicationSummary
-        {
-            public int ItemId { get; set; }
-            public string Medication { get; set; }
-            public string Instructions { get; set; }
-            public string ScheduleGuids { get; set; }
-        }
-
-        public class CampMedicationReportItem
-        {
-            public int PersonId { get; set; }
-            public string LastName { get; set; }
-            public string NickName { get; set; }
-            public DateTime? BirthDate { get; set; }
-            public string SmallGroup { get; set; }
-            public string GroupLeader { get; set; }
-            public string MedicationName { get; set; }
-            public string Instructions { get; set; }
-            public List<int> ScheduleDefinedValueIds { get; set; }
-            public string RegistrationInstanceName { get; set; }
-            public int? CampusId { get; set; }
-            public string CampusName { get; set; }
-        }
-
-        public class CampMedicationReportFilter
-        {
-            public int? RegistrationTemplateId { get; set; }
-            public int? RegistrationInstanceId { get; set; }
-            public int? CampusId { get; set; }
-            public List<int> ScheduleIds { get; set; }
-        }
     }
+
+
+
+    public class CampMedicationReportItem
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public int Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public int RegistrationTemplateId { get; set; }
+        public string RegistrationTemplateName { get; set; }
+        public int RegistrationInstanceId { get; set; }
+        public string RegistrationInstanceName { get; set; }
+        public string Medication { get; set; }
+        public string Instructions { get; set; }
+        public string Schedule { get; set; }
+        public int? GroupId { get; set; }
+        public string GroupName { get; set; }
+        public string LeaderName { get; set; }
+        public int? CampusId { get; set; }
+        public string CampusName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Concat( NickName, " ", LastName );
+            }
+        }
+        [JsonIgnore]
+        public List<Guid> DelimitedScheduleGuids
+        {
+            get
+            {
+                return Schedule.SplitDelimitedValues().AsGuidList();
+            }
+        }
+
+        public bool Breakfast
+        {
+            get
+            {
+                return DelimitedScheduleGuids
+                    .Contains( "0dd03b01-0467-4536-88c3-61bb414d2f2a".AsGuid() );
+            }
+        }
+
+        public bool Lunch
+        {
+            get
+            {
+                return DelimitedScheduleGuids
+                    .Contains( "c51ecbc4-f1f2-4ea3-820d-8fb5ba6e0a99".AsGuid() );
+            }
+        }
+
+        public bool Dinner
+        {
+            get
+            {
+                return DelimitedScheduleGuids
+                    .Contains( "ed6b440b-7401-4e0b-87f1-0ee2f6dd3b65".AsGuid() );
+            }
+        }
+
+        public bool Bedtime
+        {
+            get
+            {
+                return DelimitedScheduleGuids
+                    .Contains( "4d1033a6-3e03-43df-9982-7f5f4360497a".AsGuid() );
+            }
+        }
+
+        public bool AsNeeded
+        {
+            get
+            {
+                return DelimitedScheduleGuids
+                    .Contains( "f7d7f281-929d-4eb4-8e5c-464a9abec0f1".AsGuid() );
+            }
+        }
+
+    }
+
+    public class CampMedicationReportFilter
+    {
+        public int RegistrationTemplateId { get; set; }
+        public int RegistrationInstanceId { get; set; }
+        public int? CampusId { get; set; }
+        public Guid? ScheduleGuid { get; set; }
+    }
+
 }
---
+++ Plugins/org.secc.Reporting/org_secc/Reporting/MedReportTest.ascx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+
+using Rock.Web;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+using org.secc.Reporting;
+
+
+namespace RockWeb.Plugins.org_secc.Reporting
+{
+    [DisplayName("Med Test")]
+    [Category("SECC > Report")]
+    [Description("Test Application")]
+    public partial class MedReportTest : RockBlock
+    {
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+            lbRunTest.Click += lbRunTest_Click;
+
+            gMedications.GridRebind += gMedications_GridRebind;
+            gMedications.Actions.ShowAdd = false;
+            gMedications.Actions.ShowBulkUpdate = false;
+            gMedications.Actions.ShowCommunicate = false;
+            gMedications.Actions.ShowMergePerson = false;
+            gMedications.Actions.ShowMergeTemplate = false;
+            gMedications.ShowWorkflowOrCustomActionButtons = false;
+            gMedications.ShowHeaderWhenEmpty = false;
+            gMedications.EnableStickyHeaders = true;
+        }
+
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+        }
+
+        private void lbRunTest_Click( object sender, EventArgs e )
+        {
+            BindMedGrid();
+
+            
+        }
+        private void gMedications_GridRebind( object sender, GridRebindEventArgs e )
+        {
+            BindMedGrid();
+        }
+
+
+        private void BindMedGrid()
+        {
+            var filter = new CampMedicationReportFilter();
+            filter.RegistrationTemplateId = 1371;
+            //filter.RegistrationInstanceId = 5251;
+
+            var report = new CampMedicationReport( filter ).GenerateMedicationReportData();
+            gMedications.DataSource = report;
+            gMedications.DataBind();
+        }
+    }
+}
