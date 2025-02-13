e1af3518bd2a29e2f4f9ca28779cfa402122c26a
Chris Funk <cfunk@secc.org>
2025-02-12 20:42:01 -0600

    ROCK-7403 Medication Report with export

---
+++ Plugins/org.secc.Reporting/CampMedicationReport/CampMedicationReportFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace org.secc.Reporting
+{
+    public class CampMedicationReportFilter
+    {
+        public int RegistrationTemplateId { get; set; }
+        public int RegistrationInstanceId { get; set; }
+        public int? CampusId { get; set; }
+        public Guid? ScheduleGuid { get; set; }
+    }
+
+}
