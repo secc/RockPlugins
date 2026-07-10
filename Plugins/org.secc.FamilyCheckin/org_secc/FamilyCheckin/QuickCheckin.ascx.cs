530610ceb6099becbca756324c5dd3a43432ffce
Stephen Lee <slee@secc.org>
2026-07-10 16:08:09 -0400

    ROCK-8801 Add missing System.Data.Entity using for AsNoTracking

--- Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/QuickCheckin.ascx.cs
+++ Plugins/org.secc.FamilyCheckin/org_secc/FamilyCheckin/QuickCheckin.ascx.cs
@@ -15,6 +15,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
