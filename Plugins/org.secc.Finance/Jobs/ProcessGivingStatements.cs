6ddd2dde2a5314b6560fde3384b842204bddae0a
Stephen Lee <slee@secc.org>
2026-07-20 16:49:03 -0400

    Remove duplicate ProcessGivingStatements job from org.secc.Workflow
    The Workflow copy was added in accfe778 to satisfy a stale csproj Compile
    entry; the canonical job lives in org.secc.Finance/Jobs. Having the same
    class in two assemblies makes the job type ambiguous in Rock. Also restore
    [DisallowConcurrentExecution] on the Finance copy, which was dropped during
    the IJob -> RockJob conversion.

--- Plugins/org.secc.Finance/Jobs/ProcessGivingStatements.cs
+++ Plugins/org.secc.Finance/Jobs/ProcessGivingStatements.cs
@@ -16,6 +16,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Quartz;
 using Rock;
 using Rock.Attribute;
 using Rock.Data;
@@ -31,6 +32,7 @@ namespace org.secc.Finance.Jobs
     /// </summary>
     [WorkflowTypeField( "Statement Generator Workflow", "", false, true, "", "Workflow" )]
     [TextField( "Workflow Activity Name", "The name of the PDF generation activity within the Statement Generator Workflow", true )]
+    [DisallowConcurrentExecution]
     public class ProcessGivingStatements : RockJob
     {
         /// <summary> 
